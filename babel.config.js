5f4b8cdaed62151d5ab556c47feec75705183723
Trond Bergquist <trond_bergquist@hotmail.com>
2018-11-26 12:12:27 +0100

    Enable nullish coalescing

--- babel.config.js
+++ babel.config.js
@@ -29,6 +29,7 @@ module.exports = function(api /*: ApiType */) {
     '@babel/plugin-proposal-object-rest-spread',
     '@babel/plugin-proposal-class-properties',
     '@babel/plugin-proposal-optional-chaining',
+    '@babel/plugin-proposal-nullish-coalescing-operator',
   ];
 
   return {
