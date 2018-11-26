9f5257d65cf600ea03ef15b31cfd7bb124d8ba18
Martin Zlámal <mrtnzlml@gmail.com>
2018-11-26 12:53:20 +0100

    NPM publisher: implement packages autodiscovery
    NPM packages are now automatically discovered in the filesystem and they are published when then have `private:false` status. This applies only to `src/packages` folder where we store the packages.
    
    Closes: https://gitlab.skypicker.com/graphql/graphql/issues/370

--- src/packages/npm-publisher/src/index.js
+++ src/packages/npm-publisher/src/index.js
@@ -6,6 +6,7 @@ import tar from 'tar';
 import path from 'path';
 import semver from 'semver';
 import webpack from 'webpack';
+import glob from 'glob';
 
 import paths from '../../../../paths';
 import NPM from './NPM';
@@ -13,71 +14,75 @@ import createWebpackConfig from './createWebpackConfig';
 
 // TODO: rimraf build cache
 
-['graphql-resolve-wrapper', 'logz'].forEach(packageFolder => {
-  // $FlowIssue: https://github.com/facebook/flow/issues/2692
-  const packageJSON = require(path.join(
-    paths.packages,
-    packageFolder,
-    'package.json',
-  ));
+glob(path.join(paths.packages, './*/package.json'), (error, filenames) => {
+  filenames.forEach(filename => {
+    // $FlowIssue: https://github.com/facebook/flow/issues/2692
+    const packageJSON = require(filename);
+    // we can publish only public packages
+    if (packageJSON.private === false) {
+      const packageFolder = path
+        .dirname(filename)
+        .replace(new RegExp(paths.packages + '/'), '');
 
-  NPM.getPackageInfo(
-    {
-      package: packageJSON.name,
-    },
-    (error, data /*, raw, res*/) => {
-      if (error) {
-        throw error;
-      }
+      NPM.getPackageInfo(
+        {
+          package: packageJSON.name,
+        },
+        (error, data /*, raw, res*/) => {
+          if (error) {
+            throw error;
+          }
 
-      if (semver.gt(packageJSON.version, data.latest)) {
-        webpack(
-          createWebpackConfig(packageFolder, packageJSON),
-          async (err, stats) => {
-            if (err || stats.hasErrors()) {
-              console.error(err);
-            }
+          if (semver.gt(packageJSON.version, data.latest)) {
+            webpack(
+              createWebpackConfig(packageFolder, packageJSON),
+              async (err, stats) => {
+                if (err || stats.hasErrors()) {
+                  console.error(err);
+                }
 
-            console.log(
-              stats.toString({
-                colors: true,
-              }),
-            );
+                console.log(
+                  stats.toString({
+                    colors: true,
+                  }),
+                );
 
-            await tar.create(
-              {
-                gzip: true,
-                cwd: paths.buildCache,
-                portable: true,
-                file: path.join(paths.buildCache, packageFolder + '.tgz'),
-              },
-              [packageFolder],
-            );
+                await tar.create(
+                  {
+                    gzip: true,
+                    cwd: paths.buildCache,
+                    portable: true,
+                    file: path.join(paths.buildCache, packageFolder + '.tgz'),
+                  },
+                  [packageFolder],
+                );
 
-            NPM.publishPackage(
-              {
-                metadata: packageJSON,
-                body: fs.createReadStream(
-                  path.join(paths.buildCache, packageFolder + '.tgz'),
-                ),
-              },
-              () => {
-                console.warn(
-                  `PUBLISHED ${packageJSON.name} version ${
-                    packageJSON.version
-                  } 🎉`,
+                NPM.publishPackage(
+                  {
+                    metadata: packageJSON,
+                    body: fs.createReadStream(
+                      path.join(paths.buildCache, packageFolder + '.tgz'),
+                    ),
+                  },
+                  () => {
+                    console.warn(
+                      `PUBLISHED ${packageJSON.name} version ${
+                        packageJSON.version
+                      } 🎉`,
+                    );
+                  },
                 );
               },
             );
-          },
-        );
-      } else {
-        console.warn(
-          `Skipping release of ${
-            packageJSON.name
-          } - there is nothing to release`,
-        );
-      }
-    },
-  );
+          } else {
+            console.warn(
+              `Skipping release of ${
+                packageJSON.name
+              } - there is nothing to release`,
+            );
+          }
+        },
+      );
+    }
+  });
 });
