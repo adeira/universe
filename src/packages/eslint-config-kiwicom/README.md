cb5d992ea02c2e73d6877cd0c16e292caae6ae9a
Martin Zlámal <mrtnzlml@gmail.com>
2018-11-27 15:25:20 +0100

    Create new '@kiwicom/eslint-config' package
    So we can create one really good and share it with other projects...

---
+++ src/packages/eslint-config-kiwicom/README.md
@@ -0,0 +1,1 @@
+Purpose of this package is to share common eslint configuration among other JS projects in our company.
---
+++ src/packages/eslint-config-kiwicom/index.js
@@ -0,0 +1,78 @@
+// @flow
+
+module.exports = {
+  env: {
+    node: true,
+    jest: true,
+  },
+  extends: [
+    'airbnb-base',
+    'eslint:recommended',
+    'prettier',
+    'prettier/react',
+    'plugin:flowtype/recommended',
+    'plugin:jest/recommended',
+  ],
+  plugins: [
+    'import',
+    'jest',
+    'kiwi-graphql',
+    'prettier',
+    'promise',
+    'flowtype',
+  ],
+  rules: {
+    curly: [2, 'all'],
+    camelcase: 1,
+    'no-use-before-define': 0,
+    'no-shadow': 0,
+    'import/prefer-default-export': 0,
+    'import/no-dynamic-require': 0,
+    'import/no-extraneous-dependencies': [
+      'error',
+      {
+        devDependencies: ['**/*.test.js'],
+      },
+    ],
+    'kiwi-graphql/only-nullable-fields': 'error',
+    'prefer-const': 'error',
+    'prettier/prettier': [
+      'error',
+      {
+        bracketSpacing: true,
+        printWidth: 80,
+        singleQuote: true,
+        tabWidth: 2,
+        trailingComma: 'all',
+      },
+    ],
+    'promise/always-return': 'error',
+    'promise/no-return-wrap': 'error',
+    'promise/param-names': 'error',
+    'promise/catch-or-return': 'error',
+    'promise/no-native': 'off',
+    'promise/no-nesting': 'warn',
+    'promise/no-promise-in-callback': 'warn',
+    'promise/no-callback-in-promise': 'warn',
+    'promise/avoid-new': 'off',
+    'promise/prefer-await-to-then': 'error',
+    'promise/prefer-await-to-callbacks': 'off',
+    'flowtype/require-valid-file-annotation': ['error', 'always'],
+    'flowtype/newline-after-flow-annotation': ['error', 'always'],
+    'flowtype/generic-spacing': 'off',
+    'eol-last': 'error',
+    'no-restricted-imports': [
+      'error',
+      {
+        paths: [
+          {
+            name: 'graphql-relay',
+            importNames: ['connectionFromArray'],
+            message:
+              "please import { connectionFromArray } from '/src/common/services/ArrayConnection' instead",
+          },
+        ],
+      },
+    ],
+  },
+};
