e76897279a99fed1bb9777c407eff9beb1ebece4
Martin Zlámal <mrtnzlml@gmail.com>
2018-12-13 14:33:53 -0500

    Add Docusaurus application
    Just a base structure for our future documentation site.

---
+++ src/apps/docusaurus/siteConfig.js
@@ -0,0 +1,71 @@
+// @flow
+
+// See https://docusaurus.io/docs/site-config for all the possible
+// site configuration options.
+
+const siteConfig = {
+  title: 'GraphQL', // Title for your website.
+  tagline: 'Documentation for GraphQL at Kiwi.com',
+  url: 'https://your-docusaurus-test-site.com', // Your website URL
+  baseUrl: '/', // Base URL for your project */
+  // For github.io type URLs, you would set the url and baseUrl like:
+  //   url: 'https://facebook.github.io',
+  //   baseUrl: '/test-site/',
+
+  customDocsPath: 'docusaurus/docs',
+
+  // Used for publishing and more
+  projectName: 'graphql',
+  organizationName: 'kiwicom',
+  // For top-level user or org sites, the organization is still the same.
+  // e.g., for the https://JoelMarcey.github.io site, it would be set like...
+  //   organizationName: 'JoelMarcey'
+
+  // For no header links in the top nav bar -> headerLinks: [],
+  headerLinks: [{ doc: 'directory-structure', label: 'Docs' }],
+
+  // If you have users set above, you add it here:
+  users: [],
+
+  /* path to images for header/footer */
+  headerIcon: 'img/logo-white.png', // TODO: svg
+  footerIcon: 'img/logo-white.png', // TODO: svg
+
+  /* Colors for website */
+  colors: {
+    primaryColor: '#D03195',
+    secondaryColor: '#205C3B',
+  },
+
+  // This copyright info is used in /core/Footer.js and blog RSS/Atom feeds.
+  copyright: `Copyright © ${new Date().getFullYear()} Kiwi.com`,
+
+  highlight: {
+    // Highlight.js theme to use for syntax highlighting in code blocks.
+    theme: 'default',
+  },
+
+  // Add custom scripts here that would be placed in <script> tags.
+  scripts: ['https://buttons.github.io/buttons.js'],
+
+  // On page navigation for the current documentation page.
+  onPageNav: 'separate',
+  // No .html extensions for paths.
+  cleanUrl: true,
+
+  // Open Graph and Twitter card images.
+  ogImage: 'img/logo-white.png',
+  twitterImage: 'img/logo-white.png',
+
+  // Show documentation's last contributor's name.
+  enableUpdateBy: true,
+
+  // Show documentation's last update time.
+  enableUpdateTime: true,
+
+  // You may provide arbitrary config keys to be used as needed by your
+  // template. For example, if you need your repo's URL...
+  //   repoUrl: 'https://github.com/facebook/test-site',
+};
+
+module.exports = siteConfig;
