668b705cbc45892183e39e63b861c6629b76939e
Martin Zlámal <mrtnzlml@gmail.com>
2018-12-14 11:15:56 -0500

    Finish workspace sensitive testing
    I am aware of some edge-cases but overall it works well and I am confident to consider it more or less finished. We still test everything before the build so we should be able to catch everything. There is still a space for improvements but we can improve later.
    
    Closes https://gitlab.skypicker.com/graphql/graphql/issues/389

--- scripts/jest/runTests.js
+++ scripts/jest/runTests.js
@@ -3,12 +3,16 @@
 const execa = require('execa');
 
 let data = '';
-process.stdin.on('data', chunk => {
-  data += chunk;
-});
-process.stdin.on('end', () => {
-  runTests(JSON.parse(JSON.parse(data).data));
-});
+if (process.argv.includes('--all')) {
+  runJest([]); // just run everything
+} else {
+  process.stdin.on('data', chunk => {
+    data += chunk;
+  });
+  process.stdin.on('end', () => {
+    runTests(JSON.parse(JSON.parse(data).data));
+  });
+}
 
 function findRelatedWorkspaces(
   workspaceDependencies,
@@ -50,8 +54,16 @@ function findDirtyWorkspaces(
   return dirtyWorkspaces;
 }
 
-function runJest(config, stdio = 'inherit') {
-  return execa.sync('jest', ['--config=jest.json', ...config], {
+function runJest(config, stdio = 'inherit', timezone = 'UTC') {
+  process.env.TZ = timezone;
+  const jestOptions = ['--config=jest.json', ...config];
+  // eslint-disable-next-line no-console
+  console.warn(
+    `Running tests in TZ=${
+      process.env.TZ
+    } with these options: ${jestOptions.join(' ')}`,
+  );
+  return execa.sync('jest', jestOptions, {
     stdio,
   });
 }
@@ -73,8 +85,14 @@ function runJest(config, stdio = 'inherit') {
  * this script. See: https://github.com/facebook/jest/issues/6062
  */
 function runTests(workspaceDependencies) {
-  // https://jestjs.io/docs/en/cli.html#changedfileswithancestor
+  // TODO:
+  //  This is probably not good enough because it lists only related tests.
+  //  However, there may be changes in non-JS files affecting the tests results
+  //  and Jest is not able to detect this for obvious reasons. It would be better
+  //  to extract the implementation from Jest and use Git here directly. This
+  //  way we can find all related files and not only related test files.
   const changedFilesOutput = runJest(
+    // https://jestjs.io/docs/en/cli.html#changedfileswithancestor
     ['--listTests', '--changedFilesWithAncestor', '--json'],
     'pipe',
   );
@@ -90,7 +108,7 @@ function runTests(workspaceDependencies) {
     dirtyWorkspaces,
   );
 
-  console.warn('RELEVANT TESTS: ', changedTestFiles); // eslint-disable-line no-console
+  // console.warn('RELEVANT TESTS: ', changedTestFiles); // eslint-disable-line no-console
   console.warn('DIRTY WORKSPACES: ', dirtyWorkspaces); // eslint-disable-line no-console
   console.warn('WORKSPACES TO TEST: ', relatedWorkspaces); // eslint-disable-line no-console
 
@@ -100,11 +118,14 @@ function runTests(workspaceDependencies) {
   });
 
   if (pathsToTest.size > 0 || changedTestFiles.length > 0) {
-    runJest(
-      Array.from(pathsToTest)
-        .concat(changedTestFiles) // some tests may be outside of Yarn Workspace
-        .concat(process.argv.slice(2)),
-    );
+    const jestConfig = Array.from(pathsToTest)
+      .concat(process.argv.slice(2))
+      .concat(changedTestFiles); // some tests may be outside of Yarn Workspace
+
+    // we do run the same tests in different timezone to uncover TZ issues
+    runJest(jestConfig, 'inherit', 'UTC');
+    runJest(jestConfig, 'inherit', 'Asia/Tokyo'); // +9
+    runJest(jestConfig, 'inherit', 'America/Lima'); // -5
   }
 }
 
