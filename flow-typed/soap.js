3e972b51de15e4dc256ab4653faecb66e21d762a
Matija Marohnić <matija.marohnic@gmail.com>
2019-01-03 17:40:57 +0100

    Refactor Bedfinder with node-soap
    node-soap seems to be a much better library than strong-soap:
    
    - Collection fields are always reurned as arrays. With strong-soap
    fields like Hotel were a single object if there was only one element,
    so we had to fix that nasty inconsistency manually.
    
    - More predictable request object. I don't have to repeat the
    `GetPackageProductRequest` key when calling `GetPackageProductAsync`.
    
    - It seems to work when built with webpack without any warnings and
    workarounds.
    
    As a bonus I improved Flow types in some places, to avoid typecasting.
    Although I couldn't avoid it when handling error response.

---
+++ flow-typed/soap.js
@@ -0,0 +1,47 @@
+declare module 'soap' {
+  declare type SoapOptions = $ReadOnly<{|
+    attributesKey?: string,
+    disableCache?: boolean,
+    endpoint?: string,
+    envelopeKey?: string,
+    escapeXML?: boolean,
+    forceSoap12Headers?: boolean,
+    httpClient?: any,
+    namespaceArrayElements?: boolean,
+    normalizeNames?: boolean,
+    overridePromiseSuffix?: string,
+    preserveWhitespace?: boolean,
+    request?: any,
+    returnFault?: boolean,
+    suppressStack?: boolean,
+    wsdl_headers?: {},
+    wsdl_options?: {},
+  |}>;
+
+  declare type RawResponse = string;
+  declare type SoapHeader = { [key: string]: any };
+  declare type RawRequest = string;
+  declare type MapResponse = <Request, Response>(
+    Request => Response
+  ) => Request => Promise<[
+    Response,
+    RawResponse,
+    SoapHeader,
+    RawRequest,
+  ]>;
+  declare export type SoapClient<SoapMethods> = $ReadOnly<{|
+    describe(): Object,
+    setEndpoint(url: string): void,
+    lastRequest: string,
+    ...$ObjMap<SoapMethods, MapResponse>,
+  |}>;
+
+  declare type Soap = $ReadOnly<{|
+    createClientAsync<SoapClient>(
+      url: string,
+      options?: SoapOptions,
+    ): Promise<SoapClient>,
+  |}>;
+
+  declare export default Soap;
+};
