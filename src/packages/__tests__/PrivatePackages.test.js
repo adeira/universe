4fa61b97795f44efae65bcaf8557e8b02e1245fb
Martin Zlámal <mrtnzlml@gmail.com>
2019-01-11 15:57:44 -0600

    Make sure package '@kiwicom/environment' will stay private

---
+++ src/packages/__tests__/PrivatePackages.test.js
@@ -0,0 +1,23 @@
+// @flow
+
+import path from 'path';
+import { iterateWorkspacesInPath } from '@kiwicom/monorepo';
+
+const PRIVATE_PACKAGES = [
+  // following package(s) should be never exposed on NPM because they
+  // contain private or project specific code
+  '@kiwicom/environment',
+];
+
+test('all private packages should have private flag set forever', () => {
+  expect.assertions(PRIVATE_PACKAGES.length);
+
+  iterateWorkspacesInPath(path.join(__dirname, '..'), packageJSONLocation => {
+    // $FlowAllowDynamicImport
+    const packageJson = require(packageJSONLocation);
+
+    if (PRIVATE_PACKAGES.includes(packageJson.name)) {
+      expect(packageJson.private).toBe(true);
+    }
+  });
+});
