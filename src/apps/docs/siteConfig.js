70f64a194b261e7a2e71ebb79dcb40cb7c74d49a
Martin Zlámal <mrtnzlml@gmail.com>
2019-01-24 12:52:12 -0600

    Upgrade Docusaurus to the latest version (^1.7.1)
    I also improved the structure a little bit. We have think about this site as a site for monorepo docs and not only GraphQL docs.

--- src/apps/docs/siteConfig.js
+++ src/apps/docs/siteConfig.js
@@ -62,6 +62,8 @@ const siteConfig = {
   // Show documentation's last update time.
   enableUpdateTime: true,
 
+  docsSideNavCollapsible: false,
+
   // You may provide arbitrary config keys to be used as needed by your
   // template. For example, if you need your repo's URL...
   //   repoUrl: 'https://github.com/facebook/test-site',
