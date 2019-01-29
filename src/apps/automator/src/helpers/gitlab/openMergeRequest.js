1209c14c49b2fa37071aeb993428c44e9be6b7cb
Martin Zlámal <mrtnzlml@gmail.com>
2019-01-29 15:37:08 -0600

    Automator: remove dry logs
    I changed my mind about this. Moreover it doesn't work on CI because it's still dev env (not production). It's obvious that I was lazy and I did manual testing here. I should do proper testing next time though so let's remove it.

--- src/apps/automator/src/helpers/gitlab/openMergeRequest.js
+++ src/apps/automator/src/helpers/gitlab/openMergeRequest.js
@@ -3,8 +3,6 @@
 import fetch from '@mrtnzlml/fetch';
 import ENV from '@kiwicom/environment';
 
-import { dryLog } from '../../log';
-
 const PROJECT_ID = 1419; // https://gitlab.skypicker.com/incubator/universe
 
 export default async function openMergeRequest(
@@ -13,26 +11,22 @@ export default async function openMergeRequest(
 ) {
   const apiURL = `https://gitlab.skypicker.com/api/v4/projects/${PROJECT_ID}/merge_requests`;
 
-  if (__DEV__) {
-    dryLog(`Calling API: ${apiURL}`);
-  } else {
-    const response = await fetch(apiURL, {
-      method: 'POST',
-      body: JSON.stringify({
-        id: PROJECT_ID,
-        source_branch: sourceBranch,
-        target_branch: 'master',
-        title: commitMessage,
-        labels: 'automator',
-        remove_source_branch: true,
-      }),
-      headers: {
-        'Private-Token': ENV.AUTOMATOR_GITLAB_PRIVATE_TOKEN,
-        'Content-Type': 'application/json',
-      },
-    });
+  const response = await fetch(apiURL, {
+    method: 'POST',
+    body: JSON.stringify({
+      id: PROJECT_ID,
+      source_branch: sourceBranch,
+      target_branch: 'master',
+      title: commitMessage,
+      labels: 'automator',
+      remove_source_branch: true,
+    }),
+    headers: {
+      'Private-Token': ENV.AUTOMATOR_GITLAB_PRIVATE_TOKEN,
+      'Content-Type': 'application/json',
+    },
+  });
 
-    // eslint-disable-next-line no-console
-    console.warn(await response.json());
-  }
+  // eslint-disable-next-line no-console
+  console.warn(await response.json());
 }
--- src/apps/automator/src/log.js
+++ src/apps/automator/src/log.js
@@ -4,7 +4,3 @@ export default function log(taskIdentifier: string, message: string) {
   // eslint-disable-next-line no-console
   console.log(`${taskIdentifier} ~ ${message}`);
 }
-
-export function dryLog(message: string) {
-  log('DRY RUN', message);
-}
