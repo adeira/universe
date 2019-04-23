7aa50faa60a1d28e8feae7cf4ebfc4510f8ab156
Martin Zlámal <martin.zlamal@kiwi.com>
2019-04-23 15:55:11 -0500

    GraphQL: change fetch headers to comply with KW-RFC-22

--- src/packages/monorepo/src/Git.js
+++ src/packages/monorepo/src/Git.js
@@ -8,7 +8,7 @@ function __parseRows(changes /*: string */) /*: $ReadOnlyArray<string> */ {
   return changes.split(os.EOL).filter(row => row !== '');
 }
 
-function git(args /*: $ReadOnlyArray<string> */): string {
+function git(...args /*: $ReadOnlyArray<string> */): string {
   // TODO: unify with Git implementation from Shipit (?)
   return new ShellCommand(null, 'git', '--no-pager', ...args)
     .runSynchronously()
@@ -18,28 +18,33 @@ function git(args /*: $ReadOnlyArray<string> */): string {
 const Git = {
   __parseRows,
 
+  getHeadRevision(short /*: boolean */): string {
+    const printShort = short ? '--short' : '';
+    return git('rev-parse', printShort, 'HEAD').trim();
+  },
+
   getUntrackedFiles() /*: $ReadOnlyArray<string> */ {
-    const rawUntrackedChanges = git([
+    const rawUntrackedChanges = git(
       'ls-files',
       '--others',
       '--exclude-standard',
-    ]);
+    );
     return __parseRows(rawUntrackedChanges);
   },
 
   // Returns uncommitted (but staged) changes from Git worktree.
   getWorktreeChangedFiles() /*: $ReadOnlyArray<string> */ {
-    const rawWorktreeChanges = git(['diff', '--name-only', 'HEAD']);
+    const rawWorktreeChanges = git('diff', '--name-only', 'HEAD');
     return __parseRows(rawWorktreeChanges);
   },
 
   getChangedFiles() /*: $ReadOnlyArray<string> */ {
-    const rawChanges = git(['diff', '--name-only', 'origin/master...HEAD']);
+    const rawChanges = git('diff', '--name-only', 'origin/master...HEAD');
     return __parseRows(rawChanges);
   },
 
   getLastCommitChanges() /*: $ReadOnlyArray<string> */ {
-    const rawChanges = git(['diff', '--name-only', 'HEAD^', 'HEAD']);
+    const rawChanges = git('diff', '--name-only', 'HEAD^', 'HEAD');
     return __parseRows(rawChanges);
   },
 
