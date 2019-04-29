1754ebbe41a46a65f81288d2cbdae1977585c4e3
Martin Zlámal <martin.zlamal@kiwi.com>
2019-04-29 12:18:10 -0500

    Monorepo Shipit: workaround broken Babel Parser 7.4.4
    Fixes broken Shipit script.
    
    See: https://github.com/babel/babel/issues/9921
    
    Closes: https://gitlab.skypicker.com/incubator/universe/issues/484

--- src/packages/babel-preset-kiwicom/src/__tests__/transformations.test.js
+++ src/packages/babel-preset-kiwicom/src/__tests__/transformations.test.js
@@ -2,21 +2,26 @@
 
 import * as babel from '@babel/core';
 import { generateTestsFromFixtures } from '@kiwicom/test-utils';
+import stripAnsi from 'strip-ansi';
 
 function transform(target) {
   return input => {
-    const oldEnv = process.env.NODE_ENV;
-    process.env.NODE_ENV = 'production';
+    try {
+      const oldEnv = process.env.NODE_ENV;
+      process.env.NODE_ENV = 'production';
 
-    const transformedCode = babel.transform(input, {
-      presets:
-        target === 'js'
-          ? ['@kiwicom/babel-preset-kiwicom'] // keep it here to test the defaults
-          : [['@kiwicom/babel-preset-kiwicom', { target }]], // for any other target
-    }).code;
+      const transformedCode = babel.transform(input, {
+        presets:
+          target === 'js'
+            ? ['@kiwicom/babel-preset-kiwicom'] // keep it here to test the defaults
+            : [['@kiwicom/babel-preset-kiwicom', { target }]], // for any other target
+      }).code;
 
-    process.env.NODE_ENV = oldEnv;
-    return transformedCode;
+      process.env.NODE_ENV = oldEnv;
+      return transformedCode;
+    } catch (error) {
+      throw new Error(stripAnsi(error.message));
+    }
   };
 }
 
