08374ae08d7e8c9f73ae001d13a75fef4c211122
Martin Zlámal <martin.zlamal@kiwi.com>
2019-05-16 16:31:01 -0500

    Babel preset: fix invalid Flow notation
    Still hitting the same Flow issue. See: https://github.com/babel/babel/issues/9810

--- src/packages/babel-preset-kiwicom/src/__tests__/__fixtures__/babel-issue-9810.js
+++ src/packages/babel-preset-kiwicom/src/__tests__/__fixtures__/babel-issue-9810.js
@@ -4,14 +4,19 @@
 
 /*:: type TestType = any; */
 
+// This currently yields invalid Flow code. One workaround is to use regular
+// functions and not arrow functions. See the original issue.
 const executeNodeScript = (
   aaa /*: string */,
   bbb /*: number */,
   ccc /*: any */,
 ) /*: TestType */ => {
-  // This currently yields invalid Flow code. One workaround is to use regular
-  // functions and not arrow functions. See the original issue.
   return [aaa, bbb, ccc];
 };
 
-module.exports = executeNodeScript;
+// This currently yields invalid Flow code.
+const supportsESM = (target: string) /*: boolean %checks */ => {
+  return target === 'js-esm';
+};
+
+module.exports = { executeNodeScript, supportsESM };
