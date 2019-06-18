69cafa66ed204ad94de696f025762dd77e2a8503
Martin Zlámal <martin.zlamal@kiwi.com>
2019-06-18 11:39:21 -0500

    @kiwicom/eslint-config: disable broken 'react/no-unused-state' rule
    See related issue: https://github.com/yannickcr/eslint-plugin-react/issues/1910

---
+++ src/packages/eslint-config-kiwicom/__tests__/__fixtures__/valid-eslint-examples/react/no-unused-state.js
@@ -0,0 +1,37 @@
+// @flow strict
+
+import * as React from 'react'; // eslint-disable-line import/no-extraneous-dependencies
+
+type Props = {|
+  +accessToken?: string,
+  +children: React.Node,
+|};
+
+type State = {|
+  accessToken: ?string,
+|};
+
+const MyContext = React.createContext<State>({
+  accessToken: undefined,
+});
+
+export default class MyProvider extends React.Component<Props, State> {
+  constructor(props: Props) {
+    super(props);
+    this.state = {
+      accessToken: props.accessToken ?? null,
+    };
+  }
+
+  setAccessToken = (accessToken: ?string) => {
+    this.setState({ accessToken });
+  };
+
+  render() {
+    return (
+      <MyContext.Provider value={this.state}>
+        {this.props.children}
+      </MyContext.Provider>
+    );
+  }
+}
