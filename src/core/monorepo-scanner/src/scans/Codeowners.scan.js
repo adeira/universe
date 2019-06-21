cdb184e9b070f86c178f0a85095bb4b19bd8c992
Martin Zlámal <martin.zlamal@kiwi.com>
2019-06-21 11:33:53 -0500

    Add scanner for CODEOWNERS file
    We already did a few mistakes around this file: missing Universe codeowners, non-existing paths, ...

---
+++ src/core/monorepo-scanner/src/scans/Codeowners.scan.js
@@ -0,0 +1,55 @@
+// @flow
+
+import os from 'os';
+import fs from 'fs';
+import path from 'path';
+import { findRootPackageJsonPath } from '@kiwicom/monorepo-utils';
+
+// https://docs.gitlab.com/ee/user/project/code_owners.html
+
+function* iterateRules() {
+  const root = path.dirname(findRootPackageJsonPath());
+  const codeownersPath = path.join(root, 'CODEOWNERS');
+  const codeowners = fs.readFileSync(codeownersPath, 'utf8');
+
+  for (const line of codeowners.split(os.EOL)) {
+    if (line === '') {
+      // empty
+      continue;
+    }
+    if (/^#(?<comment>.*)|^\*/.test(line)) {
+      continue;
+    }
+
+    yield line;
+  }
+}
+
+test('codeowners paths', () => {
+  expect.hasAssertions();
+  const root = path.dirname(findRootPackageJsonPath());
+  for (const line of iterateRules()) {
+    const parsedLine = line.match(/^(?<path>[^\s]+)/);
+    const projectPath = String(parsedLine?.groups?.path);
+    expect({
+      pathExists: fs.existsSync(path.join(root, projectPath)),
+      path: projectPath,
+    }).toEqual({
+      pathExists: true,
+      path: projectPath,
+    });
+  }
+});
+
+test('core maintainers', () => {
+  expect.hasAssertions();
+  const requiredCodeowners = [
+    'martin.zlamal@kiwi.com',
+    'michal.sanger@kiwi.com',
+  ];
+  for (const line of iterateRules()) {
+    for (const codeowner of requiredCodeowners) {
+      expect(line).toMatch(codeowner);
+    }
+  }
+});
