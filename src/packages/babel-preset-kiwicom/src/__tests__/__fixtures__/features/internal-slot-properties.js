94dc649fa24e363cd4c8363fb91a0a823ebf57f9
Martin Zlámal <martin.zlamal@kiwi.com>
2019-06-24 17:03:22 -0500

    Babel preset: add failing test for internal slot properties (Flow)
    See: https://github.com/babel/babel-eslint/pull/777

---
+++ src/packages/babel-preset-kiwicom/src/__tests__/__fixtures__/features/internal-slot-properties.js
@@ -0,0 +1,11 @@
+// @flow strict
+
+// Babel-eslint currently fails on internal slot properties (this file is excluded in .eslintignore).
+// See: https://github.com/babel/babel-eslint/pull/777
+
+type F = {
+  (): string,
+  (x: boolean): string,
+  [[call]](x: number): string,
+  [[call]]: string => string,
+};
