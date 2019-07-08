a80a5384a0edb4aaf2e4c49503622763bbb44ea3
Martin Zlámal <martin.zlamal@kiwi.com>
2019-07-08 13:04:10 +0200

    Monorepo Scanner: remove scans scoping
    There is no need for this annoying optimizations since we now run it in extra CI job and it's very fast. It just made in unnecessarily complicated for development.

--- src/core/monorepo-scanner/.jest.config.js
+++ src/core/monorepo-scanner/.jest.config.js
@@ -2,63 +2,17 @@
 
 require('@babel/register'); // to be able to use non-transpiled '@kiwicom/monorepo-utils' here
 
-const fs = require('fs');
 const path = require('path');
-const assert = require('assert').strict;
-const { findRootPackageJsonPath, Git } = require('@kiwicom/monorepo-utils');
+const { findRootPackageJsonPath } = require('@kiwicom/monorepo-utils');
 
-const changes = Git.getChangesToTest();
 const monorepoSrcRoot = path.join(
   path.dirname(findRootPackageJsonPath()),
   'src',
 );
-const rootDir = path.join(__dirname, 'src', 'scans');
-
-const dependenciesTable = new Map([
-  // when should the check be executed => the check itself
-  [/.+/i, 'Babel.scan.js'],
-  [/(?:package\.json|CHANGELOG\.md)$/i, 'Changelogs.scan.js'],
-  [/(?:package\.json|LICENSE)$/i, 'Licenses.scan.js'],
-  [/(?:package\.json|\.babelrc\.js)$/i, 'NextjsApplications.scan.js'],
-  [/package\.json$/i, 'PrivatePackages.scan.js'],
-  [/\.js$/i, 'TestsWithCorrectName.scan.js'],
-  [/(?:package\.json|LICENSE)$/i, 'Workspaces.scan.js'],
-  [/package\.json$/i, 'WorkspacesDependencies.scan.js'],
-  // TODO: every test should define their own dependencies to scale well (?), also, how to test the scanner itself?
-]);
-
-assert.deepStrictEqual(
-  [...dependenciesTable.values()],
-  fs.readdirSync(rootDir), // every scan should be manually registered here
-);
-
-const testRegexps = [];
-for (const [regexp, newTestRegex] of dependenciesTable) {
-  if (
-    changes.some(changedFile => {
-      const testResult = regexp.test(changedFile);
-      if (testResult) {
-        console.warn(
-          `Matched %s on file '%s' resulting in '%s'`,
-          regexp,
-          changedFile,
-          newTestRegex,
-        );
-      }
-      return testResult;
-    })
-  ) {
-    testRegexps.push(newTestRegex);
-  }
-}
-
-// fallback to test everything if there is no match
-const testRegex = testRegexps.length > 0 ? testRegexps : '.scan.js$';
-console.warn('Value of `testRegex`:', testRegex);
 
 module.exports = {
-  rootDir,
-  testRegex,
+  rootDir: path.join(__dirname, 'src', 'scans'),
+  testRegex: '.scan.js$',
   verbose: true,
   globals: {
     __DEV__: true,
