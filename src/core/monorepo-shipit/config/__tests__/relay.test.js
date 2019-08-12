1803932ab4e8cc99add0f5c8dce06bd614e017ad
Martin Zlámal <mrtnzlml@gmail.com>
2019-08-12 11:43:26 -0500

    Shipit: prepare Relay config for OSS
    In preparation for: https://github.com/kiwicom/relay/pull/1

--- src/core/monorepo-shipit/config/__tests__/relay.test.js
+++ src/core/monorepo-shipit/config/__tests__/relay.test.js
@@ -7,6 +7,16 @@ import testExportedPaths from './testExportedPaths';
 testExportedPaths(path.join(__dirname, '..', 'relay.js'), [
   ['src/packages/relay/package.json', 'package.json'],
   ['src/packages/relay/src/index.js', 'src/index.js'],
+  ['src/packages/relay/.eslintrc.js', undefined],
+  ['src/packages/relay/__github__/.eslintrc.js', '.eslintrc.js'],
+  ['src/packages/relay/__github__/.flowconfig', '.flowconfig'],
+  ['src/packages/relay/__github__/babel.config.js', 'babel.config.js'],
+  ['src/packages/relay/__github__/flow-typed/globals.js', 'flow-typed/globals.js'],
+  [
+    'src/packages/relay/__github__/flow-typed/npm/jest_v24.x.x.js',
+    'flow-typed/npm/jest_v24.x.x.js',
+  ],
+  ['src/packages/relay/__github__/jest.config.js', 'jest.config.js'],
 
   // invalid cases:
   ['src/packages/xyz/outsideScope.js', undefined], // correctly deleted
--- src/core/monorepo-shipit/config/relay.js
+++ src/core/monorepo-shipit/config/relay.js
@@ -7,6 +7,17 @@ module.exports = {
     };
   },
   getPathMappings(): Map<string, string> {
-    return new Map([['src/packages/relay/', '']]);
+    return new Map([
+      ['src/packages/relay/.eslintrc.js', 'STRIPPED'], // our original Eslint config is being removed and replaced with the one from __github__
+      ['src/packages/relay/__github__/flow-typed/', 'flow-typed/'],
+      ['src/packages/relay/__github__/.eslintrc.js', '.eslintrc.js'],
+      ['src/packages/relay/__github__/.flowconfig', '.flowconfig'],
+      ['src/packages/relay/__github__/babel.config.js', 'babel.config.js'],
+      ['src/packages/relay/__github__/jest.config.js', 'jest.config.js'],
+      ['src/packages/relay/', ''],
+    ]);
+  },
+  getStrippedFiles(): Set<RegExp> {
+    return new Set([/__github__/, /^STRIPPED$/]);
   },
 };
