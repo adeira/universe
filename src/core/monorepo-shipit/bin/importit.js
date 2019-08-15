3a8268383e37aababafc2f533bc09088f66aa2c6
Martin Zlámal <mrtnzlml@gmail.com>
2019-08-15 11:22:35 -0500

    Shipit: add project name to the import branch name
    This way the branch name won't collide with other projects (previously we used only PR number which is not enough to make it unique).

--- src/core/monorepo-shipit/bin/importit.js
+++ src/core/monorepo-shipit/bin/importit.js
@@ -10,7 +10,7 @@ import createCheckCorruptedRepoPhase from '../src/phases/createCheckCorruptedRep
 import createCleanPhase from '../src/phases/createCleanPhase';
 import createImportSyncPhase from '../src/phases/createImportSyncPhase';
 
-// TODO: check we can actually import this package
+// TODO: check we can actually import this package (whether we have config for it)
 // yarn monorepo-babel-node src/core/monorepo-shipit/bin/importit.js git@github.com:kiwicom/fetch.git 1
 
 const argv = process.argv.splice(2); // TODO: better CLI
@@ -19,10 +19,15 @@ invariant(argv.length === 2, 'Importit expects two arguments: git URL and PR num
 const exportedRepoURL = argv[0]; // git@github.com:kiwicom/fetch.git
 const pullRequestNumber = argv[1];
 
+const gitRegex = /^git@github.com:(?<packageName>.+)\.git$/;
 invariant(
-  /^git@github.com:.+\.git$/.test(exportedRepoURL),
+  gitRegex.test(exportedRepoURL),
   'We currently support imports only from GitHub.com - please open an issue to add additional services.',
 );
+
+const match = exportedRepoURL.match(gitRegex);
+const packageName = match?.groups?.packageName;
+invariant(packageName != null, 'Cannot figure out package name from: %s', exportedRepoURL);
 
 iterateConfigs(config => {
   if (config.exportedRepoURL === exportedRepoURL) {
@@ -30,7 +35,7 @@ iterateConfigs(config => {
       createClonePhase(config.exportedRepoURL, config.destinationPath),
       createCheckCorruptedRepoPhase(config.destinationPath),
       createCleanPhase(config.destinationPath),
-      createImportSyncPhase(config, pullRequestNumber),
+      createImportSyncPhase(config, packageName, pullRequestNumber),
     ]).forEach(phase => phase());
   }
 });
