9380daf77e4d75250fb9ae3e604f291ce96a513d
Martin Zlámal <mrtnzlml@gmail.com>
2019-08-19 10:44:31 -0500

    Relay example: use different Babel config for OSS
    See: https://github.com/kiwicom/relay-example/pull/3

--- src/incubator/example-relay/next.config.js
+++ src/incubator/example-relay/next.config.js
@@ -6,7 +6,11 @@ const withCustomBabelConfigFile = require('next-plugin-custom-babel-config');
 
 module.exports = withCustomBabelConfigFile(
   withTranspileModules({
-    babelConfigFile: path.join(__dirname, '.babelrc'),
+    babelConfigFile: path.join(
+      __dirname,
+      // @x-shipit-enable: 'babel.config.js',
+      '.babelrc', // @x-shipit-disable
+    ),
     transpileModules: ['@kiwicom'],
   }),
 );
