6fbe48f477c604c00d861dc1d6307d208517a51b
Martin Zlámal <mrtnzlml@gmail.com>
2019-08-21 13:03:51 -0500

    Babel Orbit plugin: add Eslint config for OSS

--- src/core/monorepo-shipit/config/babel-plugin-orbit-components.js
+++ src/core/monorepo-shipit/config/babel-plugin-orbit-components.js
@@ -8,8 +8,9 @@ module.exports = {
   },
   getPathMappings(): Map<string, string> {
     return new Map([
-      ['src/packages/babel-plugin-orbit-components/__github__/.travis.yml', '.travis.yml'],
       ['src/packages/babel-plugin-orbit-components/__github__/.eslintignore', '.eslintignore'],
+      ['src/packages/babel-plugin-orbit-components/__github__/.eslintrc.js', '.eslintrc.js'],
+      ['src/packages/babel-plugin-orbit-components/__github__/.travis.yml', '.travis.yml'],
       ['src/packages/babel-plugin-orbit-components/', ''],
     ]);
   },
