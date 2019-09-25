982f63768af13d12bdb436a29716e005e4dc1975
Jaroslav Kubicek <jaroslav.kubicek@gmail.com>
2019-09-25 23:20:21 +0200

    Relay: improve commitMutation config types to enable usage of generated types

--- src/packages/relay/src/commitMutation.js
+++ src/packages/relay/src/commitMutation.js
@@ -8,26 +8,43 @@ import type {
   DeclarativeMutationConfig,
   RecordSourceSelectorProxy,
   Environment,
+  Uploadables,
 } from './types.flow';
 
 opaque type SelectorData = $FlowFixMe;
 
-type Config = {|
-  +mutation: GraphQLTaggedNode,
+type MutationParameters = {|
+  +response: { +[key: string]: any, ... },
   +variables: Variables,
+  +rawResponse?: { ... },
+|};
+
+type Config<T: MutationParameters> = {|
+  +mutation: GraphQLTaggedNode,
+  +variables: $ElementType<T, 'variables'>,
   // TODO: 2 kinds of errors? What about changing the interface a little bit to make it more obvious?
-  +onCompleted?: ?(response: ?{ +[key: string]: any, ... }, errors: ?$ReadOnlyArray<Error>) => void,
+  +onCompleted?: ?(response: $ElementType<T, 'response'>, errors: ?$ReadOnlyArray<Error>) => void,
   +onError?: ?(error: Error) => void,
-  +optimisticResponse?: { +[key: string]: any, ... },
+  +optimisticResponse?: $ElementType<{| +rawResponse?: { ... }, ...T |}, 'rawResponse'>,
   +optimisticUpdater?: ?(store: RecordSourceSelectorProxy) => void,
   +updater?: ?(store: RecordSourceSelectorProxy, data: SelectorData) => void,
+  +uploadables?: Uploadables,
   +configs?: $ReadOnlyArray<DeclarativeMutationConfig>,
 |};
 
 /**
  * The first parameter `environment` should be from `props.relay.environment`
  * to ensure the mutation is performed in the correct environment.
+ *
+ * Correct usage of generated flow types for "commitMutation" function:
+ *
+ * import type { NamedMutation } from './__generated__/NamedMutation.graphql';
+ *
+ * commitMutation<NamedMutation>(...)
  */
-export default function commitMutation(environment: Environment, config: Config) {
+export default function commitMutation<T: MutationParameters>(
+  environment: Environment,
+  config: Config<T>,
+) {
   return _commitMutation(environment, config);
 }
