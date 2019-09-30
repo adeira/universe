d87dcf432adeb81c5f986f93eb7a6e258ed80a1b
Martin Zlámal <mrtnzlml@gmail.com>
2019-09-30 09:33:12 -0500

    1/n sketchy-null-string: prepare some files before we can enable the lint

--- src/packages/graphql-utils/src/ArrayConnection.js
+++ src/packages/graphql-utils/src/ArrayConnection.js
@@ -20,8 +20,8 @@ export default function connectionFromArray<T>(
   const { edges, pageInfo } = relayConnectionFromArray(data, args);
   const firstCursor = offsetToCursor(0);
   const lastCursor = offsetToCursor(data.length - 1);
-  const lowerBound = cursorToOffset(pageInfo.startCursor || firstCursor);
-  const upperBound = cursorToOffset(pageInfo.endCursor || lastCursor);
+  const lowerBound = cursorToOffset(pageInfo.startCursor ?? firstCursor);
+  const upperBound = cursorToOffset(pageInfo.endCursor ?? lastCursor);
 
   // there are some data "before" current slice
   if (lowerBound > 0) {
