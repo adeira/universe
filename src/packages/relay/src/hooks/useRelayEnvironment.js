d5b02f1ced4192772a309e7225edd6b0a1237657
Martin Zlámal <mrtnzlml@gmail.com>
2019-10-01 15:36:57 -0500

    Relay: improve error message inside 'useRelayEnvironment'

--- src/packages/relay/src/hooks/useRelayEnvironment.js
+++ src/packages/relay/src/hooks/useRelayEnvironment.js
@@ -13,11 +13,11 @@ export default function useRelayEnvironment(): Environment {
   const context = useContext(ReactRelayContext);
   invariant(
     context != null,
-    'useRelayEnvironment: Expected to have found a Relay environment provided by ' +
-      'a `RelayEnvironmentProvider` component. This usually means that useRelayEnvironment ' +
-      'was used in a component that is not a descendant of a `RelayEnvironmentProvider`. ' +
-      'Please make sure a `RelayEnvironmentProvider` has been rendered somewhere ' +
-      'as a parent of ancestor your component.',
+    'useRelayEnvironment: Expected to have found a Relay environment provided by' +
+      'a `RelayEnvironmentProvider` component or `QueryRenderer`. This usually means that' +
+      'useRelayEnvironment was used in a component that is not a descendant of these components. ' +
+      'Please make sure a `RelayEnvironmentProvider` has been rendered somewhere as a parent of ' +
+      'your component. It should be defined only once in the whole application.',
   );
   return context.environment;
 }
