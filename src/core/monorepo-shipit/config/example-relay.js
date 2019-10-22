1bbf6d06c3f3b75a221541aabc6d07e334e56873
Martin Zlámal <mrtnzlml@gmail.com>
2019-10-22 09:24:56 -0500

    Relay example: remove Circle CI config from OSS
    We don't use it anyway and we should try Github Actions instead.

--- src/core/monorepo-shipit/config/example-relay.js
+++ src/core/monorepo-shipit/config/example-relay.js
@@ -8,7 +8,6 @@ module.exports = {
   },
   getPathMappings(): Map<string, string> {
     return new Map([
-      ['src/incubator/example-relay/__github__/.circleci', '.circleci'],
       ['src/incubator/example-relay/__github__/.flowconfig', '.flowconfig'],
       ['src/incubator/example-relay/__github__/babel.config.js', 'babel.config.js'],
       ['src/incubator/example-relay/', ''],
