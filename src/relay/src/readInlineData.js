1ddce47f109d6e7a823d33c41b15203784bbd44e
Trond Bergquist <trond_bergquist@hotmail.com>
2019-11-07 08:18:57 +0100

    Fix readInline flowtype
    closes #37

--- src/relay/src/readInlineData.js
+++ src/relay/src/readInlineData.js
@@ -4,9 +4,12 @@ import { readInlineData as _readInlineData } from 'react-relay';
 
 import type { GraphQLTaggedNode } from './types.flow';
 
-export default function readInlineData(
-  fragment: GraphQLTaggedNode,
-  fragmentRef: GraphQLTaggedNode,
-) {
+opaque type FragmentReference = empty;
+
+export default function readInlineData<
+  TRef: FragmentReference,
+  TData,
+  TKey: ?{ +$data?: TData, +$fragmentRefs: TRef, ... },
+>(fragment: GraphQLTaggedNode, fragmentRef: TKey): ?TData {
   return _readInlineData(fragment, fragmentRef);
 }
