eaccd8b45d2fa943d25cad71a0902d060a975594
Trond Bergquist <trond_bergquist@hotmail.com>
2019-11-08 23:38:01 +0100

    Test that npm packages has private false

--- scripts/publishNPMPackages.js
+++ scripts/publishNPMPackages.js
@@ -5,6 +5,7 @@ import path from 'path';
 import publish from '@adeira/monorepo-npm-publisher';
 import { invariant } from '@adeira/js';
 
+import publishedPackages from './publishedPackages.json';
 // yarn monorepo-babel-node scripts/publishNPMPackages.js
 
 (async () => {
@@ -19,21 +20,6 @@ import { invariant } from '@adeira/js';
     buildCache: path.join(os.tmpdir(), 'com.kiwi.universe.npm', '.build'),
     dryRun: false,
     npmAuthToken,
-    workspaces: new Set([
-      // TODO: throw when trying to whitelist non-existing workspace
-      '@adeira/babel-preset-adeira',
-      '@adeira/fetch',
-      '@adeira/js',
-      '@adeira/logger',
-      '@adeira/monorepo-utils',
-      '@adeira/relay',
-      '@adeira/signed-source',
-      '@adeira/test-utils',
-      '@adeira/flow-bin',
-      '@adeira/eslint-config',
-      '@adeira/graphql-global-id',
-      '@adeira/graphql-bc-checker',
-      'eslint-plugin-adeira',
-    ]),
+    workspaces: new Set(publishedPackages),
   });
 })();
