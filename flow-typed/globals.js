015d95ff1d7b6a93e081fa2b96221b4c4d8c6c3d
Trond Bergquist <trond_bergquist@hotmail.com>
2019-11-08 22:42:16 +0100

    Remove some kiwi related stuff

--- flow-typed/globals.js
+++ flow-typed/globals.js
@@ -1,4 +1,3 @@
-declare var __: string => string; // https://kiwicom.github.io/nitrolib/services.html#intl
 declare var __DEV__: boolean;
 
 // https://github.com/facebook/flow/blob/fa89aadb55ae9bb37c71e14d7274935903d501ce/lib/core.js#L835
--- src/monorepo-utils/bin/monorepo-babel-node-runner.js
+++ src/monorepo-utils/bin/monorepo-babel-node-runner.js
@@ -2,8 +2,8 @@
 
 require('@babel/register')({
   // Babel by default ignores our workspaces when executed from the nested
-  // repository. We need to whitelist our `@kiwicom` scope here.
-  ignore: [/node_modules\/(?!@kiwicom)/], // TODO: and possibly others out of `@kiwicom` scope - see `yarn workspaces info`
+  // repository. We need to whitelist our `@adeira` scope here.
+  ignore: [/node_modules\/(?!@adeira)/], // TODO: and possibly others out of `@adeira` scope - see `yarn workspaces info`
 
   // We need to look upwards for the root babel.config.js because these
   // scripts are being executed from the nested workspace.
