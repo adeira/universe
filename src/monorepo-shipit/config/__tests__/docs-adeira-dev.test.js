f71ab42d3cdc813a417b0c18de79076ccaeb1f40
Martin Zlámal <mrtnzlml@gmail.com>
2019-11-18 18:22:03 +0200

    Docs: setup Shipit
    Closes: https://github.com/adeira/universe/issues/82

---
+++ src/monorepo-shipit/config/__tests__/docs-adeira-dev.test.js
@@ -0,0 +1,16 @@
+// @flow strict-local
+
+import path from 'path';
+
+import testExportedPaths from './testExportedPaths';
+
+testExportedPaths(path.join(__dirname, '..', 'docs-adeira-dev.js'), [
+  ['src/docs/package.json', 'package.json'],
+  ['src/docs/README.md', 'README.md'],
+  ['src/docs/core/Footer.js', 'core/Footer.js'],
+  ['src/docs/docs/general/introduction.md', 'docs/general/introduction.md'],
+
+  // invalid cases:
+  ['src/packages/monorepo/outsideScope.js', undefined], // correctly deleted
+  ['package.json', undefined], // correctly deleted
+]);
