c4c7d37d90dccec47de3982e2beb974151906388
Martin Zlámal <mrtnzlml@gmail.com>
2019-11-23 10:47:03 +0100

    Upgrade Flow to the latest version 0.112.0

--- src/__flowtests__/lints/implicit-inexact-object.js
+++ src/__flowtests__/lints/implicit-inexact-object.js
@@ -1,7 +1,9 @@
 // @flow strict
 
-/* eslint-disable */
+/* eslint-disable flowtype/require-inexact-type */
 
+// This is currently not allowed but it will eventually be the default:
+// flowlint ambiguous-object-type:off
 export type A = { x: number };
 export type B = { x: number, ... };
 export type C = {| x: number |};
