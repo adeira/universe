b34104bef7a104fd9324ec106ddfe236f0ba5c14
Trond Bergquist <trond_bergquist@hotmail.com>
2019-11-24 13:33:21 +0100

    eslint-config: Lint all when .eslintignore changes

--- src/eslint-config-adeira/runner/shouldLintAll.js
+++ src/eslint-config-adeira/runner/shouldLintAll.js
@@ -3,5 +3,5 @@
 export default function shouldLintAll(filename: string): boolean %checks {
   // Eslint configs can be nested (not only the root path).
   // See: https://eslint.org/docs/user-guide/configuring#configuration-file-formats
-  return /\.eslintrc(?:\.(?:js(?:on)?|ya?ml))?$|package.json$/.test(filename);
+  return /\.eslintrc(?:\.(?:js(?:on)?|ya?ml))?$|package.json$|.eslintignore$/.test(filename);
 }
