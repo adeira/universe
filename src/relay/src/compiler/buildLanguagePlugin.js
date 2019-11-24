70aaf3355858fc990c444edf89ec38eaa30d76b5
Martin Zlámal <mrtnzlml@gmail.com>
2019-11-24 18:48:30 +0100

    Relay: expose language plugins interface
    This exposes public interface for changing the language plugin. Currently, people are mostly interested in 'javascript' or 'typescript' languages so I added support only for these two. Javascript is and will stay as a default.

--- src/relay/src/compiler/buildLanguagePlugin.js
+++ src/relay/src/compiler/buildLanguagePlugin.js
@@ -2,15 +2,22 @@
 
 import { FlowGenerator } from 'relay-compiler';
 import { find } from 'relay-compiler/lib/language/javascript/FindGraphQLTags'; // TODO: better (?)
+import TypescriptPluginFactory from 'relay-compiler-language-typescript';
 
 import formatGeneratedModule from './formatGeneratedModule';
 
-export default function buildLanguagePlugin() {
-  return {
-    inputExtensions: ['js', 'jsx'],
-    outputExtension: 'js',
-    typeGenerator: FlowGenerator,
-    formatModule: formatGeneratedModule,
-    findGraphQLTags: find,
-  };
+export default function buildLanguagePlugin(language: 'javascript' | 'typescript') {
+  if (language === 'typescript') {
+    return TypescriptPluginFactory();
+  } else if (language === 'javascript') {
+    return {
+      inputExtensions: ['js', 'jsx'],
+      outputExtension: 'js',
+      typeGenerator: FlowGenerator,
+      formatModule: formatGeneratedModule,
+      findGraphQLTags: find,
+    };
+  }
+
+  throw new Error(`Language '${(language: empty)}' is not supported.`);
 }
