d822687250d1079569daea9cb17c958174a5fb6a
Trond Bergquist <trond_bergquist@hotmail.com>
2019-12-02 20:09:05 +0100

    Fix isTestRegex
    It was mssing escape charactare in regex making positive
    isTest hits for file that was not test

--- src/eslint-plugin-adeira/src/rules/valid-test-folder.js
+++ src/eslint-plugin-adeira/src/rules/valid-test-folder.js
@@ -15,10 +15,10 @@ module.exports = ({
         const fileName = context.getFilename();
         const isTestRegex =
           typeof context.settings.isTestRegex === 'string'
-            ? context.settings.isTestRegex
-            : '(?:spec|test).js$';
+            ? new RegExp(context.settings.isTestRegex)
+            : /\.(?:spec|test)\.js$/;
 
-        if (new RegExp(isTestRegex).test(fileName)) {
+        if (isTestRegex.test(fileName)) {
           const isValidTestPathRegex =
             typeof context.settings.validTestFolderRegex === 'string'
               ? context.settings.validTestFolderRegex
