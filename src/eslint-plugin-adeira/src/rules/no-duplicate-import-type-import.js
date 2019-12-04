22be73a2c622cdea27a39e737498794b5980969c
Trond Bergquist <trond_bergquist@hotmail.com>
2019-12-04 19:28:51 +0100

    Autofix adeira/no-duplicate-import-type-import

--- src/eslint-plugin-adeira/src/rules/no-duplicate-import-type-import.js
+++ src/eslint-plugin-adeira/src/rules/no-duplicate-import-type-import.js
@@ -2,28 +2,72 @@
 
 /*::
 
-import type { EslintRule } from './EslintRule.flow';
+import type { EslintRule, Node } from './EslintRule.flow';
 
 */
 
-// TODO: Implement auto-fixing
+function typeNodeSpecifiersToImport(typeNode) {
+  // Build the import string, it will result in a string like: type Node, type ElementRef
+  // Then caller wraps this accordingly to match it's specific case.
+  return typeNode.specifiers.map(i => `type ${i.imported.name}`).join(',');
+}
+function autoFix(typeNode, valueNode, fixer) {
+  // Each import has an array of specifiers, we want to append our typeimport after the last import
+  const { start, end, type } = valueNode.specifiers[valueNode.specifiers.length - 1];
+  const range = [start, end];
+  let newImport;
+  switch (type) {
+    case 'ImportDefaultSpecifier':
+      // This covers import like `import React from 'react';`
+      newImport = fixer.insertTextAfterRange(
+        range,
+        `, { ${typeNodeSpecifiersToImport(typeNode)} }`,
+        fixer,
+      );
+      break;
+    case 'ImportSpecifier':
+      // This covers imports like `import {graphql, createFragmentContainer } from 'react-relay'`
+      // and `import React, {Component, type Node } from 'react'`
+      newImport = fixer.insertTextAfterRange(range, `, ${typeNodeSpecifiersToImport(typeNode)}`);
+      break;
+    default:
+      // This is a case we don't know about, lets not fix anything
+      return [];
+  }
+  return [newImport, fixer.remove(typeNode)];
+}
+
 module.exports = ({
   meta: {
+    fixable: true,
     docs: {},
     schema: [],
   },
 
   create(context) {
-    const imports /* : Array<{| +name: string, +importKind: string | null |}> */ = [];
+    const imports /* : Array<Node> */ = [];
     return {
       ImportDeclaration: node => {
-        if (imports.some(i => i.name === node.source.value && i.importKind !== node.importKind)) {
+        // An import like `import React, { type Node } from 'react'` has importKind value
+        // An import like `import type { Node } from 'react'` has importKind type;
+        const existingNode = imports.find(
+          i => i.source.value === node.source.value && i.importKind !== node.importKind,
+        );
+        if (existingNode != null) {
           context.report({
             node,
             message: `Found duplicate import/type import ${node.source.value}`,
+            fix: fixer => {
+              // We don't know if user did `import React from 'react'; import type { Node } from 'react'`;
+              // or `import type { Node } from 'react'; import React from 'react';`
+              // Here we identify which import is typeImport and which one is value import
+              const [typeNode, valueNode] =
+                existingNode.importKind === 'type' ? [existingNode, node] : [node, existingNode];
+              return autoFix(typeNode, valueNode, fixer);
+            },
           });
         } else {
-          imports.push({ name: node.source.value, importKind: node.importKind });
+          imports.push(node);
         }
       },
     };
