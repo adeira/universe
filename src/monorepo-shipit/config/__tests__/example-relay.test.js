9c60ff3e3b33e64bfe801c1b500ce4bba1f29ae8
Jaroslav Kubicek <jaroslav.kubicek@gmail.com>
2019-12-11 20:54:47 +0100

    shipit: Remove non-existing ".github" dir from "relay-example" config

--- src/monorepo-shipit/config/__tests__/example-relay.test.js
+++ src/monorepo-shipit/config/__tests__/example-relay.test.js
@@ -12,10 +12,6 @@ testExportedPaths(path.join(__dirname, '..', 'example-relay.js'), [
   ['src/example-relay/__github__/.flowconfig', '.flowconfig'],
   ['src/example-relay/__github__/babel.config.js', 'babel.config.js'],
   ['src/example-relay/__github__/flow-typed/globals.js', 'flow-typed/globals.js'],
-  [
-    'src/example-relay/__github__/.github/workflows/continuous-integration.yml',
-    '.github/workflows/continuous-integration.yml',
-  ],
   ['src/example-relay/.babelrc.js', undefined], // correctly deleted
 
   // invalid cases:
