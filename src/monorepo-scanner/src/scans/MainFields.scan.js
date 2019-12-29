0b95c3438ab7dcc4bd6189c34acfc72150194440
Trond Bergquist <trond_bergquist@hotmail.com>
2019-12-29 11:04:58 +0100

    scanner: Add test to check if main fields file exists
    closes #274

---
+++ src/monorepo-scanner/src/scans/MainFields.scan.js
@@ -0,0 +1,31 @@
+// @flow
+
+import fs from 'fs';
+import path from 'path';
+import { Workspaces } from '@adeira/monorepo-utils';
+
+Workspaces.iterateWorkspaces(packageJSONLocation => {
+  // $FlowAllowDynamicImport
+  const packageJson = require(packageJSONLocation);
+  test(`${packageJson.name}`, () => {
+    const dirname = path.dirname(packageJSONLocation);
+    const main = packageJson.main;
+    if (main != null) {
+      expect(fs.existsSync(path.join(dirname, `${main}.js`))).toGiveHelp(
+        'The file specified in main field does not exist. If this is intentional, you can remove this field from package json',
+      );
+    }
+
+    if (packageJson.module != null && packageJson.module !== false) {
+      expect(fs.existsSync(path.join(dirname, packageJson.module))).toGiveHelp(
+        'The file specified in module field does not exist. If this is intentional, you can remove this field from package json',
+      );
+    }
+
+    if (packageJson.browser != null) {
+      expect(fs.existsSync(path.join(dirname, packageJson.browser))).toGiveHelp(
+        'The file specified in browser field does not exist. If this is intentional, you can remove this field from package json',
+      );
+    }
+  });
+});
