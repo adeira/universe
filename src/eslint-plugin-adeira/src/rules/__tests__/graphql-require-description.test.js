32f04b64050f07b28c302d4fc3885568483c04c4
Jaroslav Kubicek <jaroslav.kubicek@gmail.com>
2020-01-14 20:58:43 +0100

    eslint-plugin: Add new "graphql-require-description" rule

---
+++ src/eslint-plugin-adeira/src/rules/__tests__/graphql-require-description.test.js
@@ -0,0 +1,48 @@
+// @flow
+
+const RuleTester = require('eslint').RuleTester;
+
+const rule = require('../graphql-require-description');
+
+const ruleTester = new RuleTester({
+  parser: require.resolve('babel-eslint'),
+});
+
+ruleTester.run('graphql-require-description', rule, {
+  valid: [
+    `new Date`,
+    `new Pokus({ foo: { baz: 'bar' }})`,
+    `new GraphQLObjectType('Random stuff')`,
+    `
+    new GraphQLObjectType({
+      name: 'TripSegment',
+      description: '"TripSegment" represents single atomic connection between two places in itinerary.',
+      fields: {
+        departure: {
+          type: GraphQLRouteStop,
+          resolve: ({ departure }: TripData): DepartureArrival => departure,
+        },
+      },
+    });
+  `,
+  ],
+
+  invalid: [
+    {
+      code: `
+        new GraphQLObjectType({
+          name: 'TripSegment',
+          fields: {
+            departure: {
+              type: GraphQLRouteStop,
+              resolve: ({ departure }: TripData): DepartureArrival => departure,
+            },
+          },
+        });
+  `,
+      errors: [
+        "Graph type 'TripSegment' name has no description. Every instance of 'GraphQLObjectType' has to include it to keep the graph well documented.",
+      ],
+    },
+  ],
+});
