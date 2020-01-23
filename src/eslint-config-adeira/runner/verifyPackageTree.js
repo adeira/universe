6b2a19808036d07508e54b05aa9dc43d886df172
Trond Bergquist <trond_bergquist@hotmail.com>
2020-01-23 12:18:15 +0100

    eslint: Less strict warning log for dependency
    We should only log when version is less than expected
    or if it is a major version greater than expected.
    Using 7.18.0 when expected is 7.17.0 should not be a problem.

--- src/eslint-config-adeira/runner/verifyPackageTree.js
+++ src/eslint-config-adeira/runner/verifyPackageTree.js
@@ -1,14 +1,14 @@
-// @flow strict
+// @flow
 
 const fs = require('fs');
 const path = require('path');
+const semver = require('semver');
 
 /**
  * Inspiration: https://github.com/facebook/create-react-app/blob/3495286f8f3835d55cbdedbc881ae481eb0a3c65/packages/react-scripts/scripts/utils/verifyPackageTree.js
  * MIT licenced.
  */
 module.exports = function verifyPackageTree() {
-  // $FlowAllowDynamicImport
   const ownPackageJson = require('../package.json');
   const depsToCheck = Object.keys(ownPackageJson.dependencies).filter(dep =>
     dep.startsWith('eslint-plugin'),
@@ -42,7 +42,13 @@ module.exports = function verifyPackageTree() {
       }
       const depPackageJson = JSON.parse(fs.readFileSync(maybeDepPackageJson, 'utf8'));
       const expectedVersion = expectedVersionsByDep[dep];
-      if (depPackageJson.version !== expectedVersion) {
+
+      // I think logging always is to strict, since using 7.18.0 when 7.17.0 is expected should not be a problem.
+      // Using lower version or major versions should be considered problematic
+      if (
+        semver.lt(depPackageJson.version, expectedVersion) ||
+        semver.gte(depPackageJson.version, semver.inc(expectedVersion, 'major'))
+      ) {
         console.warn(
           'There might be a problem with the following plugin: %s (used %s but expected %s)',
           dep,
