df96dad26423b0174c040db9150887d24ca74890
Trond Bergquist <trond_bergquist@hotmail.com>
2020-01-27 10:09:01 +0100

    relay-runtime: Export connectionHandler

--- src/relay-runtime/index.js
+++ src/relay-runtime/index.js
@@ -3,3 +3,5 @@
 export { default as RelayLogger } from './src/RelayLogger';
 
 export type { Variables } from './src/RelayRuntimeTypes';
+
+export { default as ConnectionHandler } from './src/ConnectionHandler';
