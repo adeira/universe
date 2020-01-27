f994f1f18a02f474bdfcce44bff3178fc61b96ca
Trond Bergquist <trond_bergquist@hotmail.com>
2020-01-27 10:09:01 +0100

    relay-runtime: Export connectionHandler

--- src/relay-runtime/src/RelayRuntimeTypes.js
+++ src/relay-runtime/src/RelayRuntimeTypes.js
@@ -3,3 +3,49 @@
 // https://github.com/facebook/relay/blob/master/packages/relay-runtime/util/RelayRuntimeTypes.js
 
 export type Variables = { +[name: string]: $FlowFixMe, ... };
+
+export type DataID = string;
+
+export interface RecordProxy {
+  copyFieldsFrom(source: RecordProxy): void;
+  getDataID(): DataID;
+  getLinkedRecord(name: string, args?: ?Variables): ?RecordProxy;
+  getLinkedRecords(name: string, args?: ?Variables): ?Array<?RecordProxy>;
+  getOrCreateLinkedRecord(name: string, typeName: string, args?: ?Variables): RecordProxy;
+  getType(): string;
+  getValue(name: string, args?: ?Variables): mixed;
+  setLinkedRecord(record: RecordProxy, name: string, args?: ?Variables): RecordProxy;
+  setLinkedRecords(records: Array<?RecordProxy>, name: string, args?: ?Variables): RecordProxy;
+  setValue(value: mixed, name: string, args?: ?Variables): RecordProxy;
+  invalidateRecord(): void;
+}
+
+export interface RecordSourceProxy {
+  create(dataID: DataID, typeName: string): RecordProxy;
+  delete(dataID: DataID): void;
+  get(dataID: DataID): ?RecordProxy;
+  getRoot(): RecordProxy;
+  invalidateStore(): void;
+}
+
+export interface ReadOnlyRecordProxy {
+  getDataID(): DataID;
+  getLinkedRecord(name: string, args?: ?Variables): ?RecordProxy;
+  getLinkedRecords(name: string, args?: ?Variables): ?Array<?RecordProxy>;
+  getType(): string;
+  getValue(name: string, args?: ?Variables): mixed;
+}
+
+export type HandleFieldPayload = {|
+  // The arguments that were fetched.
+  +args: Variables,
+  // The __id of the record containing the source/handle field.
+  +dataID: DataID,
+  // The (storage) key at which the original server data was written.
+  +fieldKey: string,
+  // The name of the handle.
+  +handle: string,
+  // The (storage) key at which the handle's data should be written by the
+  // handler.
+  +handleKey: string,
+|};
