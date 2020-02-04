66c4cd70064ea9081424113061c1e0f26a0e3d46
Martin Zlámal <mrtnzlml@gmail.com>
2020-02-04 08:25:19 +0100

    Relay logger: sync TS definitions
    See: https://github.com/DefinitelyTyped/DefinitelyTyped/pull/41807

--- src/relay-runtime/src/RelayLogger.d.ts
+++ src/relay-runtime/src/RelayLogger.d.ts
@@ -1,61 +1,63 @@
+type OperationAvailability = 'available' | 'stale' | 'missing';
+
 interface LogEventQueryResourceFetch {
-	readonly name: 'queryresource.fetch';
-	readonly operation: unknown;
-	// FetchPolicy from relay-experimental
-	readonly fetchPolicy: string;
-	// RenderPolicy from relay-experimental
-	readonly renderPolicy: string;
-	readonly hasFullQuery: boolean;
-	readonly shouldFetch: boolean;
+  readonly name: 'queryresource.fetch';
+  readonly operation: unknown; // TODO
+  // FetchPolicy from relay-experimental
+  readonly fetchPolicy: string;
+  // RenderPolicy from relay-experimental
+  readonly renderPolicy: string;
+  readonly queryAvailability: OperationAvailability;
+  readonly shouldFetch: boolean;
 }
 
 interface LogEventExecuteInfo {
-	readonly name: 'execute.info';
-	readonly transactionID: number;
-	readonly info: unknown;
+  readonly name: 'execute.info';
+  readonly transactionID: number;
+  readonly info: unknown;
 }
 
 interface LogEventExecuteStart {
-	readonly name: 'execute.start';
-	readonly transactionID: number;
-	readonly params: {
-		// RequestParameters type
-		readonly name: string;
-		readonly operationKind: string;
-		readonly text: string;
-	};
-	readonly variables: object;
+  readonly name: 'execute.start';
+  readonly transactionID: number;
+  readonly params: {
+    // RequestParameters type
+    readonly name: string;
+    readonly operationKind: string;
+    readonly text: string;
+  };
+  readonly variables: object;
 }
 
 interface LogEventExecuteNext {
-	readonly name: 'execute.next';
-	readonly transactionID: number;
-	readonly response: unknown;
+  readonly name: 'execute.next';
+  readonly transactionID: number;
+  readonly response: unknown;
 }
 
 interface LogEventExecuteError {
-	readonly name: 'execute.error';
-	readonly transactionID: number;
-	readonly error: Error;
+  readonly name: 'execute.error';
+  readonly transactionID: number;
+  readonly error: Error;
 }
 
 interface LogEventExecuteComplete {
-	readonly name: 'execute.complete';
-	readonly transactionID: number;
+  readonly name: 'execute.complete';
+  readonly transactionID: number;
 }
 
 interface LogEventExecuteUnsubscribe {
-	readonly name: 'execute.unsubscribe';
-	readonly transactionID: number;
+  readonly name: 'execute.unsubscribe';
+  readonly transactionID: number;
 }
 
 type TransactionalLogEvent =
-	| LogEventExecuteInfo
-	| LogEventExecuteStart
-	| LogEventExecuteNext
-	| LogEventExecuteError
-	| LogEventExecuteComplete
-	| LogEventExecuteUnsubscribe;
+  | LogEventExecuteInfo
+  | LogEventExecuteStart
+  | LogEventExecuteNext
+  | LogEventExecuteError
+  | LogEventExecuteComplete
+  | LogEventExecuteUnsubscribe;
 
 type LogEvent = LogEventQueryResourceFetch | TransactionalLogEvent;
 
