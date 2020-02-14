d04a14987c07cacc0624dca1ffcf5051d85e7a2d
Trond Bergquist <trond_bergquist@hotmail.com>
2020-02-14 10:32:24 +0100

    Upgrade relay to v9
    closes #421

--- src/relay/src/hooks/useRelayEnvironment.js
+++ src/relay/src/hooks/useRelayEnvironment.js
@@ -1,9 +1,20 @@
 // @flow
 
-import { useRelayEnvironment as _useRelayEnvironment } from 'react-relay/hooks';
+import * as React from 'react';
+import { ReactRelayContext } from 'react-relay';
+import { invariant } from '@adeira/js';
 
 import type { Environment } from '../runtimeTypes.flow';
 
 export default function useRelayEnvironment(): Environment {
-  return _useRelayEnvironment();
+  const context = React.useContext(ReactRelayContext);
+  invariant(
+    context != null,
+    'useRelayEnvironment: Expected to have found a Relay environment provided by' +
+      'a `RelayEnvironmentProvider` component or `QueryRenderer`. This usually means that' +
+      'useRelayEnvironment was used in a component that is not a descendant of these components. ' +
+      'Please make sure a `RelayEnvironmentProvider` has been rendered somewhere as a parent of ' +
+      'your component. It should be defined only once in the whole application.',
+  );
+  return context.environment;
 }
