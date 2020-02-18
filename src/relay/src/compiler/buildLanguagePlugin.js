cf1b8f0911cec96fc5a56916ea98b694ddb46451
Martin Zlámal <mrtnzlml@gmail.com>
2020-02-18 08:33:20 +0100

    Revert "Relay: expose language plugins interface"
    We never ended up using it and I think it was a bad idea anyway. This reverts commit 70aaf335
    
    Closes: https://github.com/adeira/universe/pull/427

--- src/relay/src/compiler/buildLanguagePlugin.js
+++ src/relay/src/compiler/buildLanguagePlugin.js
@@ -5,19 +5,12 @@ import { find } from 'relay-compiler/lib/language/javascript/FindGraphQLTags'; /
 
 import formatGeneratedModule from './formatGeneratedModule';
 
-export default function buildLanguagePlugin(language: 'javascript' | 'typescript') {
-  if (language === 'typescript') {
-    const TypescriptPluginFactory = require('relay-compiler-language-typescript').default;
-    return TypescriptPluginFactory();
-  } else if (language === 'javascript') {
-    return {
-      inputExtensions: ['js', 'jsx'],
-      outputExtension: 'js',
-      typeGenerator: FlowGenerator,
-      formatModule: formatGeneratedModule,
-      findGraphQLTags: find,
-    };
-  }
-
-  throw new Error(`Language '${(language: empty)}' is not supported.`);
+export default function buildLanguagePlugin() {
+  return {
+    inputExtensions: ['js', 'jsx'],
+    outputExtension: 'js',
+    typeGenerator: FlowGenerator,
+    formatModule: formatGeneratedModule,
+    findGraphQLTags: find,
+  };
 }
