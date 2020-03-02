255d7eb94eda68a7290f77695403008142968d30
Martin Zlámal <mrtnzlml@gmail.com>
2020-03-02 08:42:06 +0100

    JS: add additional basic TS types

--- src/js/CHANGELOG.md
+++ src/js/CHANGELOG.md
@@ -1,5 +1,9 @@
 # Unreleased
 
+# 1.2.0
+
+- New basic TS types for `isBrowser`, `isNumeric`, `isObject`, `isObjectEmpty` and `sprintf`.
+
 # 1.1.0
 
 - New basic TS types for `invariant`, `warning` and `nullthrows`.
--- src/js/src/index.d.ts
+++ src/js/src/index.d.ts
@@ -1,15 +1,17 @@
 declare module '@adeira/js' {
-	export function invariant(
-		condition: boolean,
-		format: string,
-		...args: ReadonlyArray<mixed>
-	): void;
+  export function invariant(condition: boolean, format: string, ...args: ReadonlyArray<any>): void;
 
-	export function nullthrows<T>(x?: T | null, message?: string): void;
+  export function isBrowser(): boolean;
 
-  export function invariant(
-    condition: boolean,
-    format: string,
-    ...args: ReadonlyArray<mixed>
-  ): void;
+  export function isNumeric(value: any): boolean;
+
+  export function isObject(value: any): boolean;
+
+  export function isObjectEmpty(value: any): boolean;
+
+  export function nullthrows<T>(x?: T | null, message?: string): void;
+
+  export function sprintf(format: string, ...args: ReadonlyArray<any>): void;
+
+  export function warning(condition: boolean, format: string, ...args: ReadonlyArray<any>): void;
 }
