fb74fa30d32ece613b5ffd449d2e84d8fb64cdee
Michal Sanger <michal.sanger@gmail.com>
2020-04-08 19:03:07 +0200

    evaluateGraphQLResolver support non null fields

--- src/test-utils/src/__tests__/evaluateGraphQLResolver.test.js
+++ src/test-utils/src/__tests__/evaluateGraphQLResolver.test.js
@@ -1,17 +1,19 @@
 // @flow
 
 /* eslint-disable adeira/graphql-require-object-description */
+/* eslint-disable adeira/only-nullable-fields */
 
-import { GraphQLObjectType, GraphQLFloat } from 'graphql';
+import { GraphQLObjectType, GraphQLFloat, GraphQLNonNull, GraphQLInt } from 'graphql';
 
 import evaluateGraphQLResolver from '../evaluateGraphQLResolver';
 
-function createType(type: any): GraphQLObjectType {
+function createType(type: any, resolveFn?: () => any): GraphQLObjectType {
   return new GraphQLObjectType({
     name: 'SomeType',
     fields: {
       value: {
         type,
+        resolve: resolveFn ?? undefined,
       },
     },
   });
@@ -28,10 +30,33 @@ describe('evaluateGraphQLResolver', () => {
     );
   });
 
+  it('works with not null graphql scalar', () => {
+    const { value } = createType(new GraphQLNonNull(GraphQLInt)).getFields();
+
+    expect(evaluateGraphQLResolver(value, { value: 42 })).toBe(42);
+    expect(() => evaluateGraphQLResolver(value, { value: null })).toThrowError(
+      'Invalid value null: Expected non-nullable type Int! not to be null',
+    );
+    expect(() => evaluateGraphQLResolver(value, { value: 9999999999 })).toThrowError(
+      'Int cannot represent non 32-bit signed integer value: 9999999999',
+    );
+  });
+
   it('just returns the input on complex type', () => {
     const { value } = createType('ComplexType').getFields();
     const input = { foo: 'bar' };
 
     expect(evaluateGraphQLResolver(value, { value: input })).toStrictEqual(input);
+  });
+
+  it('works with resolve function', () => {
+    const { value } = createType(GraphQLInt, (_source, { param }: any) => {
+      return param;
+    }).getFields();
+
+    expect(evaluateGraphQLResolver(value, {}, { param: 42 })).toBe(42);
+    expect(() => evaluateGraphQLResolver(value, {}, { param: 3.14 })).toThrowError(
+      'Int cannot represent non-integer value: 3.14',
+    );
   });
 });
