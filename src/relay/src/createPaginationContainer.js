bf1e892ff00799c89275cf6a052875e321d41a39
Trond Bergquist <trond_bergquist@hotmail.com>
2020-05-05 09:44:04 +0200

    relay: Use pagination container types from react-relay

--- src/relay/src/createPaginationContainer.js
+++ src/relay/src/createPaginationContainer.js
@@ -1,54 +1,16 @@
 // @flow
 
 import * as React from 'react';
-import { createPaginationContainer as _createPaginationContainer } from 'react-relay';
+import {
+  createPaginationContainer as _createPaginationContainer,
+  type RelayPaginationProp as _RelayPaginationProp,
+} from 'react-relay';
 import { invariant, isObjectEmpty } from '@adeira/js';
+import type { ConnectionConfig } from 'react-relay/ReactRelayPaginationContainer.js.flow';
 
-import type { FragmentSpec, GraphQLTaggedNode, Disposable, $RelayProps } from './types.flow';
-import type { Environment } from './runtimeTypes.flow';
+import type { FragmentSpec, $RelayProps } from './types.flow';
 
-type ConnectionData = {|
-  +edges?: ?Array<any>,
-  +pageInfo?: ?{|
-    +endCursor: ?string,
-    +hasNextPage: boolean,
-    +hasPreviousPage: boolean,
-    +startCursor: ?string,
-  |},
-|};
-
-type ConnectionConfig = {|
-  +direction?: 'backward' | 'forward',
-  +getConnectionFromProps?: (props: {
-    +[key: string]: any,
-    ...,
-  }) => ?ConnectionData,
-  +getFragmentVariables?: (
-    previousVariables: { +[key: string]: any, ... },
-    totalCount: number,
-  ) => { +[key: string]: any, ... },
-  +getVariables: (
-    props: { +[key: string]: any, ... },
-    paginationInfo: {|
-      +count: number,
-      +cursor: ?string,
-    |},
-    fragmentVariables: { +[key: string]: any, ... },
-  ) => { +[key: string]: any, ... },
-  +query: GraphQLTaggedNode,
-|};
-
-export type PaginationRelayProp = {|
-  +environment: Environment,
-  +hasMore: () => boolean,
-  +isLoading: () => boolean,
-  +loadMore: (pageSize: number, callback: ?(error: ?Error) => void) => ?Disposable,
-  +refetchConnection: (
-    totalCount: number,
-    callback: (error: ?Error) => void,
-    refetchVariables: ?{ +[key: string]: any, ... },
-  ) => ?Disposable,
-|};
+export type PaginationRelayProp = _RelayPaginationProp;
 
 export default function createPaginationContainer<
   Props: { ... },
@@ -62,6 +24,5 @@ export default function createPaginationContainer<
     isObjectEmpty(fragmentSpec) === false,
     'Fragment spec of this pagination container factory cannot be empty.',
   );
-  // $FlowFixMe errors after upgrading to relay 9.1.0
   return _createPaginationContainer(Component, fragmentSpec, connectionConfig);
 }
