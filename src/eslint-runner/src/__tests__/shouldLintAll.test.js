6a12e6efbd9efd5f892e864956e483b995ab003e
Trond Bergquist <trond_bergquist@hotmail.com>
2020-05-29 11:52:04 +0200

    eslint-runner: Improve lint all logic
    - Only lint all if dependency is removed or eslintConfig field is changed.
    
    The motivation for this improvement is that it is unnecessary to lint
    all files when doing changes that do not relate to eslint.
    Like upgrading a dependency or adding a script.
    What is relevant for eslint is removal of dependency. You need
    to check that the dependency is not used in the code.
    If package.json contains eslintConfig field that changed,
    we also need to lint all files

--- src/eslint-runner/src/__tests__/shouldLintAll.test.js
+++ src/eslint-runner/src/__tests__/shouldLintAll.test.js
@@ -1,6 +1,17 @@
-// @flow strict
+// @flow
+
+import * as utils from '@adeira/monorepo-utils';
 
 import shouldLintAll from '../shouldLintAll';
+
+jest.mock('@adeira/monorepo-utils', () => ({
+  findMonorepoRoot: () => '../../../',
+  ShellCommand: jest.fn(),
+}));
+
+beforeEach(() => {
+  jest.resetModules();
+});
 
 test.each([
   '.eslintrc',
@@ -10,12 +21,97 @@ test.each([
   '.eslintrc.json',
   'src/packages/relay/.eslintrc.js',
   '/Users/TEST/universe/src/packages/relay/.eslintrc.js',
-  'package.json',
-  'src/packages/relay/package.json',
-  '/Users/code/universe/src/packages/relay/package.json',
   '.eslintignore',
 ])('filename "%s" IS eslint config file', filename => {
   expect(shouldLintAll(filename)).toBe(true);
+});
+
+it('should lint all for package.json if dependency is removed', () => {
+  jest.spyOn(utils, 'ShellCommand').mockImplementationOnce(() => ({
+    runSynchronously: () => ({
+      getStdout: () => JSON.stringify({ dependencies: { a: 'lol', b: 'lol2' } }),
+    }),
+  }));
+  jest.doMock('../../../../package.json', () => ({
+    dependencies: { a: 'lol' },
+  }));
+  expect(shouldLintAll('package.json')).toBe(true);
+});
+
+it('should not lint all for package.json if dependency is added', () => {
+  jest.spyOn(utils, 'ShellCommand').mockImplementationOnce(() => ({
+    runSynchronously: () => ({
+      getStdout: () => JSON.stringify({ dependencies: { a: 'lol', b: 'lol2' } }),
+    }),
+  }));
+  jest.doMock('../../../../package.json', () => ({
+    dependencies: { a: 'lol', b: 'lol2', c: 'lol3' },
+  }));
+
+  expect(shouldLintAll('package.json')).toBe(false);
+});
+
+it('should not lint all for package.json if dependency is upgraded', () => {
+  jest.spyOn(utils, 'ShellCommand').mockImplementationOnce(() => ({
+    runSynchronously: () => ({
+      getStdout: () => JSON.stringify({ dependencies: { a: 'lol', b: 'lol2' } }),
+    }),
+  }));
+  jest.doMock('../../../../package.json', () => ({
+    dependencies: { a: 'lol', b: 'lol3' },
+  }));
+
+  expect(shouldLintAll('package.json')).toBe(false);
+});
+
+it('should lint all if package.json has eslintConfig field that changed', () => {
+  jest.spyOn(utils, 'ShellCommand').mockImplementationOnce(() => ({
+    runSynchronously: () => ({
+      getStdout: () =>
+        JSON.stringify({
+          dependencies: { a: 'lol', b: 'lol2' },
+          eslintConfig: { rules: { 'no-lol': 1 } },
+        }),
+    }),
+  }));
+  jest.doMock('../../../../package.json', () => ({
+    dependencies: { a: 'lol', b: 'lol2' },
+    eslintConfig: { rules: { 'no-lol': 2 } },
+  }));
+  expect(shouldLintAll('package.json')).toBe(true);
+});
+
+it('should not lint all if package.json has eslintConfig field that did not change', () => {
+  jest.spyOn(utils, 'ShellCommand').mockImplementationOnce(() => ({
+    runSynchronously: () => ({
+      getStdout: () =>
+        JSON.stringify({
+          dependencies: { a: 'lol', b: 'lol2' },
+          eslintConfig: { rules: { 'no-lol': 1 } },
+        }),
+    }),
+  }));
+  jest.doMock('../../../../package.json', () => ({
+    dependencies: { a: 'lol', b: 'lol2' },
+    eslintConfig: { rules: { 'no-lol': 1 } },
+  }));
+  expect(shouldLintAll('package.json')).toBe(false);
+});
+
+it('should lint all if eslintConfig was added to package.json', () => {
+  jest.spyOn(utils, 'ShellCommand').mockImplementationOnce(() => ({
+    runSynchronously: () => ({
+      getStdout: () =>
+        JSON.stringify({
+          dependencies: { a: 'lol', b: 'lol2' },
+        }),
+    }),
+  }));
+  jest.doMock('../../../../package.json', () => ({
+    dependencies: { a: 'lol', b: 'lol2' },
+    eslintConfig: { rules: { 'no-lol': 2 } },
+  }));
+  expect(shouldLintAll('package.json')).toBe(true);
 });
 
 test.each([
--- src/eslint-runner/src/shouldLintAll.js
+++ src/eslint-runner/src/shouldLintAll.js
@@ -1,7 +1,81 @@
-// @flow strict
+// @flow
 
-export default function shouldLintAll(filename: string): boolean %checks {
+import { ShellCommand, findMonorepoRoot } from '@adeira/monorepo-utils';
+import path from 'path';
+import deepEqual from 'deep-equal';
+
+type KeyValue = {
+  [key: string]: string,
+  ...,
+};
+type PackageJson = {
+  dependencies?: KeyValue,
+  devDependencies?: KeyValue,
+  peerDependencies?: KeyValue,
+  eslintConfig?: KeyValue,
+  ...
+};
+
+const getDependencies = (packageJson: PackageJson) => {
+  const set = new Set();
+  const dependencyKeys = ['dependencies', 'devDependencies', 'peerDependencies'];
+  for (const key of dependencyKeys) {
+    if (packageJson[key] != null) {
+      for (const dependency of Object.keys(packageJson[key])) {
+        set.add(dependency);
+      }
+    }
+  }
+  return set;
+};
+
+function diffDependencies(filename: string): boolean {
+  try {
+    const headFile = JSON.parse(
+      new ShellCommand(findMonorepoRoot(), 'git', 'show', `master:${filename}`)
+        .runSynchronously()
+        .getStdout(),
+    );
+    const headFileDependencies = Array.from(getDependencies(headFile));
+
+    // $FlowAllowDynamicImport
+    const currentBranchFile = require(path.join(findMonorepoRoot(), filename));
+    const currentBranchDependencies = Array.from(getDependencies(currentBranchFile));
+    let eslintConfigChanged = false;
+
+    if (currentBranchFile.eslintConfig != null) {
+      eslintConfigChanged = !deepEqual(headFile.eslintConfig, currentBranchFile.eslintConfig);
+    }
+
+    // We only need to lint all if a dependency is removed or eslintConfig field is changed
+    return (
+      headFileDependencies.some(dependency => !currentBranchDependencies.includes(dependency)) ||
+      eslintConfigChanged
+    );
+  } catch {
+    // Return fallback true in case something should fail, file is not in head etc.
+    return true;
+  }
+}
+
+// This function is being called multiple times pr file per run, so lets cache the files
+const shouldLintAllCache = new Map<string, boolean>();
+const { NODE_ENV } = process.env;
+
+export default function shouldLintAll(filename: string): boolean {
+  if (shouldLintAllCache.has(filename) && NODE_ENV !== 'test') {
+    // $FlowExpectedError: We know this key exists, but flow does not
+    return shouldLintAllCache.get(filename);
+  }
+
+  if (/package\.json$/.test(filename)) {
+    const lintAll = diffDependencies(filename);
+    shouldLintAllCache.set(filename, lintAll);
+    return lintAll;
+  }
   // Eslint configs can be nested (not only the root path).
   // See: https://eslint.org/docs/user-guide/configuring#configuration-file-formats
-  return /\.eslintrc(?:\.(?:js(?:on)?|ya?ml))?$|package\.json$|^\.eslintignore$/.test(filename);
+  const lintAll = /\.eslintrc(?:\.(?:js(?:on)?|ya?ml))?$|^\.eslintignore$/.test(filename);
+  shouldLintAllCache.set(filename, lintAll);
+  return lintAll;
 }
