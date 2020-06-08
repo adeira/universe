83eb3575f679161166af4277da0721477ad24599
Jaroslav Kubicek <jaroslav.kubicek@gmail.com>
2020-06-09 00:50:09 +0200

    docs: Migrate to Docusaurus v2.x

---
+++ src/docs/docusaurus.config.js
@@ -0,0 +1,58 @@
+// @flow strict
+
+module.exports = {
+  url: 'https://adeira.dev/',
+  baseUrl: '/',
+  favicon: 'img/favicons/favicon.ico',
+  title: 'adeira/universe', // Title for your website.
+  tagline: 'Universe of libs to write better code.',
+  // Add custom scripts here that would be placed in <script> tags.
+  scripts: ['https://buttons.github.io/buttons.js'],
+  // Used for publishing and more
+  projectName: 'adeira.dev',
+  organizationName: 'adeira',
+  themeConfig: {
+    // You may provide arbitrary config keys to be used as needed by your
+    // template. For example, if you need your repo's URL...
+    //   repoUrl: 'https://github.com/facebook/test-site',
+    // algolia: { ... },
+    image: 'img/logo-transparent.png',
+    sidebarCollapsible: true,
+    navbar: {
+      title: 'Adeira',
+      logo: {
+        src: 'img/logo-transparent.png',
+        alt: 'Adeira.dev',
+      },
+      links: [
+        { to: 'docs/general/introduction', label: 'Docs', position: 'left' },
+        {
+          href: 'https://github.com/adeira/universe',
+          label: 'GitHub',
+          position: 'right',
+        },
+      ],
+    },
+    footer: {
+      // This copyright info is used in /core/Footer.js and blog RSS/Atom feeds.
+      copyright: `Copyright © 2017 - 2019 Kiwi.com, 2019 - ${new Date().getFullYear()} Adeira`,
+    },
+  },
+  presets: [
+    [
+      '@docusaurus/preset-classic',
+      {
+        theme: {
+          customCss: require.resolve('./static/css/custom.css'),
+        },
+        docs: {
+          editUrl: 'https://github.com/adeira/universe/tree/master/src/docs/',
+          showLastUpdateTime: true,
+          showLastUpdateAuthor: true,
+          sidebarPath: require.resolve('./sidebars.js'),
+        },
+        scrollToTop: true,
+      },
+    ],
+  ],
+};
