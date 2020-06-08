3be3513c8133e6a177244f8912d0376372171c6a
Trond Bergquist <trond_bergquist@hotmail.com>
2020-06-08 16:14:36 +0200

    Add @adeira/graphql-relay
    This is a fork from graphql/graphql-relay-js.
    We chose to for this, since PR's are no longer
    being merged and no release has been done since
    Jan 2019

---
+++ src/graphql-relay/src/node/__tests__/nodeasync.test.js
@@ -0,0 +1,106 @@
+/**
+ * Copyright (c) 2015-present, Facebook, Inc.
+ *
+ * This source code is licensed under the MIT license found in the
+ * LICENSE file in the root directory of this source tree.
+ *
+ * @flow
+ */
+
+/* eslint-disable adeira/only-nullable-fields */
+
+import {
+  GraphQLID,
+  GraphQLNonNull,
+  GraphQLObjectType,
+  GraphQLSchema,
+  GraphQLString,
+  graphql,
+} from 'graphql';
+
+import { nodeDefinitions } from '../node';
+
+const userData = {
+  '1': {
+    id: 1,
+    name: 'John Doe',
+  },
+  '2': {
+    id: 2,
+    name: 'Jane Smith',
+  },
+};
+
+const { nodeField, nodeInterface } = nodeDefinitions(
+  (id) => {
+    return userData[id];
+  },
+  () => {
+    return userType;
+  },
+);
+
+const userType = new GraphQLObjectType({
+  name: 'User',
+  description: 'test',
+  interfaces: [nodeInterface],
+  fields: () => ({
+    id: {
+      type: new GraphQLNonNull(GraphQLID),
+    },
+    name: {
+      type: GraphQLString,
+    },
+  }),
+});
+
+const queryType = new GraphQLObjectType({
+  name: 'Query',
+  description: 'test',
+  fields: () => ({
+    node: nodeField,
+  }),
+});
+
+const schema = new GraphQLSchema({
+  query: queryType,
+  types: [userType],
+});
+
+describe('Node interface and fields with async object fetcher', () => {
+  it('gets the correct ID for users', async () => {
+    const query = `{
+      node(id: "1") {
+        id
+      }
+    }`;
+
+    expect(await graphql(schema, query)).toEqual({
+      data: {
+        node: {
+          id: '1',
+        },
+      },
+    });
+  });
+
+  it('gets the correct name for users', async () => {
+    const query = `{
+      node(id: "1") {
+        id
+        ... on User {
+          name
+        }
+      }
+    }`;
+
+    expect(await graphql(schema, query)).toEqual({
+      data: {
+        node: {
+          id: '1',
+          name: 'John Doe',
+        },
+      },
+    });
+  });
+});
---
+++ src/graphql-relay/src/test-helpers/starWarsData.js
@@ -0,0 +1,113 @@
+/**
+ * Copyright (c) 2015-present, Facebook, Inc.
+ *
+ * This source code is licensed under the MIT license found in the
+ * LICENSE file in the root directory of this source tree.
+ * @flow
+ */
+
+/**
+ * This defines a basic set of data for our Star Wars Schema.
+ *
+ * This data is hard coded for the sake of the demo, but you could imagine
+ * fetching this data from a backend service rather than from hardcoded
+ * JSON objects in a more complex demo.
+ */
+
+const xwing = {
+  id: '1',
+  name: 'X-Wing',
+};
+
+const ywing = {
+  id: '2',
+  name: 'Y-Wing',
+};
+
+const awing = {
+  id: '3',
+  name: 'A-Wing',
+};
+
+// Yeah, technically it's Corellian. But it flew in the service of the rebels,
+// so for the purposes of this demo it's a rebel ship.
+const falcon = {
+  id: '4',
+  name: 'Millenium Falcon',
+};
+
+const homeOne = {
+  id: '5',
+  name: 'Home One',
+};
+
+const tieFighter = {
+  id: '6',
+  name: 'TIE Fighter',
+};
+
+const tieInterceptor = {
+  id: '7',
+  name: 'TIE Interceptor',
+};
+
+const executor = {
+  id: '8',
+  name: 'Executor',
+};
+
+const rebels = {
+  id: '1',
+  name: 'Alliance to Restore the Republic',
+  ships: ['1', '2', '3', '4', '5'],
+};
+
+const empire = {
+  id: '2',
+  name: 'Galactic Empire',
+  ships: ['6', '7', '8'],
+};
+
+const data = {
+  Faction: {
+    '1': rebels,
+    '2': empire,
+  },
+  Ship: {
+    '1': xwing,
+    '2': ywing,
+    '3': awing,
+    '4': falcon,
+    '5': homeOne,
+    '6': tieFighter,
+    '7': tieInterceptor,
+    '8': executor,
+  },
+};
+
+let nextShip = 9;
+export function createShip(shipName: string, factionId: string) {
+  const newShip = {
+    id: String(nextShip++),
+    name: shipName,
+  };
+  data.Ship[newShip.id] = newShip;
+  data.Faction[factionId].ships.push(newShip.id);
+  return newShip;
+}
+
+export function getShip(id: string) {
+  return data.Ship[id];
+}
+
+export function getFaction(id: string) {
+  return data.Faction[id];
+}
+
+export function getRebels() {
+  return rebels;
+}
+
+export function getEmpire() {
+  return empire;
+}
