c90f177d66bbe526927471948add98687973f1cc
Trond Bergquist <trond_bergquist@hotmail.com>
2020-07-15 08:49:04 +0200

    Remove handler provider from createEnvironment
    It is better to use the one provided from relay
    by default. The consumer can still add an override

--- src/relay/src/createEnvironment.js
+++ src/relay/src/createEnvironment.js
@@ -1,12 +1,7 @@
 // @flow
 
 import { RelayLogger } from '@adeira/relay-runtime';
-import {
-  Network,
-  Environment as RelayEnvironment,
-  ConnectionHandler,
-  type OperationLoader,
-} from 'relay-runtime';
+import { Network, Environment as RelayEnvironment, type OperationLoader } from 'relay-runtime';
 
 import createRequestHandler from './createRequestHandler';
 import createRelayStore from './createRelayStore';
@@ -29,18 +24,9 @@ function createNetwork(
   return Network.create(fetch, subscribeFn);
 }
 
-function handlerProvider(handle) {
-  // ViewerHandler missing on purpose (it's deprecated?)
-  if (handle === 'connection') {
-    return ConnectionHandler;
-  }
-  throw new Error(`handlerProvider: No handler provided for ${handle}`);
-}
-
 export default function createEnvironment(options: Options): Environment {
   const { fetchFn, subscribeFn, records, gcReleaseBufferSize, ...rest } = options;
   return new RelayEnvironment({
-    handlerProvider,
     network: createNetwork(fetchFn, subscribeFn),
     log: RelayLogger,
     store: createRelayStore(records, {
