20d0a8feb0659bab2b6b9d5ea4dd3d932fc989c2
Trond Bergquist <trond_bergquist@hotmail.com>
2020-07-31 15:56:25 +0200

    js: Check getOwnPropertyNames also to determine if object is empty

--- src/js/src/__tests__/isObjectEmpty.test.js
+++ src/js/src/__tests__/isObjectEmpty.test.js
@@ -6,7 +6,13 @@ it('is an empty object', () => {
   expect(isObjectEmpty({})).toBe(true);
 });
 
-test.each([{ a: 1 }, new Date(), '', true, null, undefined, new RegExp(/(?:)/)])(
+const obj = {};
+Object.defineProperty(obj, 'test', {
+  enumerable: false,
+  value: 'test',
+});
+
+test.each([{ a: 1 }, new Date(), '', true, null, undefined, new RegExp(/(?:)/), obj])(
   'is NOT an empty object: %p',
   (input) => {
     expect(isObjectEmpty(input)).toBe(false);
