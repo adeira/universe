a28d1e099326f4ef9c3c43421b6b79ebe749eec1
Trond Bergquist <trond_bergquist@hotmail.com>
2020-07-31 15:56:25 +0200

    js: Check getOwnPropertyNames also to determine if object is empty

--- src/js/src/isObjectEmpty.js
+++ src/js/src/isObjectEmpty.js
@@ -4,5 +4,11 @@ import isObject from './isObject';
 
 export default function isObjectEmpty(obj: mixed): boolean %checks {
   // https://stackoverflow.com/a/32108184/3135248
-  return isObject(obj) && Object.entries(obj).length === 0 && obj.constructor === Object;
+
+  return (
+    isObject(obj) &&
+    Object.entries(obj).length === 0 &&
+    Object.getOwnPropertyNames(obj).length === 0 &&
+    obj.constructor === Object
+  );
 }
