380de3b9a3269cc9664636e8af575da26ac6ebcd
Jaroslav Kubicek <jaroslav.kubicek@gmail.com>
2020-08-03 13:43:11 +0200

    relay: Improve type definitions of "fetchQuery" function

--- src/relay/src/fetchQuery.js
+++ src/relay/src/fetchQuery.js
@@ -1,16 +1,17 @@
 // @flow
 
 import { fetchQuery as relayFetchQuery } from 'react-relay';
-import type { Variables, GraphQLTaggedNode } from '@adeira/relay-runtime';
+import type { OperationType } from 'relay-runtime';
+import type { GraphQLTaggedNode } from '@adeira/relay-runtime';
 
 import type { Environment } from './runtimeTypes.flow';
 
 // https://relay.dev/docs/en/fetch-query
-export default function fetchQuery(
+export default function fetchQuery<T: OperationType>(
   environment: Environment,
   query: GraphQLTaggedNode,
-  variables: Variables,
+  variables: $PropertyType<T, 'variables'>,
   // TODO: cacheConfig (?)
-): Promise<mixed> {
+): Promise<$PropertyType<T, 'response'>> {
   return relayFetchQuery(environment, query, variables);
 }
