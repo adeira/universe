2f4f919db96651f4a2377c6f5a0a29fdea5e1bb4
Martin Zlámal <mrtnzlml@gmail.com>
2020-08-12 14:15:43 +0200

    Add KochkaIcon.svg

--- project-a/src/Home.js
+++ project-a/src/Home.js
@@ -1,35 +1,14 @@
 import React from 'react';
-import fbt from 'fbt';
 
-import logo from './react.svg';
 import './Home.css';
+import KochkaIcon from './KochkaIcon.svg';
 
-class Home extends React.Component {
+export default class Home extends React.Component {
   render() {
     return (
       <div className="Home">
-        <div className="Home-header">
-          <img src={logo} className="Home-logo" alt="logo" />
-          <h2><fbt desc="Home title">Welcome to Razzle</fbt></h2>
-        </div>
-        <p className="Home-intro">
-          To get started, edit <code>src/App.js</code> or{' '}
-          <code>src/Home.js</code> and save to reload.
-        </p>
-        <ul className="Home-resources">
-          <li>
-            <a href="https://github.com/jaredpalmer/razzle">Docs</a>
-          </li>
-          <li>
-            <a href="https://github.com/jaredpalmer/razzle/issues">Issues</a>
-          </li>
-          <li>
-            <a href="https://palmer.chat">Community Slack</a>
-          </li>
-        </ul>
+        <img src={KochkaIcon} className="Home-logo" alt="logo" />
       </div>
     );
   }
 }
-
-export default Home;
