37c3b07c6047c5917b4cf8d34cf7d37249624ed7
Martin Zlámal <mrtnzlml@gmail.com>
2020-08-16 11:07:00 +0200

    Add language switch

--- project-a/pages/index.js
+++ project-a/pages/index.js
@@ -4,15 +4,15 @@ import React from 'react';
 
 // import './Home.css';
 // import KochkaIcon from './KochkaIcon.svg';
-import Rules from './Rules';
+import Rules from '../src/Rules';
 
 export default class Home extends React.Component {
   render() {
     return (
-      <div className="Home">
+      <>
         {/* <img src={KochkaIcon} className="Home-logo" alt="logo" /> */}
         <Rules />
-      </div>
+      </>
     );
   }
 }
