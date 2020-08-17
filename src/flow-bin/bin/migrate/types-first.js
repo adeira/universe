47979a78e718bf90c6364be5b8e9fcffddb44bd5
Martin Zlámal <mrtnzlml@gmail.com>
2020-08-17 13:52:30 +0200

    Flow: fix types-first migration command
    I incorrectly changed the command in af7c259283d1d1cc2aa16ceeb69ced2f485300c9 (it accepts the whole folders and not files). It worked as well but it was too slow.

--- src/flow-bin/bin/migrate/types-first.js
+++ src/flow-bin/bin/migrate/types-first.js
@@ -1,26 +1,19 @@
 // @flow
 
-/* eslint-disable no-console */
-
 import { ShellCommand } from '@adeira/monorepo-utils';
-
-import walk from './utils/walk';
 
 // https://flow.org/en/docs/cli/annotate-exports/
 export default function typesFirst(flowPath: string, typesFirstPath: string) {
-  walk(typesFirstPath, (file) => {
-    console.log('🔷 %s', file);
-    new ShellCommand(
-      null,
-      flowPath,
-      'codemod',
-      'annotate-exports',
-      '--write',
-      '--repeat',
-      '--log-level=info',
-      file,
-    )
-      .setOutputToScreen()
-      .runSynchronously();
-  });
+  new ShellCommand(
+    null,
+    flowPath,
+    'codemod',
+    'annotate-exports',
+    '--write',
+    '--repeat',
+    '--log-level=info',
+    typesFirstPath,
+  )
+    .setOutputToScreen()
+    .runSynchronously();
 }
