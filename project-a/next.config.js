9745850f5c1f1e00c5e62513409363bb747016e6
Martin Zlámal <mrtnzlml@gmail.com>
2020-08-18 12:47:58 +0200

    Create /rules page

---
+++ project-a/next.config.js
@@ -0,0 +1,4 @@
+module.exports = {
+  poweredByHeader: false,
+  reactStrictMode: true,
+};
--- project-a/pages/index.js
+++ project-a/pages/index.js
@@ -2,17 +2,10 @@
 
 import * as React from 'react';
 
-// import './Home.css';
 // import KochkaIcon from './KochkaIcon.svg';
-import Rules from '../src/Rules';
 
 export default class Home extends React.Component<{||}> {
   render(): React.Node {
-    return (
-      <>
-        {/* <img src={KochkaIcon} className="Home-logo" alt="logo" /> */}
-        <Rules />
-      </>
-    );
+    return <>{/* <img src={KochkaIcon} className="Home-logo" alt="logo" /> */}</>;
   }
 }
