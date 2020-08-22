016d4f659b0ceb5c1f2f34600760e7f35f755c91
Martin Zlámal <mrtnzlml@gmail.com>
2020-08-22 12:22:14 +0200

    SX: normalize color values

--- src/sx/src/__tests__/transformValue.test.js
+++ src/sx/src/__tests__/transformValue.test.js
@@ -3,14 +3,21 @@
 import transformValue from '../transformValue';
 
 test.each`
-  styleName     | styleValue | expectedValue
-  ${'width'}    | ${150}     | ${'150px'}
-  ${'height'}   | ${150}     | ${'150px'}
-  ${'zIndex'}   | ${10}      | ${'10'}
-  ${'fontSize'} | ${8}       | ${'0.5rem'}
-  ${'fontSize'} | ${16}      | ${'1rem'}
-  ${'fontSize'} | ${24}      | ${'1.5rem'}
-  ${'fontSize'} | ${32}      | ${'2rem'}
+  styleName     | styleValue          | expectedValue
+  ${'border'}   | ${'1px solid #Ab3'} | ${'1px solid #Ab3'}
+  ${'width'}    | ${150}              | ${'150px'}
+  ${'height'}   | ${150}              | ${'150px'}
+  ${'zIndex'}   | ${10}               | ${'10'}
+  ${'fontSize'} | ${8}                | ${'0.5rem'}
+  ${'fontSize'} | ${16}               | ${'1rem'}
+  ${'fontSize'} | ${24}               | ${'1.5rem'}
+  ${'fontSize'} | ${32}               | ${'2rem'}
+  ${'color'}    | ${'#123456'}        | ${'#123456'}
+  ${'color'}    | ${'#123ABC'}        | ${'#123abc'}
+  ${'color'}    | ${'#ffffff'}        | ${'#fff'}
+  ${'color'}    | ${'#fffff0'}        | ${'#fffff0'}
+  ${'color'}    | ${'#ABCDEF'}        | ${'#abcdef'}
+  ${'content'}  | ${'🦕'}             | ${'"🦕"'}
 `(
   '$styleName:$styleValue => $styleName:$expectedValue',
   ({ styleName, styleValue, expectedValue }) => {
--- src/sx/src/transformValue.js
+++ src/sx/src/transformValue.js
@@ -2,20 +2,27 @@
 
 const isUnitlessNumber = require('./css-properties/isUnitlessNumber');
 
-// https://engineering.fb.com/web/facebook-com-accessibility/
 export default function transformValue(
   styleName: string,
   styleValue?: string | number = '',
 ): string {
-  // TODO:
-  //  - simplify #ffffff to #fff
-  //  - postcss + autoprefixer (?)
   if (styleName === 'fontSize') {
-    const defaultFontSize = 16; // we expect `font-size: 16px;` to be set by default
+    // px -> rem (we expect `font-size: 16px;` to be set by default)
+    // https://engineering.fb.com/web/facebook-com-accessibility/
+    const defaultFontSize = 16;
     return `${Number(styleValue) / defaultFontSize}rem`;
-  }
-  if (typeof styleValue === 'number') {
+  } else if (styleName === 'content') {
+    // content style is the only one (?) which requires quoted value
+    return `"${styleValue}"`;
+  } else if (typeof styleValue === 'string' && /^#[0-9a-f]{3,6}$/i.test(styleValue)) {
+    // color normalization (to reduce duplicates in the atomic result)
+    return styleValue
+      .toLowerCase() //
+      .replace(/^#(?<char>[0-9a-f])\1{5}$/, (m) => m.substring(0, 4)); // #ffffff -> #fff
+  } else if (typeof styleValue === 'number') {
+    // 42 -> 42px (unless it's unit-less property like zIndex)
     return isUnitlessNumber[styleName] ? `${styleValue}` : `${styleValue}px`;
   }
+
   return `${styleValue}`;
 }
