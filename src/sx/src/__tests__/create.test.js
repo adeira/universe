f94557efd20936711cf7ad2551a4a876f2896d28
Martin Zlámal <mrtnzlml@gmail.com>
2020-08-22 17:06:14 +0200

    SX: add validations of incorrect usage to improve DX

--- src/sx/src/__tests__/create.test.js
+++ src/sx/src/__tests__/create.test.js
@@ -3,8 +3,7 @@
 import create from '../create';
 
 it('returns sx function', () => {
-  // TODO: disallow empty styles and calling non-existing stylesheets
-  expect(create({ aaa: {} })).toBeInstanceOf(Function);
+  expect(create({ aaa: { color: 'red' } })).toBeInstanceOf(Function);
 });
 
 it('returns correct style names', () => {
@@ -82,4 +81,25 @@ it('supports multiple styles', () => {
       blue: { color: 'blue' },
     })('blue', 'red'),
   ).toMatchInlineSnapshot(`"wUqnh _2FdJlr"`); // red + zIndex
+});
+
+it('validates incorrect usage', () => {
+  expect(() => create({})).toThrowErrorMatchingInlineSnapshot(
+    `"Function 'sx.create' cannot be called with empty stylesheet definition."`,
+  );
+  expect(() => create({ aaa: {} })).toThrowErrorMatchingInlineSnapshot(
+    `"Stylesheet 'aaa' must have at least one CSS property."`,
+  );
+
+  // $FlowExpectedError[prop-missing] ccc
+  const styles = create({
+    aaa: { color: 'red' },
+    bbb: { color: 'blue' },
+  });
+  expect(() => styles()).toThrowErrorMatchingInlineSnapshot(
+    `"SX must be called with at least one stylesheet name."`,
+  );
+  expect(() => styles('ccc')).toThrowErrorMatchingInlineSnapshot(
+    `"SX was called with 'ccc' stylesheet name but it doesn't exist. Available names are: [\\"aaa\\",\\"bbb\\"]"`,
+  );
 });
