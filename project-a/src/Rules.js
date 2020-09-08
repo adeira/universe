1dc45526884c152d7c5e0e7570c5795676dd072b
Martin Zlámal <mrtnzlml@gmail.com>
2020-09-08 18:52:30 +0200

    Draft design of the subpages

--- project-a/src/Rules.js
+++ project-a/src/Rules.js
@@ -1,14 +1,15 @@
 // @flow
 
 import * as React from 'react';
+import * as sx from '@adeira/sx';
 import fbt from 'fbt';
 
 import Emoji from './Emoji';
 
 export default function Rules(): React.Node {
   return (
-    <ol>
-      <li>
+    <ol className={styles('orderedList')}>
+      <li className={styles('listItem')}>
         <fbt desc="Rule number 1">
           Be careful when entering the café so that the cats do not run away.{' '}
           <fbt:param name="pleading face emoji">
@@ -16,18 +17,18 @@ export default function Rules(): React.Node {
           </fbt:param>
         </fbt>
       </li>
-      <li>
+      <li className={styles('listItem')}>
         <fbt desc="Rule number 2">
           Feel free to play with the cats and have fun. But please, respect their space and do not
           force them to do things they do not want, such as pulling their tails, whiskers, etc.
         </fbt>
       </li>
-      <li>
+      <li className={styles('listItem')}>
         <fbt desc="Rule number 3">
           Try not to wake up our cats or interrupt them while they are having a snack.
         </fbt>
       </li>
-      <li>
+      <li className={styles('listItem')}>
         <fbt desc="Rule number 4">
           We encourage you to take pictures.{' '}
           <fbt:param name="thumbs up emoji">
@@ -37,33 +38,33 @@ export default function Rules(): React.Node {
           <fbt:param name="instagram account">{'@tktk'}</fbt:param>.
         </fbt>
       </li>
-      <li>
+      <li className={styles('listItem')}>
         <fbt desc="Rule number 5">
           Our cats have a special healthy food which you can buy from us. Human food might be
           dangerous for them. Please don’t feed them with your food.
         </fbt>
       </li>
-      <li>
+      <li className={styles('listItem')}>
         <fbt desc="Rule number 6">
           Cats are very sensitive. Be careful with loud and sudden noises, which could scare them.
         </fbt>
       </li>
-      <li>
+      <li className={styles('listItem')}>
         <fbt desc="Rule number 7">Leave your pets at home. Other pets are not allowed here.</fbt>
       </li>
-      <li>
+      <li className={styles('listItem')}>
         <fbt desc="Rule number 8">
           We are not responsible for any injuries caused by our cats - they are very playful, and
           they do not mean it. Pay special attention to your kids. Feel free to ask for medical aid
           when needed.
         </fbt>
       </li>
-      <li>
+      <li className={styles('listItem')}>
         <fbt desc="Rule number 9">
           Hygiene is important. Wash your hand when playing with our cats.
         </fbt>
       </li>
-      <li>
+      <li className={styles('listItem')}>
         <fbt desc="Rule number 10">
           Please, follow our rules. Misbehaving guests will be asked to leave.
         </fbt>
@@ -71,3 +72,12 @@ export default function Rules(): React.Node {
     </ol>
   );
 }
+
+const styles = sx.create({
+  orderedList: {
+    listStyleType: 'upper-roman',
+  },
+  listItem: {
+    marginBottom: 10,
+  },
+});
