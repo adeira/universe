c1ebd156669267766b76c1da7cc035c8f88c47dd
Martin Zlámal <mrtnzlml@gmail.com>
2020-09-09 15:30:39 +0200

    Add custom 404

---
+++ project-a/pages/404.js
@@ -0,0 +1,9 @@
+// @flow strict
+
+import * as React from 'react';
+
+import Heading from '../src/components/Heading';
+
+export default function Custom404(): React.Node {
+  return <Heading>404 - Page Not Found</Heading>;
+}
--- project-a/src/Homepage.js
+++ project-a/src/Homepage.js
@@ -4,20 +4,26 @@ import * as React from 'react';
 import * as sx from '@adeira/sx';
 
 import { Logo } from './Logo';
+import HomepageNavigation from './HomepageNavigation';
 
 export default function Homepage(): React.Node {
   return (
-    <div className={styles('root')}>
-      <div className={styles('center')}>
-        <Logo />
-      </div>
-      <div className={styles('bottomInfo')}>
-        {/* <p>TODO: contact (?)</p> */}
-        {/* <p>TODO: social networks</p> */}
-        <div>Monday - Friday 8am - 6pm</div>
-        <a href="https://goo.gl/maps/2jh2w78yFTPEDSrS6">
-          Av. Coyoacán 2000, Xoco, Benito Juárez, 03330 Ciudad de México, CDMX, Mexico
-        </a>
+    <div className={styles('background')}>
+      <div className={styles('backgroundOverlay')}>
+        <HomepageNavigation />
+        <div className={styles('root')}>
+          <div className={styles('center')}>
+            <Logo />
+          </div>
+          <div className={styles('bottomInfo')}>
+            {/* <p>TODO: contact (?)</p> */}
+            {/* <p>TODO: social networks</p> */}
+            <div>Monday - Friday 8am - 6pm</div>
+            <a href="https://goo.gl/maps/2jh2w78yFTPEDSrS6">
+              Av. Coyoacán 2000, Xoco, Benito Juárez, 03330 Ciudad de México, CDMX, Mexico
+            </a>
+          </div>
+        </div>
       </div>
     </div>
   );
@@ -43,5 +49,15 @@ const styles = sx.create({
     display: 'flex',
     flexDirection: 'column',
     alignItems: 'center',
+  },
+  background: {
+    backgroundImage: 'url(coffee-background.jpg)',
+    backgroundSize: 'cover',
+  },
+  backgroundOverlay: {
+    display: 'flex',
+    flexDirection: 'column',
+    minHeight: '100vh',
+    backgroundColor: 'var(--main-bg-color-transparent)',
   },
 });
