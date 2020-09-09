ad92fc07f40b0255a53a62a625f281dc8d25229d
Martin Zlámal <mrtnzlml@gmail.com>
2020-09-09 15:30:39 +0200

    Add custom 404

--- project-a/pages/index.js
+++ project-a/pages/index.js
@@ -1,31 +1,9 @@
 // @flow
 
 import * as React from 'react';
-import * as sx from '@adeira/sx';
 
-import Homepage from '../src/Homepage';
-import HomepageNavigation from '../src/HomepageNavigation';
+import IndexPageLang from './[lang]/index';
 
 export default function IndexPage(): React.Node {
-  return (
-    <div className={styles('background')}>
-      <div className={styles('backgroundOverlay')}>
-        <HomepageNavigation />
-        <Homepage />
-      </div>
-    </div>
-  );
+  return <IndexPageLang />;
 }
-
-const styles = sx.create({
-  background: {
-    backgroundImage: 'url(coffee-background.jpg)',
-    backgroundSize: 'cover',
-  },
-  backgroundOverlay: {
-    display: 'flex',
-    flexDirection: 'column',
-    minHeight: '100vh',
-    backgroundColor: 'var(--main-bg-color-transparent)',
-  },
-});
