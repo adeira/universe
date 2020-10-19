0c0932d719bb6f4add03207bd199f0d0fad1641d
Martin Zlámal <mrtnzlml@gmail.com>
2020-10-19 17:17:40 -0500

    SX: improve generated types for colors
    I also extracted and added some tests for the 'collectTypes' utility.

--- src/sx/src/css-properties/generateFlowTypes.js
+++ src/sx/src/css-properties/generateFlowTypes.js
@@ -3,11 +3,12 @@
 import fs from 'fs';
 import path from 'path';
 import prettier from 'prettier';
-import { warning } from '@adeira/js';
 import SignedSource from '@adeira/signed-source';
 import mdnData from 'mdn-data';
-import { definitionSyntax } from 'css-tree';
 import * as changeCase from 'change-case';
+
+import collectTypes from './generator/collectTypes';
+import { FLOW_TYPE_NUMBER, FLOW_TYPE_STRING } from './generator/flowTypes';
 
 const allProperties = new Set();
 const sourceJSON = path.join(__dirname, 'all-properties.en.json');
@@ -16,60 +17,6 @@ const allPropertiesRaw = require(sourceJSON);
 for (const rawProperty of allPropertiesRaw) {
   // the properties are duplicated in the list
   allProperties.add(rawProperty.property);
-}
-
-const FLOW_TYPE_NUMBER = Symbol.for('number');
-const FLOW_TYPE_STRING = Symbol.for('string');
-
-let panicCounter = 1;
-function collectTypes(syntax) {
-  const flowTypes = new Set();
-  try {
-    const ast = definitionSyntax.parse(syntax);
-    definitionSyntax.walk(ast, function enter(node) {
-      if (node.type === 'Keyword') {
-        flowTypes.add(node.name);
-      } else if (node.type === 'Type') {
-        if (['length', 'length-percentage'].includes(node.name)) {
-          // Length can be Flow type of a number (unitless number converted to PX) or with some
-          // explicit unit (therefore string), see: https://developer.mozilla.org/en-US/docs/Web/CSS/length
-          flowTypes.add(FLOW_TYPE_NUMBER);
-          flowTypes.add(FLOW_TYPE_STRING);
-        } else if (['integer', 'number'].includes(node.name)) {
-          flowTypes.add(FLOW_TYPE_NUMBER);
-        } else if (
-          ['angle', 'angle-percentage', 'percentage', 'ratio', 'time'].includes(node.name)
-        ) {
-          flowTypes.add(FLOW_TYPE_STRING);
-        } else {
-          throw node;
-        }
-      } else if (node.type === 'Group') {
-        if (node.combinator !== '|' && node.combinator !== ' ') {
-          throw node;
-        }
-      } else if (node.type === 'Multiplier') {
-        if (node.comma === true) {
-          // multiplier "#" implies the resulting Flow type must be a string becuase the values can repeat
-          flowTypes.add(FLOW_TYPE_STRING);
-        } else {
-          throw node;
-        }
-      } else {
-        throw node;
-      }
-    });
-    return flowTypes;
-  } catch (node) {
-    warning(
-      false,
-      'Panicking, unsupported property syntax (%s):\n\t%s\n\t%j',
-      panicCounter++,
-      syntax,
-      node,
-    );
-    return null;
-  }
 }
 
 let flowPrint = '';
