a7de46d2c925c67672b96c74932502366dfed295
Martin Zlámal <mrtnzlml@gmail.com>
2020-10-29 12:42:39 -0600

    Shipit: refactor no-missing-tests.test.js

--- src/monorepo-shipit/config/__tests__/no-missing-tests.test.js
+++ src/monorepo-shipit/config/__tests__/no-missing-tests.test.js
@@ -4,25 +4,25 @@ import fs from 'fs';
 import path from 'path';
 import { globSync } from '@adeira/monorepo-utils';
 
-test('there is a test file for every config file', () => {
-  const configFilenames = globSync('/**/*.js', {
-    root: path.join(__dirname, '..'),
-    ignore: [
-      '**/node_modules/**',
-      '**/__[a-z]*__/**', // ignore __tests__, __mocks__, ...
-    ],
-  });
-  configFilenames.forEach((configFilename) => {
-    const testFilename = configFilename.replace(
-      /^(?<path>.+?)(?<file>[^/]+)\.js$/,
-      `$1__tests__${path.sep}$2.test.js`,
-    );
-    expect({
-      isOK: fs.existsSync(testFilename),
-      testFilename,
-    }).toEqual({
-      isOK: true,
-      testFilename,
-    });
+const configFilenames = globSync('/**/*.js', {
+  root: path.join(__dirname, '..'),
+  ignore: [
+    '**/node_modules/**',
+    '**/__[a-z]*__/**', // ignore __tests__, __mocks__, ...
+  ],
+});
+
+test.each(configFilenames)('config %s has a test file', (configFilename) => {
+  const testFilename = configFilename.replace(
+    /^(?<path>.+?)(?<file>[^/]+)\.js$/,
+    `$1__tests__${path.sep}$2.test.js`,
+  );
+
+  expect({
+    isOK: fs.existsSync(testFilename),
+    testFilename,
+  }).toEqual({
+    isOK: true,
+    testFilename,
   });
 });
