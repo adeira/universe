a5a2bc93e565248ebb3e60bc79195930f9e6f874
Martin Zlámal <mrtnzlml@gmail.com>
2020-11-03 17:21:49 -0600

    CSS colors: improve rgba() and hsla() detection
    I added a support for whitespace only syntaxes to be closer to the formal syntax. All the changes are supported with tests (based on https://developer.mozilla.org/en-US/docs/Web/CSS/color_value#Examples). Please note: I made it purposefully more benevolent (it's possible to mix numbers and percentages).

--- src/css-colors/src/isColor.js
+++ src/css-colors/src/isColor.js
@@ -1,8 +1,65 @@
 // @flow strict
 
-// https://developer.mozilla.org/en-US/docs/Web/CSS/color_value
 import cssColorNames from './cssColorNames';
 
+const _optional = (arr) => {
+  return ['(?:', ...arr, ')?'].join('');
+};
+
+const _or = (arr) => {
+  return ['(?:', arr.join('|'), ')'].join('');
+};
+
+const _spaceOrComma = _or(['\\s', ',\\s?']);
+
+// https://developer.mozilla.org/en-US/docs/Web/CSS/number
+export const _number = '[+-]?\\d*(?:\\.\\d+)?(?:e[+-]?\\d+)?';
+// https://developer.mozilla.org/en-US/docs/Web/CSS/percentage
+const _percentage = `${_number}%`;
+// https://developer.mozilla.org/en-US/docs/Web/CSS/angle
+const _angle = `${_number}(?:deg|grad|rad|turn)?`;
+
+// https://developer.mozilla.org/en-US/docs/Web/CSS/color_value#Syntax
+function isRGBA(value: string): boolean %checks {
+  return new RegExp(
+    [
+      '^rgba?\\(',
+      _or([_number, _percentage]), // red
+      _spaceOrComma,
+      _or([_number, _percentage]), // green
+      _spaceOrComma,
+      _or([_number, _percentage]), // blue
+      _optional([
+        _or(['\\s?/\\s?', ',\\s?']), // " / " or comma
+        _or([_number, _percentage]), // alpha
+      ]),
+      '\\)$',
+    ].join(''),
+    'i',
+  ).test(value);
+}
+
+// https://developer.mozilla.org/en-US/docs/Web/CSS/color_value#Syntax
+function isHSLA(value: string): boolean %checks {
+  return new RegExp(
+    [
+      '^hsla?\\(',
+      _angle, // hue
+      _spaceOrComma,
+      _or([_number, _percentage]), // saturation
+      _spaceOrComma,
+      _or([_number, _percentage]), // lightness
+      _optional([
+        _or(['\\s?/\\s?', ',\\s?']), // " / " or comma
+        _or([_number, _percentage]), // alpha
+      ]),
+      '\\)$',
+    ].join(''),
+    'i',
+  ).test(value);
+}
+
+// https://developer.mozilla.org/en-US/docs/Web/CSS/color_value
 export default function isColor(value: string): boolean {
   if (typeof value !== 'string') {
     return false;
@@ -16,11 +73,7 @@ export default function isColor(value: string): boolean {
     /^#[0-9a-f]{4}$/i.test(value) || // RGB hexadecimal shorthand (with alpha)
     /^#[0-9a-f]{6}$/i.test(value) || // RGB hexadecimal full
     /^#[0-9a-f]{8}$/i.test(value) || // RGB hexadecimal full (with alpha)
-    // RGB[A] functional (simplified):
-    /^rgba?\(\s*[0-9.]+%?\s*,\s*[0-9.]+%?\s*,\s*[0-9.]+%?\s*(?:,\s*[0-9.]+%?\s*)?\)$/i.test(
-      value,
-    ) ||
-    // HSL[A] functional (simplified):
-    /^hsla?\(\s*.+\s*,\s*[0-9.]+%\s*,\s*[0-9.]+%\s*(?:,\s*[0-9.]+%?\s*)?\)$/i.test(value)
+    isRGBA(value) || // RGB[A] functional
+    isHSLA(value) // HSL[A] functional
   );
 }
