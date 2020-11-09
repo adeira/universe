6c23b70e45f8780ba7833c5a2ba3b35970f426f9
Michal Sanger <michal.sanger@gmail.com>
2020-11-09 16:40:25 +0100

    SXT Babel: Support for custom font family
    Just a first step toward a full `tailwind.config.js` support:
    https://tailwindcss.com/docs/configuration#theme

--- src/sx-tailwind-website/src/pages/_document.js
+++ src/sx-tailwind-website/src/pages/_document.js
@@ -20,6 +20,10 @@ export default class MyDocument extends Document {
       <html lang="en-US">
         <Head>
           <link href="https://unpkg.com/tailwindcss@1.8.10/dist/base.css" rel="stylesheet" />
+          <link
+            href="https://fonts.googleapis.com/css?family=Inter:100,200,300,400,500,600,700,800,900"
+            rel="stylesheet"
+          />
         </Head>
         <body>
           <Main />
