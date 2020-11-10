e1333fb570608fb9451e26038450981e828dc464
Martin Zlámal <mrtnzlml@gmail.com>
2020-11-10 15:20:13 -0600

    Babel SXT: use snapshot testing
    Just a proposal. It is a bit cleaner in terms of configuration but might be less readable when you want to study the output code.

--- src/babel-plugin-transform-sx-tailwind/src/__tests__/fixtures/no-sx-used/output.js
+++
@@ -1,9 +0,0 @@
-// @flow
-import type { Element } from 'react';
-export default function Example(): Element<'button'> {
-  return (
-    <button className="my-button" type="button">
-      Button
-    </button>
-  );
-}
