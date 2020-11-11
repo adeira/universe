00c24aca4ae5d0d35704b21435317b86a3268e31
Martin Zlámal <mrtnzlml@gmail.com>
2020-11-11 09:28:23 -0600

    CSS colors: add simple 'hex3ToHex6'
    As an opposite of already existing `hex6ToHex3`. The original function doesn't have any validation so I didn't do it here either.

--- src/css-colors/src/index.js
+++ src/css-colors/src/index.js
@@ -1,6 +1,7 @@
 // @flow strict
 
 export { default as cssColorNames } from './cssColorNames';
+export { default as hex3ToHex6 } from './hex3ToHex6';
 export { default as hex6ToHex3 } from './hex6ToHex3';
 export { default as isColor } from './isColor';
 export { default as normalizeColor } from './normalizeColor';
