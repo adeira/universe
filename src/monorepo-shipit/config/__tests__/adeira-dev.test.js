0aa4813414bd5524e32a8fad59b02379137fb39b
Trond Bergquist <trond_bergquist@hotmail.com>
2020-11-14 12:51:03 +0100

    adeira.dev: Add babel.config.js to adeira.dev project
    The build was failing after we added the new JSX transform.
    This should fix the failing build

--- src/monorepo-shipit/config/__tests__/adeira-dev.test.js
+++ src/monorepo-shipit/config/__tests__/adeira-dev.test.js
@@ -8,6 +8,7 @@ testExportedPaths(path.join(__dirname, '..', 'adeira-dev.js'), [
   ['src/adeira.dev/package.json', 'package.json'],
   ['src/adeira.dev/README.md', 'README.md'],
   ['src/adeira.dev/docs/general/introduction.md', 'docs/general/introduction.md'],
+  ['src/adeira.dev/__github__/babel.config.js', 'babel.config.js'],
 
   // invalid cases:
   ['src/packages/monorepo/outsideScope.js', undefined], // correctly deleted
@@ -16,4 +17,5 @@ testExportedPaths(path.join(__dirname, '..', 'adeira-dev.js'), [
   ['src/adeira.dev/WORKSPACE.bazel', undefined], // correctly deleted
   ['src/adeira.dev/WORKSPACE', undefined], // correctly deleted
   ['package.json', undefined], // correctly deleted
+  ['src/adeira.dev/.babelrc.js', undefined], // correctly deleted
 ]);
