41ffaf6e61800e9be77fc86ea7d3d8a3647c7b5f
Martin Zlámal <mrtnzlml@gmail.com>
2020-12-13 11:34:02 -0600

    YCR: implement dataloader for Card content

--- src/ya-comiste-rust/sdui/src/graphql_context.rs
+++ src/ya-comiste-rust/sdui/src/graphql_context.rs
@@ -1,6 +1,9 @@
+use crate::model::component_content::ContentDataloaderType;
+
 #[derive(Clone)]
 pub struct Context {
     pub pool: arangodb::ConnectionPool,
+    pub content_dataloader: ContentDataloaderType,
 }
 
 impl juniper::Context for Context {}
