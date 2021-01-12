1942930d4c43d0a43ae91e2d2424d24ef3f7581a
Martin Zlámal <mrtnzlml@gmail.com>
2021-01-11 20:24:36 -0600

    Add shop/product subpage

---
+++ project-a/pages/shop/[product_id].js
@@ -0,0 +1,37 @@
+// @flow
+
+import * as React from 'react';
+import fbt from 'fbt';
+import sx from '@adeira/sx';
+
+import Heading from '../../src/design/Heading';
+import Layout from '../../src/Layout';
+
+export default function ShopProductPage(): React.Node {
+  return (
+    <Layout withFullWidth={true} title={<fbt desc="TODO">TODO</fbt>} withHiddenTitle={true}>
+      <div className={styles('layout')}>
+        <div className={styles('image')}>
+          <Heading>Product Title TODO</Heading>
+        </div>
+        <div>
+          <p>Product description TODO.</p>
+          <button type="submit">Add to Cart</button>
+        </div>
+      </div>
+    </Layout>
+  );
+}
+
+const styles = sx.create({
+  layout: {
+    display: 'grid',
+    gridTemplateColumns: '1fr 1fr',
+    gap: '1rem',
+  },
+  image: {
+    width: '50vw',
+    height: '50vw',
+    backgroundColor: '#ddd',
+  },
+});
