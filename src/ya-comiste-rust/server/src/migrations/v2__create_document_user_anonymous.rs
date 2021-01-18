eebb778ab5db78ef413bc2ab712ef53f8d319778
Martin Zlámal <mrtnzlml@gmail.com>
2021-01-18 13:30:14 -0600

    YCR migrations: add idempotency key to the migration structs

--- src/ya-comiste-rust/server/src/migrations/v2__create_document_user_anonymous.rs
+++ src/ya-comiste-rust/server/src/migrations/v2__create_document_user_anonymous.rs
@@ -1,10 +1,16 @@
-use crate::migrations::utils::create_document;
+use crate::migrations::utils::{create_document, ArangoDocument};
 use arangors::ClientError;
 
 #[derive(serde::Serialize, serde::Deserialize)]
 struct User {
     _key: String,
     r#type: String,
+}
+
+impl ArangoDocument for User {
+    fn idempotency_key(&self) -> &str {
+        &self._key
+    }
 }
 
 pub async fn migrate(
@@ -18,7 +24,6 @@ pub async fn migrate(
             _key: String::from("1"),
             r#type: String::from("anonymous"),
         },
-        "1",
     )
     .await
 }
