4c2c0a868bea2058592e33111df6306491505514
Martin Zlámal <mrtnzlml@gmail.com>
2021-01-21 08:44:24 -0600

    Backoffice: initialize FBT translations

--- src/ya-comiste-backoffice/pages/products.js
+++ src/ya-comiste-backoffice/pages/products.js
@@ -2,13 +2,16 @@
 
 import * as React from 'react';
 import Link from 'next/link';
+import fbt from 'fbt';
 
 export default function ProductsPage(): React.Node {
   return (
     <div>
       <Link href="/products/create">
         {/* eslint-disable-next-line jsx-a11y/anchor-is-valid */}
-        <a>Create a new product</a>
+        <a>
+          <fbt desc="link for create a new product">Create a new product</fbt>
+        </a>
       </Link>
       <hr />
       TODO (add/edit/delete product + prices, upload photos)
