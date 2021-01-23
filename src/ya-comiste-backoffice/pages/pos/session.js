de78695cb0b7338512be6ef01876b0e383e453e6
Martin Zlámal <mrtnzlml@gmail.com>
2021-01-22 18:44:06 -0600

    YCBO: prepare Recoil atoms for POS

--- src/ya-comiste-backoffice/pages/pos/session.js
+++ src/ya-comiste-backoffice/pages/pos/session.js
@@ -4,16 +4,45 @@ import * as React from 'react';
 import sx from '@adeira/sx';
 import { graphql } from '@adeira/relay';
 
+import Link from '../../src/Link';
 import QueryRenderer from '../../src/QueryRenderer';
+import useSelectedItemsApi from '../../src/pos/recoil/selectedItemsState';
 
 export default function POSSessionPage(): React.Node {
+  const { select, selectedItems, stats } = useSelectedItemsApi();
+
+  const handleDemoItemClick = (itemID) => {
+    select({
+      itemID,
+      itemUnitAmount: 100_00, // TODO
+    });
+  };
+
   return (
     <div className={styles('mainGrid')}>
       <div className={styles('left')}>
-        <div>TODO (selected products, +- buttons, payment)</div>
+        <div>
+          TODO (selected products, +- buttons, payment)
+          <pre>{JSON.stringify(selectedItems, null, 2)}</pre>
+        </div>
+        <div>
+          <pre>{JSON.stringify(stats, null, 2)}</pre>
+        </div>
+        <Link href="/pos/checkout">Checkout</Link>
       </div>
       <main className={styles('right')}>
         <div>TODO (POS screen with all products)</div>
+
+        <button type="button" onClick={() => handleDemoItemClick(1)}>
+          Demo item 1
+        </button>
+        <button type="button" onClick={() => handleDemoItemClick(2)}>
+          Demo item 2
+        </button>
+        <button type="button" onClick={() => handleDemoItemClick(3)}>
+          Demo item 3
+        </button>
+
         <QueryRenderer
           /* eslint-disable relay/unused-fields */
           query={graphql`
@@ -38,6 +67,7 @@ export default function POSSessionPage(): React.Node {
             return <pre>{JSON.stringify(relayProps, null, 2)}</pre>;
           }}
         />
+        <Link href="/pos">Close POS view</Link>
       </main>
     </div>
   );
