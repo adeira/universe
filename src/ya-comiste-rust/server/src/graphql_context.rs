0133fad24f201f1a9ecd7ae9976b1c099e2db47f
Martin Zlámal <mrtnzlml@gmail.com>
2021-01-28 09:59:19 -0600

    YCR: allow only certain file types in GraphQL Multipart

--- src/ya-comiste-rust/server/src/graphql_context.rs
+++ src/ya-comiste-rust/server/src/graphql_context.rs
@@ -2,9 +2,21 @@ use crate::auth::users::User;
 use std::collections::HashMap;
 
 #[derive(Clone)]
+pub struct ContextUploadable {
+    data: Vec<u8>,
+    content_type: String,
+}
+
+impl ContextUploadable {
+    pub fn new(data: Vec<u8>, content_type: String) -> Self {
+        ContextUploadable { data, content_type }
+    }
+}
+
+#[derive(Clone)]
 pub struct Context {
     pub pool: crate::arangodb::ConnectionPool,
-    pub uploadables: Option<HashMap<String, Vec<u8>>>,
+    pub uploadables: Option<HashMap<String, ContextUploadable>>,
     pub user: User,
 }
 
