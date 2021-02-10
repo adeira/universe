a6fb4380043c19871128721d73d86515cfa66b9a
Martin Zlámal <mrtnzlml@gmail.com>
2021-02-10 07:30:10 -0600

    Lint: replace `@adeira/eslint-runner` with `jest-runner-eslint`
    I would like to propose replacing `@adeira/eslint-runner` with `jest-runner-eslint` which seems to be objectively better while doing basically the same. This proposal is part of the effort to make Universe setup simpler and generally better.
    
    There is one noticeable change: I am using [`--changedSince=origin/master^`](https://jestjs.io/docs/cli#--changedsince) from Jest to lint only changed files. This technically means that we might lint more files than strictly needed. But I thought that won't hurt very much.
    
    In case this proposal will be accepted I will deprecate `@adeira/eslint-runner` on NPM and recommend `jest-runner-eslint` instead.

--- src/eslint-runner/src/__tests__/sleepSort.js
+++
@@ -1,6 +0,0 @@
-// @flow strict
-
-// https://twitter.com/JavaScriptDaily/status/856267407106682880 😇
-[3, 5, 1, 8, 2, 4, 9, 6, 7].forEach((num) => {
-  setTimeout(() => console.log(num), num);
-});
