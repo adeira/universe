810bac3713c320a94e418fe213f5a6fabd6269dc
Trond Bergquist <trond_bergquist@hotmail.com>
2021-02-11 16:09:28 +0100

    upgrade babel packages

--- src/example-relay/.babelrc.js
+++ src/example-relay/.babelrc.js
@@ -2,5 +2,12 @@
 
 module.exports = {
   presets: ['@adeira/babel-preset-adeira', 'next/babel'],
-  plugins: ['relay'],
+  /**
+   * test-bc script broke after upgrading some babel packages.
+   * Adding @babel/plugin-proposal-class-properties made it work, but it should not be necessary
+   * to add it here, since we already have it in @adeira/babel-preset-adeira.
+   * We need to leave it here for now for the test-bc script to work, but we should try
+   * to remove it in the future
+   */
+  plugins: ['relay', '@babel/plugin-proposal-class-properties'],
 };
