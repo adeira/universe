9a1b0f378d873f8247e7721485d9180e8d783875
Martin Zlámal <mrtnzlml@gmail.com>
2021-03-02 14:07:35 -0600

    YCBO: add plus/minus control POS buttons

--- src/ya-comiste-backoffice/src/pos/recoil/__tests__/selectedItemsState.test.js
+++ src/ya-comiste-backoffice/src/pos/recoil/__tests__/selectedItemsState.test.js
@@ -5,12 +5,18 @@ import { RecoilRoot } from 'recoil';
 
 import useSelectedItemsApi from '../selectedItemsState';
 
+/* global window */
+
+afterEach(() => {
+  window.localStorage.clear();
+});
+
 it('appends items as expected', () => {
   const { result } = renderHook(() => useSelectedItemsApi(), {
     wrapper: RecoilRoot,
   });
 
-  expect(result.current.selectedItems).toMatchInlineSnapshot(`Object {}`);
+  expect(result.current.selectedItems).toMatchInlineSnapshot(`Immutable.List []`);
   expect(result.current.stats).toMatchInlineSnapshot(`
     Object {
       "totalPrice": 0,
@@ -21,18 +27,21 @@ it('appends items as expected', () => {
   act(() => {
     result.current.select({
       itemID: 'i_1',
+      itemTitle: 'I title 1',
       itemUnitAmount: 100_00,
+      units: 1,
     });
   });
 
   expect(result.current.selectedItems).toMatchInlineSnapshot(`
-    Object {
-      "i_1": Object {
+    Immutable.List [
+      Object {
         "itemID": "i_1",
+        "itemTitle": "I title 1",
         "itemUnitAmount": 10000,
         "units": 1,
       },
-    }
+    ]
   `);
   expect(result.current.stats).toMatchInlineSnapshot(`
     Object {
@@ -44,23 +53,27 @@ it('appends items as expected', () => {
   act(() => {
     result.current.select({
       itemID: 'i_2',
+      itemTitle: 'I title 2',
       itemUnitAmount: 100_00,
+      units: 1,
     });
   });
 
   expect(result.current.selectedItems).toMatchInlineSnapshot(`
-    Object {
-      "i_1": Object {
+    Immutable.List [
+      Object {
         "itemID": "i_1",
+        "itemTitle": "I title 1",
         "itemUnitAmount": 10000,
         "units": 1,
       },
-      "i_2": Object {
+      Object {
         "itemID": "i_2",
+        "itemTitle": "I title 2",
         "itemUnitAmount": 10000,
         "units": 1,
       },
-    }
+    ]
   `);
   expect(result.current.stats).toMatchInlineSnapshot(`
     Object {
@@ -72,28 +85,218 @@ it('appends items as expected', () => {
   act(() => {
     result.current.select({
       itemID: 'i_1',
+      itemTitle: 'I title 1',
       itemUnitAmount: 100_00,
+      units: 1,
     });
   });
 
   expect(result.current.selectedItems).toMatchInlineSnapshot(`
-    Object {
-      "i_1": Object {
+    Immutable.List [
+      Object {
         "itemID": "i_1",
+        "itemTitle": "I title 1",
         "itemUnitAmount": 10000,
         "units": 2,
       },
-      "i_2": Object {
+      Object {
         "itemID": "i_2",
+        "itemTitle": "I title 2",
         "itemUnitAmount": 10000,
         "units": 1,
       },
-    }
+    ]
   `);
   expect(result.current.stats).toMatchInlineSnapshot(`
     Object {
       "totalPrice": 30000,
       "totalSelectedItems": 3,
     }
+  `);
+});
+
+it('manipulates item counts correctly', () => {
+  const { result } = renderHook(() => useSelectedItemsApi(), {
+    wrapper: RecoilRoot,
+  });
+
+  expect(result.current.selectedItems).toMatchInlineSnapshot(`Immutable.List []`);
+
+  // add some items
+  act(() => {
+    result.current.select({
+      itemID: 'i_1',
+      itemTitle: 'I title 1',
+      itemUnitAmount: 100_00,
+      units: 1,
+    });
+  });
+  act(() => {
+    result.current.select({
+      itemID: 'i_2',
+      itemTitle: 'I title 2',
+      itemUnitAmount: 100_00,
+      units: 1,
+    });
+  });
+
+  // increase the first item count
+  act(() => {
+    result.current.increaseUnits('i_1');
+  });
+  expect(result.current.selectedItems).toMatchInlineSnapshot(`
+    Immutable.List [
+      Object {
+        "itemID": "i_1",
+        "itemTitle": "I title 1",
+        "itemUnitAmount": 10000,
+        "units": 2,
+      },
+      Object {
+        "itemID": "i_2",
+        "itemTitle": "I title 2",
+        "itemUnitAmount": 10000,
+        "units": 1,
+      },
+    ]
+  `);
+  expect(result.current.stats).toMatchInlineSnapshot(`
+    Object {
+      "totalPrice": 30000,
+      "totalSelectedItems": 3,
+    }
+  `);
+
+  // now, let's try to delete the first item
+  act(() => {
+    result.current.decreaseUnits('i_1');
+  });
+  expect(result.current.selectedItems).toMatchInlineSnapshot(`
+    Immutable.List [
+      Object {
+        "itemID": "i_1",
+        "itemTitle": "I title 1",
+        "itemUnitAmount": 10000,
+        "units": 1,
+      },
+      Object {
+        "itemID": "i_2",
+        "itemTitle": "I title 2",
+        "itemUnitAmount": 10000,
+        "units": 1,
+      },
+    ]
+  `);
+
+  // and again (deleting the first item)
+  act(() => {
+    result.current.decreaseUnits('i_1');
+  });
+  expect(result.current.selectedItems).toMatchInlineSnapshot(`
+    Immutable.List [
+      Object {
+        "itemID": "i_2",
+        "itemTitle": "I title 2",
+        "itemUnitAmount": 10000,
+        "units": 1,
+      },
+    ]
+  `);
+
+  // deleting the remaining item
+  act(() => {
+    result.current.decreaseUnits('i_2');
+  });
+  expect(result.current.selectedItems).toMatchInlineSnapshot(`Immutable.List []`);
+
+  // and deleting some nonexistent item
+  act(() => {
+    result.current.decreaseUnits('wtf');
+  });
+  expect(result.current.selectedItems).toMatchInlineSnapshot(`Immutable.List []`);
+});
+
+it('preserves order of insertion', () => {
+  const { result } = renderHook(() => useSelectedItemsApi(), {
+    wrapper: RecoilRoot,
+  });
+
+  expect(result.current.selectedItems).toMatchInlineSnapshot(`Immutable.List []`);
+
+  // add some items
+  act(() => {
+    result.current.select({
+      itemID: 'i_1',
+      itemTitle: 'I title 1',
+      itemUnitAmount: 100_00,
+      units: 1,
+    });
+  });
+  act(() => {
+    result.current.select({
+      itemID: 'i_2',
+      itemTitle: 'I title 2',
+      itemUnitAmount: 100_00,
+      units: 1,
+    });
+  });
+
+  expect(result.current.selectedItems).toMatchInlineSnapshot(`
+    Immutable.List [
+      Object {
+        "itemID": "i_1",
+        "itemTitle": "I title 1",
+        "itemUnitAmount": 10000,
+        "units": 1,
+      },
+      Object {
+        "itemID": "i_2",
+        "itemTitle": "I title 2",
+        "itemUnitAmount": 10000,
+        "units": 1,
+      },
+    ]
+  `);
+
+  // let's try to delete them and add them in the reversed order:
+  act(() => {
+    result.current.decreaseUnits('i_1');
+  });
+  act(() => {
+    result.current.decreaseUnits('i_2');
+  });
+
+  act(() => {
+    result.current.select({
+      itemID: 'i_2',
+      itemTitle: 'I title 2',
+      itemUnitAmount: 100_00,
+      units: 1,
+    });
+  });
+  act(() => {
+    result.current.select({
+      itemID: 'i_1',
+      itemTitle: 'I title 1',
+      itemUnitAmount: 100_00,
+      units: 1,
+    });
+  });
+
+  expect(result.current.selectedItems).toMatchInlineSnapshot(`
+    Immutable.List [
+      Object {
+        "itemID": "i_2",
+        "itemTitle": "I title 2",
+        "itemUnitAmount": 10000,
+        "units": 1,
+      },
+      Object {
+        "itemID": "i_1",
+        "itemTitle": "I title 1",
+        "itemUnitAmount": 10000,
+        "units": 1,
+      },
+    ]
   `);
 });
