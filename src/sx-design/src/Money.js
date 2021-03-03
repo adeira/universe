682f8e66093cc1ce0344fa7a1dc8fb66de90af20
Martin Zlámal <mrtnzlml@gmail.com>
2021-03-02 19:46:07 -0600

    SX Design: expose new 'MoneyFn' function
    It does the same as `<Money />` except it can be used in non-React context.

--- src/sx-design/src/Money.js
+++ src/sx-design/src/Money.js
@@ -11,6 +11,12 @@ type Props = {|
 |};
 
 export default function Money(props: Props): React.Node {
+  return MoneyFn(props);
+}
+
+// This function does essentially the same like the React <Money /> component except it can be
+// called from non-React environment.
+export function MoneyFn(props: Props): string {
   // https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Intl/NumberFormat/NumberFormat
   return new Intl.NumberFormat(props.locale, {
     style: 'currency',
