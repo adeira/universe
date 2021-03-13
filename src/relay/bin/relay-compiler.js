11fa23dad32f69375c2e85c9b42f47497f57701f
Martin Zlámal <mrtnzlml@gmail.com>
2021-03-13 12:12:45 -0600

    Relay: use 'relay.config.js' when fetching a new schema
    The original idea of `relay.config.js` was that all the tools can use it and the configuration is only in one place. This commit does exactly that by allowing `adeira-fetch-schema` to read the values from this config. I still kept the previous CLI interface for backward compatibility though.
    
    I also moved the validation logic closer to the CLI options resolver where it belongs.

--- src/relay/bin/relay-compiler.js
+++ src/relay/bin/relay-compiler.js
@@ -9,39 +9,14 @@ require('@babel/register')({
   rootMode: 'upward-optional',
 });
 
-const { invariant } = require('@adeira/js');
 const Logger = require('@adeira/logger').default;
-const RelayConfig = require('relay-config');
 
 const compiler = require('../src/compiler').default;
 const { relayCompilerOptions } = require('./commander/options');
 
 const options = relayCompilerOptions(process.argv);
 
-const config = {
-  src: options.src, // required
-  schema: options.schema, // required
-  persistMode: options.persistMode,
-  validate: options.validate,
-  watch: options.watch,
-  ...RelayConfig.loadConfig(),
-};
-
-invariant(config.src != null, 'Option --src is required.');
-invariant(config.schema != null, 'Option --schema is required.');
-invariant(
-  config.persistMode === undefined || config.persistMode === 'fs',
-  'Only filesystem persist mode is currently supported.',
-);
-
-// TODO: try to download the schema automatically?
-// But please note: some projects are using in-memory server for example and therefore it's not
-// possible to "fetch" the schema since there is no URL. Such projects has to sign their
-// snapshot manually.
-
-// $FlowFixMe[prop-missing]: errors after upgrading to relay 9.1.0
-// $FlowFixMe[incompatible-call]: errors after upgrading to relay 9.1.0
-compiler(config).catch((error) => {
+compiler(options).catch((error) => {
   Logger.error(error);
   process.exit(1);
 });
