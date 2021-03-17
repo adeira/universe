d3cb649e7483bcfd2c84201da3aaa37c39dce1a3
Martin Zlámal <mrtnzlml@gmail.com>
2021-03-17 07:35:15 -0600

    Remove FBT Babel workarounds
    It should no longer be needed, see: https://github.com/facebook/fbt/commit/ecc647d77ca0ce7d23c881fdc9e5fdbe22d6fa69
    
    There is another issue though in Eslint which needs to be fixed, see: https://github.com/adeira/universe/issues/2005

--- src/sx-design/.storybook/.babelrc.js
+++ src/sx-design/.storybook/.babelrc.js
@@ -2,16 +2,5 @@
 
 module.exports = {
   presets: ['@adeira/babel-preset-adeira'],
-  plugins: [
-    [
-      'babel-plugin-fbt',
-      {
-        extraOptions: {
-          __self: true,
-          __source: true,
-        },
-      },
-    ],
-    'babel-plugin-fbt-runtime',
-  ],
+  plugins: ['babel-plugin-fbt', 'babel-plugin-fbt-runtime'],
 };
