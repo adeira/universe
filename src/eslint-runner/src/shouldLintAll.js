cf81663b3f209344776b5d28f3f18ea6d4a3509b
Martin Zlámal <mrtnzlml@gmail.com>
2021-03-19 08:38:46 -0600

    Use Flow version 0.147.0
    See: https://github.com/facebook/flow/releases/tag/v0.147.0

--- src/eslint-runner/src/shouldLintAll.js
+++ src/eslint-runner/src/shouldLintAll.js
@@ -1,6 +1,6 @@
 // @flow strict
 
-export default function shouldLintAll(filename: string): boolean %checks {
+export default function shouldLintAll(filename: string): boolean {
   // Eslint configs can be nested (not only the root path).
   // See: https://eslint.org/docs/user-guide/configuring#configuration-file-formats
   return /\.eslintrc(?:\.(?:js(?:on)?|ya?ml))?$|package\.json$|^\.eslintignore$/.test(filename);
