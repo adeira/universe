800db928d870a925dcfbd799e8972d268120be84
Michal Sanger <michal.sanger@gmail.com>
2021-03-21 16:23:04 +0100

    SX Tailwind: Generated files removed
    Statically generated CSS classes and Flow types are not needed anymore.
    
    The logic was already moved into `@adeira/babel-plugin-transform-sx-tailwind` where CSS classes are generated based on config.
    
    Generated Flow types were removed completely, there is no function using them, because currently the only supported way is `sxt="..."`.
    
    Close #1825

--- src/sx-tailwind/src/sxTailwind.js
+++ src/sx-tailwind/src/sxTailwind.js
@@ -1,37 +1,6 @@
 // @flow
 
-import sx from '@adeira/sx';
-import { warning } from '@adeira/js';
 import levenshtein from 'fast-levenshtein';
-
-import { tailwindStyles } from './__generated__/tailwindStyles';
-import { type TailwindClassNames } from './__generated__/types';
-
-export function tailwind(classes: string): string {
-  const invalidClassNames = classes.split(' ').filter((name) => !(name in tailwindStyles));
-  warning(
-    invalidClassNames.length === 0,
-    `Invalid classname was used: %s. Did you mean '%s' instead?`,
-    invalidClassNames[0],
-    suggestClassName(invalidClassNames[0]),
-  );
-
-  const classNames = ((classes
-    .split(' ')
-    .filter((name) => name in tailwindStyles): any): $ReadOnlyArray<TailwindClassNames>);
-  const styles = Object.fromEntries(
-    classNames.filter((name) => name in tailwindStyles).map((name) => [name, tailwindStyles[name]]),
-  );
-  return sx.create(styles)(...classNames);
-}
-
-function suggestClassName(className: ?string): string {
-  if (className == null) {
-    return '';
-  }
-
-  return suggestUtility(className, tailwindStyles);
-}
 
 export function suggestUtility(className: string, styles: {| +[string]: any |}): string {
   return Object.keys(styles).sort((first, second) => {
