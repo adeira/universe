893dc441ce1985835a3816d723ba34084186c605
Martin Zlámal <mrtnzlml@gmail.com>
2021-04-02 12:36:21 -0600

    YCBO: init ledger page

--- src/ya-comiste-backoffice/pages/employees.js
+++ src/ya-comiste-backoffice/pages/employees.js
@@ -2,7 +2,7 @@
 
 import * as React from 'react';
 
-import EmployeesPageLayout from '../src/users/EmployeesPageLayout';
+import EmployeesPageLayout from '../src/employees/EmployeesPageLayout';
 
 export default function EmployeesPage(): React.Node {
   return <EmployeesPageLayout />;
---
+++ src/ya-comiste-backoffice/pages/ledger.js
@@ -0,0 +1,9 @@
+// @flow
+
+import * as React from 'react';
+
+import LedgerPageLayout from '../src/ledger/LedgerPageLayout';
+
+export default function LedgerPage(): React.Node {
+  return <LedgerPageLayout />;
+}
