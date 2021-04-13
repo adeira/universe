4481866be335090f450ca741a33c2c6da54d4cb7
Martin Zlámal <mrtnzlml@gmail.com>
2021-04-13 08:42:03 -0500

    KOCHKA: add `en-US` stub file
    This is a nice way how to deal with the default language (not to load special translations since there is nothing to translate).

---
+++ src/kochka.com.mx/translations/initFbtTranslations.js
@@ -0,0 +1,28 @@
+// @flow
+
+import { IntlVariations, init as fbtInit } from 'fbt';
+
+import LanguageTag, { type LanguageTagType } from '../src/LanguageTag';
+
+export default function initFbtTranslations(lang: ?string): LanguageTagType {
+  const languageTag = LanguageTag.detectLanguageTag(lang);
+  const locale = languageTag.bcp47;
+
+  const supportedLocales = {
+    // TODO: support translations lazy loading
+    'en-US': require('./out/en-US.json'), // empty stub
+    'es-MX': require('./out/es-MX.json'),
+  };
+
+  fbtInit({
+    translations: supportedLocales[locale],
+    hooks: {
+      getViewerContext: () => ({
+        GENDER: IntlVariations.GENDER_UNKNOWN,
+        locale: locale,
+      }),
+    },
+  });
+
+  return languageTag;
+}
