4a9d48d5ad11f6a5e7028f5ed7bb40f2141b7508
Martin Zlámal <mrtnzlml@gmail.com>
2021-04-15 08:07:55 -0500

    Flow-typed: update Jest definitions

--- src/monorepo-shipit/config/__mocks__/fs.js
+++ src/monorepo-shipit/config/__mocks__/fs.js
@@ -1,6 +1,5 @@
 // @flow
 
-// $FlowIssue[prop-missing]: https://github.com/flow-typed/flow-typed/pull/4062
 const fs: any = jest.createMockFromModule('fs');
 
 fs.existsSync = (path) => path !== '/unknown_path';
