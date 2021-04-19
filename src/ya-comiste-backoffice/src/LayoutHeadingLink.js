de50bf2206f0f64da6aeb74b6b0e396928aaead4
Martin Zlámal <mrtnzlml@gmail.com>
2021-04-19 17:04:13 -0500

    YCBO: use more SX Design components

--- src/ya-comiste-backoffice/src/LayoutHeadingLink.js
+++ src/ya-comiste-backoffice/src/LayoutHeadingLink.js
@@ -1,9 +1,7 @@
 // @flow
 
 import React, { type Node } from 'react';
-import sx from '@adeira/sx';
-
-import Link from './Link';
+import { Link } from '@adeira/sx-design';
 
 type Props = {
   +href: string,
@@ -12,23 +10,6 @@ type Props = {
 
 // creates <a href="…" />
 export default function LayoutHeadingLink(props: Props): Node {
-  return (
-    <Link href={props.href} xstyle={styles.link}>
-      {props.children}
-    </Link>
-  );
+  // TODO: create a new ButtonLink (?)
+  return <Link href={props.href}>{props.children}</Link>;
 }
-
-const styles = sx.create({
-  link: {
-    'cursor': 'pointer',
-    'padding': '.5rem 1rem',
-    'borderRadius': 4,
-    'border': '1px solid #e9eff3',
-    'backgroundColor': '#e9eff3',
-    'marginRight': '.5rem',
-    ':hover': {
-      color: '#3b85ff',
-    },
-  },
-});
