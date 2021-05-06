8e034a869266384837432a0b3741fa0f33e42592
Martin Zlámal <mrtnzlml@gmail.com>
2021-05-06 10:57:28 -0500

    Abacus: optimize blurhash calculation

--- src/abacus/src/images/blurhash.rs
+++ src/abacus/src/images/blurhash.rs
@@ -2,13 +2,21 @@ use blurhash_wasm::{encode, EncodingError};
 use image::DynamicImage;
 
 /// This function calculates blurhash of the image (https://blurha.sh/).
+///
+/// # How fast is encoding? Decoding?
+///
+/// Blurhash implementations are not very optimised. Running them on very large images can be a bit
+/// slow. However! The trick to using the algorithm efficiently is to not run it on full-sized data.
+/// The fine detail of an image is all thrown away, so you should scale your images down before
+/// running BlurHash on them. If you are creating thumbnails, run BlurHash on those instead of the
+/// full images. (source: https://github.com/woltapp/blurhash)
 pub(in crate::images) fn calculate_image_blurhash(
     image: DynamicImage,
 ) -> Result<String, EncodingError> {
-    // TODO: the Blurhash calculation appears to be very slow for large images!
-    let input = image.to_rgba8();
+    let thumbnail = image.thumbnail(32, 32);
+    let input = thumbnail.to_rgba8();
     let (width, height) = input.dimensions();
-    encode(input.into_vec(), 4, 4, width as usize, height as usize)
+    encode(input.into_vec(), 4, 3, width as usize, height as usize)
 }
 
 #[cfg(test)]
@@ -16,11 +24,20 @@ mod tests {
     use super::*;
 
     #[test]
-    fn blurhash_test() {
+    fn blurhash_test_png() {
         let image = image::open("src/images/tests/data/img.png").unwrap();
         assert_eq!(
             calculate_image_blurhash(image).unwrap(),
-            "UIFFXSUH]:+p@-5c+*JO@[s~[Q6.}.F_jDOZ"
+            "LJEMCJUH=z;a[OA3#uNY@]bC[RF5"
+        );
+    }
+
+    #[test]
+    fn blurhash_test_jpg() {
+        let image = image::open("src/images/tests/data/img.jpg").unwrap();
+        assert_eq!(
+            calculate_image_blurhash(image).unwrap(),
+            "LNN+%~-pTw^j}?VyVZV?}:VstQr:"
         );
     }
 }
