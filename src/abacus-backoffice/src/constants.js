15bcb7860c50c8ff3ac5a804e012864cc046a25b
Martin Zlámal <mrtnzlml@gmail.com>
2021-06-01 14:42:41 -0500

    Abacus BO: use new server URL

--- src/abacus-backoffice/src/constants.js
+++ src/abacus-backoffice/src/constants.js
@@ -1,6 +1,6 @@
 // @flow strict
 
 export default {
-  graphqlServerURL: 'http://abacus.mrtnzlml.com:32123/graphql', // TODO: HTTPS, better "abacus" domain
+  graphqlServerURL: 'https://abacus.kochka.com.mx/graphql',
   googleClientID: '245356693889-63qeuc6183hab6be342blikbknsvqrhk.apps.googleusercontent.com',
 };
