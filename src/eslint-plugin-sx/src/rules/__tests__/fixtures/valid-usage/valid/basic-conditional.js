5e9509bd222f9365f294957558ee96b62d1b7733
Martin Zlámal <mrtnzlml@gmail.com>
2021-06-07 13:20:20 -0500

    SX Eslint: add fixture for basic conditional object
    Technically, this rule doesn't yet validate the `styles(…)` arguments, however, it won't hurt to have here the valid test-case.

---
+++ src/eslint-plugin-sx/src/rules/__tests__/fixtures/valid-usage/valid/basic-conditional.js
@@ -0,0 +1,12 @@
+// @flow
+
+import type { Node } from 'react';
+import sx from '@adeira/sx';
+
+export default function MyComponent(): Node {
+  return <div className={styles({ aaa: true })} />;
+}
+
+const styles = sx.create({
+  aaa: { color: 'red' },
+});
