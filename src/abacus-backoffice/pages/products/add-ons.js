17ec82549e00f6746462aa9c2b845bbffe8466b7
Martin Zlámal <mrtnzlml@gmail.com>
2021-06-21 12:34:26 -0500

    Abacus BO: prepare products add-ons pages

---
+++ src/abacus-backoffice/pages/products/add-ons.js
@@ -0,0 +1,9 @@
+// @flow
+
+import * as React from 'react';
+
+import ProductsAddonsLayout from '../../src/products/ProductsAddonsLayout';
+
+export default function ProductsPage(): React.Node {
+  return <ProductsAddonsLayout />;
+}
