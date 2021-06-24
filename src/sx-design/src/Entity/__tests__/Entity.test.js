3d2e1fdb5cb138a8567ec9a76559420c128edc1a
Martin Zlámal <mrtnzlml@gmail.com>
2021-06-23 20:54:05 -0500

    SX Design: add new components `Entity` and `EntityField`
    As usual, inspired by: https://vercel.com/design/entity

---
+++ src/sx-design/src/Entity/__tests__/Entity.test.js
@@ -0,0 +1,34 @@
+/**
+ * @flow
+ * @jest-environment jsdom
+ */
+
+import { render } from '@testing-library/react';
+import React from 'react';
+
+import Entity from '../Entity';
+import EntityField from '../EntityField';
+
+it('renders Entity component without any problems', () => {
+  const { getByText } = render(
+    <Entity>
+      <EntityField title="Test title 1" />
+      <EntityField title="Test title 2" />
+      <EntityField title="Test title 3" />
+    </Entity>,
+  );
+  expect(getByText('Test title 1')).toBeDefined();
+  expect(getByText('Test title 2')).toBeDefined();
+  expect(getByText('Test title 3')).toBeDefined();
+});
+
+it('throws an error when used without any children', () => {
+  const consoleSpy = jest.spyOn(console, 'error').mockImplementation(() => {});
+
+  // $FlowExpectedError[prop-missing]: missing required children (on purpose)
+  expect(() => render(<Entity />)).toThrowErrorMatchingInlineSnapshot(
+    `"Component Entity has to be called with some children."`,
+  );
+
+  consoleSpy.mockRestore();
+});
