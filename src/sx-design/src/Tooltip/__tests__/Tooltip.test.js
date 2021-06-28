630c82cb61f1d4d879fb5150ae3575ed8acf9bd4
Martin Zlámal <mrtnzlml@gmail.com>
2021-06-28 14:59:10 -0500

    SX Design: implement `findBestTooltipPosition`
    This allows us to position to `Tooltip` component better when there is not enough space.

--- src/sx-design/src/Tooltip/__tests__/Tooltip.test.js
+++ src/sx-design/src/Tooltip/__tests__/Tooltip.test.js
@@ -25,34 +25,31 @@ it('renders and behaves as expected', () => {
     </SxDesignProvider>,
   );
 
-  // By default the text is hidden:
-  expect(queryByText('test tooltip content')).toBeNull();
+  // By default the text is hidden (via CSS visibility):
+  expect(getByText('test tooltip content')).toBeDefined();
+  expect(window.getComputedStyle(queryByText('test tooltip content')).visibility).toBe('hidden');
 
   // We have to uncover it:
   fireEvent.mouseOver(getByTestId('hover_over_me'));
 
   // And now we should be able to find it:
   expect(getByText('test tooltip content')).toBeDefined();
+  expect(window.getComputedStyle(queryByText('test tooltip content')).visibility).toBe('visible');
 });
 
 it('throws an error when HTML node with ID #react-portal-root is missing', () => {
   const consoleSpy = jest.spyOn(console, 'error').mockImplementation(() => {});
 
-  const { queryByText, getByTestId } = render(
-    <Tooltip data-testid="hover_over_me">
-      <fbt desc="test tooltip content title" doNotExtract={true}>
-        test tooltip content
-      </fbt>
-    </Tooltip>,
-  );
-
-  // By default the text is hidden:
-  expect(queryByText('test tooltip content')).toBeNull();
-
-  // It should fail when we try to uncover it and there is no HTML node with ID #react-portal-root
-  // so we cannot create a React Portal for this tooltip:
+  // It should fail when we try to render the `Tooltip` and there is no HTML node with ID
+  // #react-portal-root so we cannot create a React Portal for this tooltip:
   expect(() =>
-    fireEvent.mouseOver(getByTestId('hover_over_me')),
+    render(
+      <Tooltip data-testid="hover_over_me">
+        <fbt desc="test tooltip content title" doNotExtract={true}>
+          test tooltip content
+        </fbt>
+      </Tooltip>,
+    ),
   ).toThrowErrorMatchingInlineSnapshot(
     `"Tooltip component requires HTML node with ID \\"#react-portal-root\\" (none was found). Did you forget to call \\"<SxDesignProvider />\\" somewhere in the application root?"`,
   );
