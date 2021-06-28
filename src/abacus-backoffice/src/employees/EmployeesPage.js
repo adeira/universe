2835155a88ab8d6f5e4d9b03be356a9c9f13d7f2
Martin Zlámal <mrtnzlml@gmail.com>
2021-06-28 09:22:08 -0500

    Abacus BO: use `Entity` for list of employees

--- src/abacus-backoffice/src/employees/EmployeesPage.js
+++ src/abacus-backoffice/src/employees/EmployeesPage.js
@@ -1,7 +1,6 @@
 // @flow
 
-import sx from '@adeira/sx';
-import { Badge } from '@adeira/sx-design';
+import { Badge, Entity, EntityField } from '@adeira/sx-design';
 import * as React from 'react';
 import { graphql, useLazyLoadQuery } from '@adeira/relay';
 import fbt from 'fbt';
@@ -21,54 +20,37 @@ export default function EmployeesPage(): React.Node {
     }
   `);
 
-  return (
-    <div className={styles('grid')}>
-      {data.listUsers.map((user) => {
-        return (
-          <React.Fragment key={user.id}>
-            <div className={styles('cell')}>
-              {user.isActive ? (
-                <Badge tint="success">
-                  <fbt desc="active user - label title">active</fbt>
-                </Badge>
-              ) : (
-                <Badge tint="warning">
-                  <fbt desc="inactive user - label title">inactive</fbt>
-                </Badge>
-              )}
-            </div>
-            <div className={styles('cell')}>{user.name ?? <MissingData />}</div>
-            <div className={styles('cell')}>
-              {user.hasEmailVerified ? (
-                <Badge tint="success">
-                  <fbt desc="user has email verifies - label title">email verified</fbt>
-                </Badge>
-              ) : (
-                <Badge tint="error">
-                  <fbt desc="user doesn't have email verifies - label title">
-                    email NOT verified
-                  </fbt>
-                </Badge>
-              )}
-            </div>
-          </React.Fragment>
-        );
-      })}
-    </div>
-  );
+  return data.listUsers.map((user) => {
+    return (
+      <Entity key={user.id}>
+        <EntityField
+          title={
+            user.isActive ? (
+              <Badge tint="success">
+                <fbt desc="active user - label title">active</fbt>
+              </Badge>
+            ) : (
+              <Badge tint="warning">
+                <fbt desc="inactive user - label title">inactive</fbt>
+              </Badge>
+            )
+          }
+        />
+        <EntityField title={user.name ?? <MissingData />} />
+        <EntityField
+          title={
+            user.hasEmailVerified ? (
+              <Badge tint="success">
+                <fbt desc="user has email verifies - label title">email verified</fbt>
+              </Badge>
+            ) : (
+              <Badge tint="error">
+                <fbt desc="user doesn't have email verifies - label title">email NOT verified</fbt>
+              </Badge>
+            )
+          }
+        />
+      </Entity>
+    );
+  });
 }
-
-const styles = sx.create({
-  grid: {
-    display: 'grid',
-    gridTemplateColumns: 'auto 1fr auto',
-  },
-  cell: {
-    'paddingTop': '1rem',
-    'paddingBottom': '1rem',
-    'borderBottom': '1px solid rgba(var(--sx-accent-3))',
-    ':not(:last-child)': {
-      paddingRight: '1rem',
-    },
-  },
-});
