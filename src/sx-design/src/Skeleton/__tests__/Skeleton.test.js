314366ad9ac97cc07311df95778d4097b8b865bc
Martin Zlámal <mrtnzlml@gmail.com>
2021-06-27 21:29:44 -0500

    SX Design: use rounded edges (`Image`, `ProductCard`)
    Using rounded edges makes it more uniform with the rest of the design system. I also removed `squared` option from `Skeleton` component. It's because this option is not being used and SX Design should implement only what's actually needed and used.

--- src/sx-design/src/Skeleton/__tests__/Skeleton.test.js
+++ src/sx-design/src/Skeleton/__tests__/Skeleton.test.js
@@ -12,8 +12,3 @@ it('renders default Skeleton component without any problems', () => {
   const { getByTestId } = render(<Skeleton data-testid="default_skeleton" />);
   expect(getByTestId('default_skeleton')).toBeDefined();
 });
-
-it('renders squared Skeleton component without any problems', () => {
-  const { getByTestId } = render(<Skeleton data-testid="squared_skeleton" squared={true} />);
-  expect(getByTestId('squared_skeleton')).toBeDefined();
-});
