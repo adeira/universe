b05232d237889ef3ccc16580ccc19ac0af3a664d
Martin Zlámal <mrtnzlml@gmail.com>
2021-07-04 17:33:13 -0500

    Icons: improve Flow types

--- src/icons/__generated__/FlipView.js
+++ src/icons/__generated__/FlipView.js
@@ -1,8 +1,8 @@
 // @flow strict
 
 /* eslint-disable import/newline-after-import */
-import React, { type Node } from 'react';
-export default function FlipView(props: {}): Node {
+import React, { type Element } from 'react';
+export default function FlipView(props: {}): Element<'svg'> {
   return (
     <svg height="1em" viewBox="0 0 21 21" width="1em" xmlns="http://www.w3.org/2000/svg" {...props}>
       <path
--- src/icons/__generated__/Midpoint.js
+++ src/icons/__generated__/Midpoint.js
@@ -1,8 +1,8 @@
 // @flow strict
 
 /* eslint-disable import/newline-after-import */
-import React, { type Node } from 'react';
-export default function Midpoint(props: {}): Node {
+import React, { type Element } from 'react';
+export default function Midpoint(props: {}): Element<'svg'> {
   return (
     <svg height="1em" viewBox="0 0 21 21" width="1em" xmlns="http://www.w3.org/2000/svg" {...props}>
       <path
