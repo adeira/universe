42024add4b4d5b6ddc69316346864da983d0cc06
Martin Zlámal <mrtnzlml@gmail.com>
2021-07-11 12:15:38 -0500

    SX Design: allow `children` in `Skeleton`
    Skeleton component can optionally have a children component in which case the Skeleton tries to measure dimensions of this child and render itself accordingly.

--- src/sx-design/src/Skeleton/Skeleton.stories.js
+++ src/sx-design/src/Skeleton/Skeleton.stories.js
@@ -6,7 +6,10 @@
 import sx from '@adeira/sx';
 import React from 'react';
 import { rangeMap } from '@adeira/js';
+import fbt from 'fbt';
 
+import Button from '../Button/Button';
+import { initFbt } from '../test-utils';
 import Skeleton from './Skeleton';
 import type { StoryTemplate } from '../types';
 
@@ -14,25 +17,47 @@ import type { StoryTemplate } from '../types';
 export default {
   title: 'Example/Skeleton',
   component: Skeleton,
+  argTypes: {
+    'children': { table: { disable: true } },
+    'data-testid': { table: { disable: true } },
+  },
 };
 
 const styles = sx.create({
   productsGrid: {
     display: 'grid',
-    gridTemplateColumns: 'repeat(auto-fill, minmax(250px, 1fr))',
+    gridTemplateColumns: 'repeat(auto-fill, minmax(150px, 1fr))',
     gap: '1rem',
   },
 });
 
+initFbt();
+
 // 👇 We create a "template" of how args map to rendering
-const Template = (args) => (
+const StandaloneTemplate = (args) => <Skeleton {...args} />;
+const GridTemplate = (args) => (
   <div className={styles('productsGrid')}>
-    {rangeMap(12, (i) => (
+    {rangeMap(16, (i) => (
       <Skeleton key={i} {...args} />
     ))}
   </div>
 );
 
 // 👇 Each story then reuses that template
-export const DefaultSkeleton: StoryTemplate<typeof Skeleton> = Template.bind({});
-DefaultSkeleton.storyName = 'Default';
+export const StandaloneSkeleton: StoryTemplate<typeof Skeleton> = StandaloneTemplate.bind({});
+StandaloneSkeleton.storyName = 'Standalone';
+
+export const InGridSkeleton: StoryTemplate<typeof Skeleton> = GridTemplate.bind({});
+InGridSkeleton.storyName = 'In a CSS grid';
+
+export const WithChildrenSkeleton: StoryTemplate<typeof Skeleton> = StandaloneTemplate.bind({});
+WithChildrenSkeleton.storyName = 'With children';
+WithChildrenSkeleton.args = {
+  children: (
+    <Button onClick={() => {}}>
+      <fbt desc="button title" doNotExtract={true}>
+        test button
+      </fbt>
+    </Button>
+  ),
+};
