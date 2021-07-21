4db0650bd7803cceb4e213634a72a7493aad98cb
Martin Zlámal <mrtnzlml@gmail.com>
2021-07-21 09:19:55 -0500

    SX: correctly transform `webkit` style names
    We have a special property `webkitLineClamp` in our generated types so users can freely use it. However, it's being transformed incorrectly into `webkit-line-clamp` (instead of `-webkit-line-clamp`). This change fixes it.
    
    See: https://developer.mozilla.org/en-US/docs/Web/CSS/-webkit-line-clamp

--- src/sx/src/transformStyleName.js
+++ src/sx/src/transformStyleName.js
@@ -8,5 +8,12 @@ export default function transformStyleName(styleName: string): string {
     return styleName;
   }
 
-  return changeCase.paramCase(styleName);
+  let cssName = changeCase.paramCase(styleName);
+  if (cssName.startsWith('webkit')) {
+    // We allow `webkitLineClamp` in our generated types (`AllCSSPropertyTypes`) which seems to be
+    // the only one user might write manually, see: https://developer.mozilla.org/en-US/docs/Web/CSS/-webkit-line-clamp
+    cssName = `-${cssName}`;
+  }
+
+  return cssName;
 }
