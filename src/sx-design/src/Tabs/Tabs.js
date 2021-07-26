47487f83b226649eb7b37d69a052a3d39349a927
Martin Zlámal <mrtnzlml@gmail.com>
2021-07-26 09:11:33 -0500

    SX Design: improve `Tabs` design
    Better Storybook story and more appealing tabs design (dark mode included).

--- src/sx-design/src/Tabs/Tabs.js
+++ src/sx-design/src/Tabs/Tabs.js
@@ -38,13 +38,22 @@ const styles = sx.create({
     flexWrap: 'wrap',
   },
   tab: {
-    paddingInline: 20,
-    paddingBlock: 15,
-    cursor: 'pointer',
-    color: 'rgba(var(--sx-foreground))',
-    borderBlockEnd: '1px solid rgba(var(--sx-accent-2))',
+    'fontWeight': 'bold',
+    'paddingInline': 20,
+    'paddingBlock': 15,
+    'cursor': 'pointer',
+    'color': 'rgba(var(--sx-accent-6))',
+    'borderBlockEnd': '1px solid rgba(var(--sx-accent-2))',
+    ':hover': {
+      color: 'rgba(var(--sx-foreground))',
+      borderBlockEnd: '2px solid rgba(var(--sx-accent-2))',
+    },
   },
   tabSelected: {
-    borderBlockEnd: '2px solid rgba(var(--sx-foreground))',
+    'color': 'rgba(var(--sx-foreground))',
+    'borderBlockEnd': '2px solid rgba(var(--sx-foreground))',
+    ':hover': {
+      borderBlockEnd: '2px solid rgba(var(--sx-foreground))',
+    },
   },
 });
