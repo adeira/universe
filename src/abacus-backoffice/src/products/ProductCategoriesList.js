115e992730f188d6e242a58858c28886d95234ba
Martin Zlámal <mrtnzlml@gmail.com>
2021-07-27 15:00:08 -0500

    Abacus BO: print product categories

---
+++ src/abacus-backoffice/src/products/ProductCategoriesList.js
@@ -0,0 +1,38 @@
+// @flow
+
+import { useLazyLoadQuery, graphql } from '@adeira/relay';
+import React, { type Node } from 'react';
+import { Entity, EntityField } from '@adeira/sx-design';
+
+import useApplicationLocale from '../useApplicationLocale';
+import type { ProductCategoriesListQuery } from './__generated__/ProductCategoriesListQuery.graphql';
+
+export default function ProductCategoriesList(): Node {
+  const applicationLocale = useApplicationLocale();
+  const data = useLazyLoadQuery<ProductCategoriesListQuery>(
+    graphql`
+      query ProductCategoriesListQuery($clientLocale: SupportedLocale!) {
+        commerce {
+          categories: searchAllProductCategories(clientLocale: $clientLocale) {
+            id
+            name
+          }
+        }
+      }
+    `,
+    {
+      clientLocale: applicationLocale.graphql,
+    },
+  );
+
+  return data.commerce.categories.reduce((acc, category) => {
+    if (category) {
+      acc.push(
+        <Entity key={category.id}>
+          <EntityField title={category.name} />
+        </Entity>,
+      );
+    }
+    return acc;
+  }, []);
+}
---
+++ src/abacus-backoffice/src/products/__generated__/ProductCategoriesListQuery.graphql.js
@@ -0,0 +1,119 @@
+/**
+ * @flow
+ */
+
+/* eslint-disable */
+
+import type { ConcreteRequest } from 'relay-runtime';
+export type SupportedLocale = "en_US" | "es_MX" | "%future added value";
+export type ProductCategoriesListQueryVariables = {|
+  clientLocale: SupportedLocale
+|};
+export type ProductCategoriesListQueryResponse = {|
+  +commerce: {|
+    +categories: $ReadOnlyArray<?{|
+      +id: string,
+      +name: string,
+    |}>
+  |}
+|};
+export type ProductCategoriesListQuery = {|
+  variables: ProductCategoriesListQueryVariables,
+  response: ProductCategoriesListQueryResponse,
+|};
+
+/*
+query ProductCategoriesListQuery(
+  $clientLocale: SupportedLocale!
+) {
+  commerce {
+    categories: searchAllProductCategories(clientLocale: $clientLocale) {
+      id
+      name
+    }
+  }
+}
+*/
+
+const node: ConcreteRequest = (function(){
+var v0 = [
+  {
+    "defaultValue": null,
+    "kind": "LocalArgument",
+    "name": "clientLocale"
+  }
+],
+v1 = [
+  {
+    "alias": null,
+    "args": null,
+    "concreteType": "CommerceQuery",
+    "kind": "LinkedField",
+    "name": "commerce",
+    "plural": false,
+    "selections": [
+      {
+        "alias": "categories",
+        "args": [
+          {
+            "kind": "Variable",
+            "name": "clientLocale",
+            "variableName": "clientLocale"
+          }
+        ],
+        "concreteType": "ProductCategory",
+        "kind": "LinkedField",
+        "name": "searchAllProductCategories",
+        "plural": true,
+        "selections": [
+          {
+            "alias": null,
+            "args": null,
+            "kind": "ScalarField",
+            "name": "id",
+            "storageKey": null
+          },
+          {
+            "alias": null,
+            "args": null,
+            "kind": "ScalarField",
+            "name": "name",
+            "storageKey": null
+          }
+        ],
+        "storageKey": null
+      }
+    ],
+    "storageKey": null
+  }
+];
+return {
+  "fragment": {
+    "argumentDefinitions": (v0/*: any*/),
+    "kind": "Fragment",
+    "metadata": null,
+    "name": "ProductCategoriesListQuery",
+    "selections": (v1/*: any*/),
+    "type": "Query",
+    "abstractKey": null
+  },
+  "kind": "Request",
+  "operation": {
+    "argumentDefinitions": (v0/*: any*/),
+    "kind": "Operation",
+    "name": "ProductCategoriesListQuery",
+    "selections": (v1/*: any*/)
+  },
+  "params": {
+    "cacheID": "df442eca349822bf78eb2cc9cf2e77b8",
+    "id": null,
+    "metadata": {},
+    "name": "ProductCategoriesListQuery",
+    "operationKind": "query",
+    "text": "query ProductCategoriesListQuery(\n  $clientLocale: SupportedLocale!\n) {\n  commerce {\n    categories: searchAllProductCategories(clientLocale: $clientLocale) {\n      id\n      name\n    }\n  }\n}\n"
+  }
+};
+})();
+// prettier-ignore
+(node: any).hash = '7316f6f463886f0de452366ff2fc918e';
+export default node;
