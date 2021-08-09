c0d6b3c2d876db5cd47b5e6680e7d01616a1bc88
Martin Zlámal <mrtnzlml@gmail.com>
2021-08-09 07:28:35 -0500

    Abacus KO: update legal TODO notes

--- src/abacus-kochka/pages/legal/shipping.js
+++ src/abacus-kochka/pages/legal/shipping.js
@@ -1,5 +1,6 @@
 // @flow
 
+import { Note } from '@adeira/sx-design';
 import * as React from 'react';
 import fbt from 'fbt';
 
@@ -8,8 +9,9 @@ import Layout from '../../src/Layout';
 export default function LegalShipping(): React.Node {
   return (
     <Layout title={<fbt desc="shipping and returns page title">Shipping & Returns</fbt>}>
-      {/* TODO */}
-      TODO
+      <Note tint="warning">
+        <fbt desc="stay tuned note">this page is not finished yet, stay tuned</fbt>
+      </Note>
     </Layout>
   );
 }
