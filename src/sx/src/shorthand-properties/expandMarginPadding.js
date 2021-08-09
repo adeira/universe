b850c0e4844d2ca7533090dbc48a77e4f225807b
Martin Zlámal <mrtnzlml@gmail.com>
2021-08-09 11:58:33 -0500

    SX: expand logical shorthand margins and paddings

--- src/sx/src/shorthand-properties/expandMarginPadding.js
+++ src/sx/src/shorthand-properties/expandMarginPadding.js
@@ -3,56 +3,74 @@
  * @see https://developer.mozilla.org/en-US/docs/Web/CSS/Shorthand_properties#Margin_and_Padding_Properties
  */
 
+import { invariant } from '@adeira/js';
+
 import StyleCollectorNode from '../StyleCollectorNode';
 
+function isLogicalProperty(propertyName) {
+  return propertyName.endsWith('Block') || propertyName.endsWith('Inline');
+}
+
+/**
+ * Margin -> Margin Top/Right/Bottom/Left
+ * Margin Block -> Margin Block Start/End
+ * Margin Inline -> Margin Inline Start/End
+ *
+ * Padding -> Padding Top/Right/Bottom/Left
+ * Padding Block -> Padding Block Start/End
+ * Padding Inline -> Padding Inline Start/End
+ */
 export default function expandMarginPadding(
-  propertyName: 'margin' | 'padding',
+  propertyName:
+    | 'margin'
+    | 'marginBlock'
+    | 'marginInline'
+    | 'padding'
+    | 'paddingBlock'
+    | 'paddingInline',
   propertyValue: string | number,
   hashSeed: string = '',
 ): $ReadOnlyArray<StyleCollectorNode> {
-  const top = `${propertyName}Top`;
-  const right = `${propertyName}Right`;
-  const bottom = `${propertyName}Bottom`;
-  const left = `${propertyName}Left`;
+  function getExpandedNodes(a, b, c, d) {
+    if (isLogicalProperty(propertyName)) {
+      return [
+        new StyleCollectorNode(`${propertyName}Start`, a, hashSeed),
+        new StyleCollectorNode(`${propertyName}End`, b, hashSeed),
+      ];
+    }
+    return [
+      new StyleCollectorNode(`${propertyName}Top`, a, hashSeed),
+      new StyleCollectorNode(`${propertyName}Right`, b, hashSeed),
+      new StyleCollectorNode(`${propertyName}Bottom`, c, hashSeed),
+      new StyleCollectorNode(`${propertyName}Left`, d, hashSeed),
+    ];
+  }
+
   if (typeof propertyValue === 'string') {
     const chunks = propertyValue.split(/\s+/);
     if (chunks.length === 1) {
-      const chunk = chunks[0];
-      return [
-        new StyleCollectorNode(top, chunk, hashSeed),
-        new StyleCollectorNode(right, chunk, hashSeed),
-        new StyleCollectorNode(bottom, chunk, hashSeed),
-        new StyleCollectorNode(left, chunk, hashSeed),
-      ];
+      return getExpandedNodes(chunks[0], chunks[0], chunks[0], chunks[0]);
     } else if (chunks.length === 2) {
-      return [
-        new StyleCollectorNode(top, chunks[0], hashSeed),
-        new StyleCollectorNode(right, chunks[1], hashSeed),
-        new StyleCollectorNode(bottom, chunks[0], hashSeed),
-        new StyleCollectorNode(left, chunks[1], hashSeed),
-      ];
+      return getExpandedNodes(chunks[0], chunks[1], chunks[0], chunks[1]);
     } else if (chunks.length === 3) {
-      return [
-        new StyleCollectorNode(top, chunks[0], hashSeed),
-        new StyleCollectorNode(right, chunks[1], hashSeed),
-        new StyleCollectorNode(bottom, chunks[2], hashSeed),
-        new StyleCollectorNode(left, chunks[1], hashSeed),
-      ];
+      invariant(
+        isLogicalProperty(propertyName) === false,
+        'Logical property "%s" cannot have more than 2 values (got 3: "%s").',
+        propertyName,
+        propertyValue,
+      );
+      return getExpandedNodes(chunks[0], chunks[1], chunks[2], chunks[1]);
     } else if (chunks.length === 4) {
-      return [
-        new StyleCollectorNode(top, chunks[0], hashSeed),
-        new StyleCollectorNode(right, chunks[1], hashSeed),
-        new StyleCollectorNode(bottom, chunks[2], hashSeed),
-        new StyleCollectorNode(left, chunks[3], hashSeed),
-      ];
+      invariant(
+        isLogicalProperty(propertyName) === false,
+        'Logical property "%s" cannot have more than 2 values (got 4: "%s").',
+        propertyName,
+        propertyValue,
+      );
+      return getExpandedNodes(chunks[0], chunks[1], chunks[2], chunks[3]);
     }
   } else if (typeof propertyValue === 'number') {
-    return [
-      new StyleCollectorNode(top, propertyValue, hashSeed),
-      new StyleCollectorNode(right, propertyValue, hashSeed),
-      new StyleCollectorNode(bottom, propertyValue, hashSeed),
-      new StyleCollectorNode(left, propertyValue, hashSeed),
-    ];
+    return getExpandedNodes(propertyValue, propertyValue, propertyValue, propertyValue);
   }
 
   return [new StyleCollectorNode(propertyName, propertyValue, hashSeed)];
