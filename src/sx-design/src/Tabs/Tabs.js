c8fee5f1844e1f7dc241a6f762f826fdd492e5d4
Martin Zlámal <mrtnzlml@gmail.com>
2021-08-27 15:46:32 -0500

    SX Design: improve `Tabs` accessibility
    Previously, it was not possible to navigate the tabs via `Tab` and
    `Tab+Shift` keys. So I changed the internal implementation to use HTML
    `button` instead which enables it by default.
    
    The accessibility is still not perfect. Specifically, we should be able
    to change the tabs only by arrow keys (and not tabs). But that's for
    later.

--- src/sx-design/src/Tabs/Tabs.js
+++ src/sx-design/src/Tabs/Tabs.js
@@ -3,6 +3,8 @@
 import React, { type Node } from 'react';
 import sx from '@adeira/sx';
 
+import Text from '../Text/Text';
+
 type TabValue = string | number | null;
 type Props = {
   +tabs: $ReadOnlyArray<{ +title: Fbt, +value: TabValue }>,
@@ -11,25 +13,39 @@ type Props = {
 };
 
 /**
- * TODO: improve accessibility (https://www.w3.org/TR/wai-aria-practices/#tabpanel)
+ * Tabs are a set of layered sections of content, known as tab panels, that display one panel of
+ * content at a time. Each tab panel has an associated tab element, that when activated, displays
+ * the panel. The list of tab elements is arranged along one edge of the currently displayed panel.
+ *
+ * TODO: improve accessibility
+ *  - https://www.w3.org/TR/wai-aria-practices/#tabpanel
+ *  - https://www.w3.org/TR/wai-aria-practices/examples/tabs/tabs-2/tabs.html
  */
 export default function Tabs(props: Props): Node {
   return (
-    <div className={styles('tabs')}>
+    <div className={styles('tabs')} role="tablist">
       {props.tabs.map((tab) => {
+        const isTabSelected = props.selected === tab.value;
         return (
-          <div
+          // eslint-disable-next-line react/forbid-elements
+          <button
+            role="tab"
+            type="button"
             key={tab.value}
+            aria-selected={isTabSelected}
+            // tabIndex={isTabSelected ? 0 : -1} // TODO (when we can navigate tabs with arrows)
             className={styles({
-              tab: true,
-              tabSelected: props.selected === tab.value,
+              tabDefault: true,
+              tabSelected: isTabSelected,
             })}
             onClick={() => {
               props.setSelected(tab.value);
             }}
           >
-            {tab.title}
-          </div>
+            <Text size={16} weight={700}>
+              {tab.title}
+            </Text>
+          </button>
         );
       })}
     </div>
@@ -41,13 +57,15 @@ const styles = sx.create({
     display: 'flex',
     flexWrap: 'wrap',
   },
-  tab: {
-    'fontWeight': 'bold',
+  tabDefault: {
     'paddingInline': 20,
     'paddingBlock': 15,
     'cursor': 'pointer',
     'color': 'rgba(var(--sx-accent-6))',
-    'borderBlockEnd': '1px solid rgba(var(--sx-accent-2))',
+    'backgroundColor': 'rgba(var(--sx-background))',
+    'borderInline': 0,
+    'borderBlockStart': 0,
+    'borderBlockEnd': '2px solid rgba(var(--sx-accent-1))',
     ':hover': {
       color: 'rgba(var(--sx-foreground))',
       borderBlockEnd: '2px solid rgba(var(--sx-accent-2))',
