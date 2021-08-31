292de34098800020264aa63e73a0209433912bef
Martin Zlámal <mrtnzlml@gmail.com>
2021-08-31 08:45:11 -0500

    Abacus KO: acknowledge registered trademark

--- src/abacus-kochka/src/SocialMediaIcons.js
+++ src/abacus-kochka/src/SocialMediaIcons.js
@@ -1,5 +1,6 @@
 // @flow
 
+import { Tooltip } from '@adeira/sx-design';
 import * as React from 'react';
 import sx from '@adeira/sx';
 import fbt from 'fbt';
@@ -19,40 +20,64 @@ export default function SocialMediaIcons(props: Props): React.Node {
   return (
     <div className={styles('icons', props.vertical && 'iconsVertical')}>
       <div className={styles(props.vertical ? 'iconVertical' : 'icon')}>
-        <a
-          href={socialLinks.instagramURL}
-          aria-label={
-            <fbt desc="aria label of our Instagram link">link to KOCHKA Café Instagram page</fbt>
+        <Tooltip
+          title={
+            <fbt desc="instagram" doNotExtract={true}>
+              Instagram
+            </fbt>
           }
-          target="_blank"
-          rel="noreferrer"
         >
-          <Instagram color={props.color} size={props.size} />
-        </a>
+          <a
+            href={socialLinks.instagramURL}
+            aria-label={
+              <fbt desc="aria label of our Instagram link">link to KOCHKA Café Instagram page</fbt>
+            }
+            target="_blank"
+            rel="noreferrer"
+          >
+            <Instagram color={props.color} size={props.size} />
+          </a>
+        </Tooltip>
       </div>
 
       <div className={styles(props.vertical ? 'iconVertical' : 'icon')}>
-        <a
-          href={socialLinks.facebookURL}
-          aria-label={
-            <fbt desc="aria label of our Facebook link">link to KOCHKA Café facebook page</fbt>
+        <Tooltip
+          title={
+            <fbt desc="facebook" doNotExtract={true}>
+              Facebook
+            </fbt>
           }
-          target="_blank"
-          rel="noreferrer"
         >
-          <Facebook color={props.color} size={props.size} />
-        </a>
+          <a
+            href={socialLinks.facebookURL}
+            aria-label={
+              <fbt desc="aria label of our Facebook link">link to KOCHKA Café facebook page</fbt>
+            }
+            target="_blank"
+            rel="noreferrer"
+          >
+            <Facebook color={props.color} size={props.size} />
+          </a>
+        </Tooltip>
       </div>
 
       <div className={styles(props.vertical ? 'iconVertical' : 'icon')}>
-        <a
-          href={socialLinks.gitHubURL}
-          aria-label={<fbt desc="aria label of our GitHub link">link to GitHub repository</fbt>}
-          target="_blank"
-          rel="noreferrer"
+        <Tooltip
+          title={
+            <fbt desc="github" doNotExtract={true}>
+              GitHub
+            </fbt>
+          }
         >
-          <GitHub color={props.color} size={props.size} />
-        </a>
+          <a
+            href={socialLinks.gitHubURL}
+            aria-label={<fbt desc="aria label of our GitHub link">link to GitHub repository</fbt>}
+            target="_blank"
+            rel="noreferrer"
+          >
+            <GitHub color={props.color} size={props.size} />
+          </a>
+        </Tooltip>
       </div>
     </div>
   );
