fc2c8b4874abe2925ab7601898f39fd58aaed826
Martin Zlámal <mrtnzlml@gmail.com>
2021-09-03 08:33:31 -0500

    Relay: remove persist mode from the compiler
    I am not updating the changelog on purpose because this was never really
    advertised, or we explicitly discouraged people to use it. I am doing so
    in preparation for the new Relay Rust Compiler (cleanup).

--- src/relay/src/compiler/persistFunctions/createQueryID.js
+++
@@ -1,9 +0,0 @@
-// @flow strict
-
-import crypto from 'crypto';
-
-export default function createQueryID(text: string): string {
-  const hasher = crypto.createHash('md5');
-  hasher.update(text);
-  return hasher.digest('hex');
-}
