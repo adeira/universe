47942883b9b1298be2d9bee3b1c75975accf917e
Martin Zlámal <mrtnzlml@gmail.com>
2021-09-23 18:44:47 -0500

    SX Design: allow React nodes as header and cell children

--- src/abacus-backoffice/src/employees/EmployeesPage.js
+++ src/abacus-backoffice/src/employees/EmployeesPage.js
@@ -25,31 +25,25 @@ export default function EmployeesPage(): React.Node {
       columns={[
         { Header: <fbt desc="status of an employee record">Status</fbt>, accessor: 'col1' },
         { Header: <fbt desc="name of the employee">Name</fbt>, accessor: 'col2' },
-        // $FlowFixMe[incompatible-type]: https://github.com/adeira/universe/pull/3169
         { Header: () => null, accessor: 'col3' },
       ]}
       data={data.auth.listUsers.map((user) => {
         return {
           col1: user.isActive ? (
-            // $FlowFixMe[incompatible-type]: https://github.com/adeira/universe/pull/3169
             <Badge tint="success">
               <fbt desc="active user - label title">active</fbt>
             </Badge>
           ) : (
-            // $FlowFixMe[incompatible-type]: https://github.com/adeira/universe/pull/3169
             <Badge tint="warning">
               <fbt desc="inactive user - label title">inactive</fbt>
             </Badge>
           ),
-          // $FlowFixMe[incompatible-type]: https://github.com/adeira/universe/pull/3169
           col2: user.name ?? <MissingData />,
           col3: user.hasEmailVerified ? (
-            // $FlowFixMe[incompatible-type]: https://github.com/adeira/universe/pull/3169
             <Badge tint="success">
               <fbt desc="user has email verifies - label title">email verified</fbt>
             </Badge>
           ) : (
-            // $FlowFixMe[incompatible-type]: https://github.com/adeira/universe/pull/3169
             <Badge tint="error">
               <fbt desc="user doesn't have email verifies - label title">email NOT verified</fbt>
             </Badge>
