6164dcbe804ffcf29741cf1541d2f7e774ea41a2
Martin Zlámal <mrtnzlml@gmail.com>
2021-09-24 13:36:08 -0500

    Abacus BO: use `Table` component for employees

--- src/abacus-backoffice/src/employees/EmployeesPage.js
+++ src/abacus-backoffice/src/employees/EmployeesPage.js
@@ -1,6 +1,6 @@
 // @flow
 
-import { Badge, Entity, EntityField, MissingData } from '@adeira/sx-design';
+import { Badge, MissingData, Table } from '@adeira/sx-design';
 import * as React from 'react';
 import { graphql, useLazyLoadQuery } from '@adeira/relay';
 import fbt from 'fbt';
@@ -12,7 +12,6 @@ export default function EmployeesPage(): React.Node {
     query EmployeesPageQuery {
       auth {
         listUsers {
-          id
           name
           hasEmailVerified
           isActive
@@ -21,37 +20,42 @@ export default function EmployeesPage(): React.Node {
     }
   `);
 
-  return data.auth.listUsers.map((user) => {
-    return (
-      <Entity key={user.id}>
-        <EntityField
-          title={
-            user.isActive ? (
-              <Badge tint="success">
-                <fbt desc="active user - label title">active</fbt>
-              </Badge>
-            ) : (
-              <Badge tint="warning">
-                <fbt desc="inactive user - label title">inactive</fbt>
-              </Badge>
-            )
-          }
-        />
-        <EntityField title={user.name ?? <MissingData />} />
-        <EntityField
-          title={
-            user.hasEmailVerified ? (
-              <Badge tint="success">
-                <fbt desc="user has email verifies - label title">email verified</fbt>
-              </Badge>
-            ) : (
-              <Badge tint="error">
-                <fbt desc="user doesn't have email verifies - label title">email NOT verified</fbt>
-              </Badge>
-            )
-          }
-        />
-      </Entity>
-    );
-  });
+  return (
+    <Table
+      columns={[
+        { Header: <fbt desc="status of an employee record">Status</fbt>, accessor: 'col1' },
+        { Header: <fbt desc="name of the employee">Name</fbt>, accessor: 'col2' },
+        // $FlowFixMe[incompatible-type]: https://github.com/adeira/universe/pull/3169
+        { Header: () => null, accessor: 'col3' },
+      ]}
+      data={data.auth.listUsers.map((user) => {
+        return {
+          col1: user.isActive ? (
+            // $FlowFixMe[incompatible-type]: https://github.com/adeira/universe/pull/3169
+            <Badge tint="success">
+              <fbt desc="active user - label title">active</fbt>
+            </Badge>
+          ) : (
+            // $FlowFixMe[incompatible-type]: https://github.com/adeira/universe/pull/3169
+            <Badge tint="warning">
+              <fbt desc="inactive user - label title">inactive</fbt>
+            </Badge>
+          ),
+          // $FlowFixMe[incompatible-type]: https://github.com/adeira/universe/pull/3169
+          col2: user.name ?? <MissingData />,
+          col3: user.hasEmailVerified ? (
+            // $FlowFixMe[incompatible-type]: https://github.com/adeira/universe/pull/3169
+            <Badge tint="success">
+              <fbt desc="user has email verifies - label title">email verified</fbt>
+            </Badge>
+          ) : (
+            // $FlowFixMe[incompatible-type]: https://github.com/adeira/universe/pull/3169
+            <Badge tint="error">
+              <fbt desc="user doesn't have email verifies - label title">email NOT verified</fbt>
+            </Badge>
+          ),
+        };
+      })}
+    />
+  );
 }
