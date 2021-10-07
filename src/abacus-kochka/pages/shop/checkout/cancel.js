f47ad3cfcc8a6f6c755d948e8fd7f6a396e9200f
Martin Zlámal <mrtnzlml@gmail.com>
2021-10-06 20:55:21 -0500

    Abacus KO: add simple checkout pages

---
+++ src/abacus-kochka/pages/shop/checkout/cancel.js
@@ -0,0 +1,22 @@
+// @flow
+
+import { Note } from '@adeira/sx-design';
+import fbt from 'fbt';
+import React, { type Node } from 'react';
+
+import Layout from '../../../src/Layout';
+
+export default function ShopCheckoutCancelPage(): Node {
+  // TODO: this page should probably not be accessible directly
+
+  return (
+    <Layout title={<fbt desc="canceled checkout page title">Order canceled</fbt>}>
+      <Note tint="warning">
+        <fbt desc="canceled checkout message">
+          Your order has been canceled and no money was charged. We are sorry to see you go. Feel
+          free to get in touch with us and tell us what can we improve!
+        </fbt>
+      </Note>
+    </Layout>
+  );
+}
