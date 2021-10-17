c7865a87455993d491188371154df9a764a5ec0f
Martin Zlámal <mrtnzlml@gmail.com>
2021-10-17 15:08:43 -0500

    Abacus BO: use production GraphQL URL

--- src/abacus-backoffice/src/constants.js
+++ src/abacus-backoffice/src/constants.js
@@ -1,6 +1,6 @@
 // @flow strict
 
 export default {
-  graphqlServerURL: 'http://localhost:5000/graphql', // TODO: HTTPS, better "abacus" domain
+  graphqlServerURL: 'https://abacus.kochka.com.mx/graphql',
   googleClientID: '586578400209-5k61strd7i03b7pr2arn38o9qqghgfeq.apps.googleusercontent.com',
 };
