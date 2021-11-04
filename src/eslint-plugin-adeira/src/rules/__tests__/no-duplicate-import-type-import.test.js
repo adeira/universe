9d27ac5705a5c83fa3ffa5d66fc925266d5aef3f
Martin Zlámal <mrtnzlml@gmail.com>
2021-11-04 16:41:43 -0600

    Eslint plugin: fix `no-duplicate-import` rule for side-effect imports
    I decided to ignore side-effect imports completely because it felt
    strange to be enforcing consolidation of side-effects and type imports.
    
    Closes: https://github.com/adeira/universe/issues/3410

--- src/eslint-plugin-adeira/src/rules/__tests__/no-duplicate-import-type-import.test.js
+++ src/eslint-plugin-adeira/src/rules/__tests__/no-duplicate-import-type-import.test.js
@@ -15,17 +15,29 @@ ruleTester.run('no-duplicate-import', rule, {
     },
     {
       code: `import Foo, { type FooLol } from 'foo';
-    import Bar, { type BarLol} from 'bar';
-    import Lol from 'lol';`,
+import Bar, { type BarLol} from 'bar';
+import Lol from 'lol';`,
     },
     {
       // These are handled by imports/no-duplicate
       code: `import { Foo } from 'foo';
-      import { Bar } from 'foo';`,
+import { Bar } from 'foo';`,
     },
     {
       code: `import type { Foo } from 'foo'
-    import type { Bar } from 'foo'`,
+import type { Bar } from 'foo'`,
+    },
+    {
+      // https://github.com/adeira/universe/issues/3410
+      code: `import '@material/mwc-button';
+import '@material/mwc-dialog';
+import '@material/mwc-drawer';
+import '@material/mwc-icon-button';
+import '@material/mwc-list';
+import '@material/mwc-menu';
+import './WebComponents/TopAppBar';
+import './WebComponents/MaterialDrawer';
+import type {Dialog} from "@material/mwc-dialog";`,
     },
   ],
 
