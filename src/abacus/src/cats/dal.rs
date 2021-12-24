dc6c86b22adac5855554dd68ca0def43822d1108
Martin Zlámal <mrtnzlml@gmail.com>
2021-12-24 12:44:48 -0600

    Abacus: expose "cat dates" in GraphQL

--- src/abacus/src/cats/dal.rs
+++ src/abacus/src/cats/dal.rs
@@ -8,6 +8,9 @@ pub struct CatInfo {
     name: String,
     description: String,
     order: i32,
+    date_adoption: Option<String>,
+    date_deworming: Option<String>,
+    date_castration: Option<String>,
 }
 
 #[juniper::graphql_object]
@@ -27,6 +30,21 @@ impl CatInfo {
     /// Order in which the cat was admitted to KOCHKA Café.
     fn order(&self) -> i32 {
         self.order.to_owned()
+    }
+
+    /// When was the cat adopted (when it leaved KOCHKA Café).
+    fn date_of_adoption(&self) -> Option<String> {
+        self.date_adoption.to_owned()
+    }
+
+    /// When was the cat dewormed for the last time.
+    fn date_of_deworming(&self) -> Option<String> {
+        self.date_deworming.to_owned()
+    }
+
+    /// When was the cat castrated.
+    fn date_of_castration(&self) -> Option<String> {
+        self.date_castration.to_owned()
     }
 }
 
