3857fadee8b32c021293ba39c8d182085383a869
Martin Zlámal <mrtnzlml@gmail.com>
2022-01-13 16:31:33 -0600

    Abacus: remove tracking module

--- src/abacus/server/src/migrations/v6__create_collection_tracking.rs
+++
@@ -1,7 +0,0 @@
-use crate::arango::collection::CollectionType;
-use crate::arango::DatabaseType;
-use crate::migrations::utils::create_collection;
-
-pub async fn migrate(db: &DatabaseType) -> anyhow::Result<()> {
-    create_collection(db, "tracking", &CollectionType::Edge, &None).await
-}
