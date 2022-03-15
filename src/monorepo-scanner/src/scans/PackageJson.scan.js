82efce6841433f5bfd3173c954d7a06bed79209c
Martin Zlámal <mrtnzlml@gmail.com>
2022-03-15 13:35:23 -0600

    Yarn: publish `plugin-monorepo-scanner.js`
    I also removed some scans which I considered less useful.
    
    Related issue: https://github.com/adeira/universe/issues/4141

--- src/monorepo-scanner/src/scans/PackageJson.scan.js
+++ src/monorepo-scanner/src/scans/PackageJson.scan.js
@@ -9,23 +9,12 @@ Workspaces.iterateWorkspaces((packageJSONLocation) => {
   test(`${packageJson.name}`, () => {
     const dirname = path.dirname(packageJSONLocation);
     const main = packageJson.main;
-    const type = packageJson.type;
 
     if (main != null) {
       const mainEntrypoint = path.join(dirname, main);
       // $FlowIssue[incompatible-call]: https://github.com/facebook/flow/issues/3018
       expect(fs.existsSync(mainEntrypoint)).toGiveHelp(
         `The file specified in main field does not exist (${mainEntrypoint}). If this is intentional, you can remove this field from package.json`,
-      );
-
-      // $FlowIssue[incompatible-call]: https://github.com/facebook/flow/issues/3018
-      expect(type != null).toGiveHelp(
-        `Each package must specify "type" to be either "commonjs" or "module".`,
-      );
-
-      // $FlowIssue[incompatible-call]: https://github.com/facebook/flow/issues/3018
-      expect(['commonjs', 'module'].includes(type)).toGiveHelp(
-        `Package type must be either "commonjs" or "module" (given "${type}").`,
       );
     }
   });
--- src/monorepo-scanner/src/scans/Workspaces.scan.js
+++ src/monorepo-scanner/src/scans/Workspaces.scan.js
@@ -8,42 +8,7 @@ Workspaces.iterateWorkspaces((packageJSONLocation) => {
   test(`${packageJSONLocation}`, () => {
     const packageJson = require(packageJSONLocation);
 
-    // Packages 'eslint-plugin-*' are the only exception since it wasn't
-    // possible to have scoped packages in Eslint. Dunno if it's possible now.
-    // $FlowIssue[incompatible-call]: https://github.com/facebook/flow/issues/3018
-    expect(/^eslint-plugin-.+|^@adeira\/.+/.test(packageJson.name) === true).toGiveHelp(
-      `All packages in our monorepo must start with '@adeira/' prefix. This name is not valid: ${packageJson.name}`,
-    );
-
-    if (packageJson.main !== undefined) {
-      // $FlowIssue[incompatible-call]: https://github.com/facebook/flow/issues/3018
-      expect(packageJson.main.endsWith('.js') === true).toGiveHelp(
-        "Field 'main' should have '.js' extension.",
-      );
-    }
-
     if (packageJson.private !== true) {
-      expect(packageJson.description).toBeDefined();
-
-      // We currently have only MIT and "Unlicense" packages.
-      // https://docs.npmjs.com/files/package.json#license
-      expect(['MIT', 'Unlicense']).toContain(packageJson.license);
-
-      expect(packageJson.homepage).toMatch(
-        new RegExp(
-          `^https://github\\.com/adeira/universe|https://github\\.com/adeira/${path.basename(
-            path.dirname(packageJSONLocation),
-          )}$`,
-        ),
-      );
-
-      expect(packageJson.bugs).toMatch('https://github.com/adeira/universe/issues');
-
-      expect(packageJson.repository).toBeDefined();
-
-      // each public package must specify `main` or `bin` field to be useful
-      expect(packageJson.main !== undefined || packageJson.bin !== undefined).toBe(true);
-
       const packagePath = path.dirname(packageJSONLocation);
       ['README.md', 'LICENSE', '.npmignore'].forEach((requiredFilename) => {
         if (!fs.existsSync(path.join(packagePath, requiredFilename))) {
@@ -51,23 +16,5 @@ Workspaces.iterateWorkspaces((packageJSONLocation) => {
         }
       });
     }
-
-    expect(packageJson.version).toBeDefined();
-
-    const dependencies = Object.entries(packageJson.dependencies ?? {}).concat(
-      Object.entries(packageJson.devDependencies ?? {}),
-    );
-
-    dependencies.forEach(([dependency, value]) => {
-      const version = typeof value === 'string' ? value : '^';
-      if (!/^(?:[~^]?[0-9]+(?:\.[0-9]+){2}|http.+|\.\/[a-zA-Z0-9-_/.]+\.tgz)/.test(version)) {
-        // this is necessary because we repeatedly released package without
-        // bumped dependency version which made the released package broken
-        // (we should probably use tilde ranges for our workspace deps though)
-        throw new Error(
-          `Please be more explicit about the required versions inside monorepo. Package ${packageJson.name} required dependency ${dependency} with version: ${version}`,
-        );
-      }
-    });
   });
 });
