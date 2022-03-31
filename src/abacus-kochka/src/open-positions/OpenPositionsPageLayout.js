62f43e8b6b3825e6384e0c614b07c29ea58d715a
Martin Zlámal <mrtnzlml@gmail.com>
2022-03-31 14:03:16 -0600

    Abacus KO: update open positions

--- src/abacus-kochka/src/open-positions/OpenPositionsPageLayout.js
+++ src/abacus-kochka/src/open-positions/OpenPositionsPageLayout.js
@@ -4,11 +4,12 @@ import { LayoutBlock, Link } from '@adeira/sx-design';
 import * as React from 'react';
 import fbt from 'fbt';
 
+import FullTimeBadge from './badges/FullTimeBadge';
 import Layout from '../Layout';
 import OpenPosition from './OpenPosition';
-import FullTimeBadge from './badges/FullTimeBadge';
 import PositionAvailableBadge from './badges/PositionAvailableBadge';
 import PositionUnavailableBadge from './badges/PositionUnavailableBadge';
+import SalaryBadge from './badges/SalaryBadge';
 
 export default function OpenPositionsPageLayout(): React.Node {
   return (
@@ -25,11 +26,35 @@ export default function OpenPositionsPageLayout(): React.Node {
          */}
         <OpenPosition
           title={
-            <fbt desc="barista position title">
-              Person in charge of beverage preparation (Barista)
+            <fbt desc="assistant position title">
+              Person responsible for our premises and customers (Ayudante General)
             </fbt>
           }
-          badges={[<PositionUnavailableBadge key={1} />, <FullTimeBadge key={2} />]}
+          badges={[
+            <PositionAvailableBadge key={1} />,
+            <FullTimeBadge key={2} />,
+            <SalaryBadge key={3} />,
+          ]}
+          description={
+            <fbt desc="assistant position description">
+              Person in this position is responsible for customer service, keeping the facilities
+              clean, and assisting in the food preparation sector. This is a full-time position
+              only.
+            </fbt>
+          }
+        />
+
+        <OpenPosition
+          title={
+            <fbt desc="barista position title">
+              Person responsible for beverage preparation (Barista)
+            </fbt>
+          }
+          badges={[
+            <PositionUnavailableBadge key={1} />,
+            <FullTimeBadge key={2} />,
+            <SalaryBadge key={3} />,
+          ]}
           description={
             <fbt desc="barista position description">
               Barista is responsible for preparation of coffee based drinks, teas, milkshakes and
@@ -40,29 +65,17 @@ export default function OpenPositionsPageLayout(): React.Node {
         />
 
         <OpenPosition
-          title={<fbt desc="chef position title">Person in charge of food preparation</fbt>}
-          badges={[<PositionUnavailableBadge key={1} />, <FullTimeBadge key={2} />]}
+          title={<fbt desc="chef position title">Person responsible for food preparation</fbt>}
+          badges={[
+            <PositionUnavailableBadge key={1} />,
+            <FullTimeBadge key={2} />,
+            <SalaryBadge key={3} />,
+          ]}
           description={
             <fbt desc="chef position description">
               Person in charge of food preparation is responsible for preparing and improving our
               food options as well as attending our clients and keeping the workplace clean. This is
               a full-time position only.
-            </fbt>
-          }
-        />
-
-        <OpenPosition
-          title={
-            <fbt desc="assistant position title">
-              Person in charge of our premises and customers
-            </fbt>
-          }
-          badges={[<PositionAvailableBadge key={1} />, <FullTimeBadge key={2} />]}
-          description={
-            <fbt desc="assistant position description">
-              Person in this position is responsible for customer service, keeping the facilities
-              clean, and assisting in the food preparation sector. This is a full-time position
-              only.
             </fbt>
           }
         />
