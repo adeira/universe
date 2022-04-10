23e838e4953870b18a7d747f7a1ab362af94b4a8
Martin Zlámal <mrtnzlml@gmail.com>
2022-04-10 17:17:45 -0500

    Abacus KO: update open positions

--- src/abacus-kochka/src/open-positions/OpenPositionsPageLayout.js
+++ src/abacus-kochka/src/open-positions/OpenPositionsPageLayout.js
@@ -7,7 +7,6 @@ import fbt from 'fbt';
 import FullTimeBadge from './badges/FullTimeBadge';
 import Layout from '../Layout';
 import OpenPosition from './OpenPosition';
-import PositionAvailableBadge from './badges/PositionAvailableBadge';
 import PositionUnavailableBadge from './badges/PositionUnavailableBadge';
 import SalaryBadge from './badges/SalaryBadge';
 
@@ -31,7 +30,7 @@ export default function OpenPositionsPageLayout(): React.Node {
             </fbt>
           }
           badges={[
-            <PositionAvailableBadge key={1} />,
+            <PositionUnavailableBadge key={1} />,
             <FullTimeBadge key={2} />,
             <SalaryBadge key={3} />,
           ]}
