33e56d0d8d3f773d99e39a90b4f439bec9992aa0
Martin Zlámal <mrtnzlml@gmail.com>
2022-06-04 12:21:10 -0500

    Remove Relay Example repository
    It's still publicly available here: https://github.com/adeira/relay-example
    
    Closes: https://github.com/adeira/universe/issues/4497

--- src/example-relay/scripts/jest/custom-transformer.js
+++
@@ -1,8 +0,0 @@
-// @flow
-
-const path = require('path');
-const babelJest = require('babel-jest').default; // https://github.com/facebook/jest/issues/11444
-
-module.exports = (babelJest.createTransformer(
-  require(path.join(__dirname, '..', '..', '.babelrc.js')),
-) /*: any */);
