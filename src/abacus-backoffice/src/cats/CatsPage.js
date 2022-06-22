34791c25066e253a149296cfb591bcbee6ae83aa
Martin Zlámal <mrtnzlml@gmail.com>
2022-06-22 14:10:24 -0500

    Abacus BO: show number of cats

--- src/abacus-backoffice/src/cats/CatsPage.js
+++ src/abacus-backoffice/src/cats/CatsPage.js
@@ -1,9 +1,8 @@
 // @flow
 
-import { LayoutBlock, Text } from '@adeira/sx-design';
+import { LayoutBlock } from '@adeira/sx-design';
 import * as React from 'react';
 import { graphql, useLazyLoadQuery } from '@adeira/relay';
-import fbt from 'fbt';
 
 import CatsTableAdopted from './CatsTableAdopted';
 import CatsTableCurrent from './CatsTableCurrent';
@@ -21,29 +20,8 @@ export default function CatsPage(): React.Node {
 
   return (
     <LayoutBlock spacing="large">
-      <LayoutBlock spacing="none">
-        <Text as="h2">
-          <fbt desc="title of a section of currently available cats">Currently available cats</fbt>
-        </Text>
-        <p>
-          <fbt desc="description of a section of currently available cats">
-            List of cats currently living in KOCHKA Café.
-          </fbt>
-        </p>
-        <CatsTableCurrent data={data.cats} />
-      </LayoutBlock>
-
-      <LayoutBlock spacing="none">
-        <Text as="h2">
-          <fbt desc="title of a section of already adopted cats">Adopted cats</fbt>
-        </Text>
-        <p>
-          <fbt desc="description of a section of already adopted cats">
-            List of cats that are no longer in KOCHKA Café because they were adopted.
-          </fbt>
-        </p>
-        <CatsTableAdopted data={data.cats} />
-      </LayoutBlock>
+      <CatsTableCurrent data={data.cats} />
+      <CatsTableAdopted data={data.cats} />
     </LayoutBlock>
   );
 }
