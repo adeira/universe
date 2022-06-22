f434607827936e879b2c12fa421879a99eb1a859
Martin Zlámal <mrtnzlml@gmail.com>
2022-06-22 14:10:24 -0500

    Abacus BO: show number of cats

--- src/abacus-backoffice/src/cats/CatsTableAdopted.js
+++ src/abacus-backoffice/src/cats/CatsTableAdopted.js
@@ -1,9 +1,9 @@
 // @flow
 
-import { MissingData, Table, DateTime } from '@adeira/sx-design';
+import { MissingData, Table, DateTime, LayoutBlock, Text } from '@adeira/sx-design';
 import fbt from 'fbt';
 import { useFragment, graphql } from '@adeira/relay';
-import type { Node } from 'react';
+import React, { type Node } from 'react';
 
 import TableCellTripleCuadrupleFelina from './TableCellTripleCuadrupleFelina';
 import type { CatsTableAdoptedFragment$key } from './__generated__/CatsTableAdoptedFragment.graphql';
@@ -32,88 +32,104 @@ export default function CatsTableAdopted(props: Props): Node {
   );
 
   return (
-    <Table
-      columns={[
-        {
-          Header: <fbt desc="order of the cat (table header)">Order</fbt>,
-          accessor: 'col1',
-        },
-        {
-          Header: <fbt desc="name of the cat (table header)">Name of the cat</fbt>,
-          accessor: 'col2',
-        },
-        {
-          Header: (
-            <fbt desc="date when the cat castration was performed (table header)">
-              Date of castration
-            </fbt>
-          ),
-          accessor: 'col3',
-        },
-        {
-          Header: (
-            <fbt desc="date of the last deworming (table header)">Date of last deworming</fbt>
-          ),
-          accessor: 'col4',
-        },
-        {
-          Header: <fbt desc="date of the cat adoption (table header)">Date of adoption</fbt>,
-          accessor: 'col5',
-        },
-        {
-          Header: <fbt desc="rabies vaccination (table header)">Rabies vaccination</fbt>,
-          accessor: 'col6',
-        },
-        {
-          Header: (
-            <fbt desc="vaccination triple/cuadruple felina (table header)">
-              Vaccination &quot;triple/cuadruple felina&quot;
-            </fbt>
-          ),
-          accessor: 'col7',
-        },
-        {
-          Header: (
-            <fbt desc="vaccination leucemia felina (table header)">
-              Vaccination &quot;leucemia felina&quot;
-            </fbt>
-          ),
-          accessor: 'col8',
-        },
-      ]}
-      data={cats.adoptedCats.map((cat) => {
-        return {
-          col1: `#${cat.order}`,
-          col2: cat.name,
-          col3:
-            cat.dateOfCastration != null ? (
-              <DateTime value={cat.dateOfCastration} />
-            ) : (
-              <MissingData />
+    <LayoutBlock spacing="none">
+      <Text as="h2">
+        <fbt desc="title of a section of already adopted cats">
+          Adopted cats (<fbt:param name="numberOfAdoptedCats">{cats.adoptedCats.length}</fbt:param>)
+        </fbt>
+      </Text>
+      <p>
+        <fbt desc="description of a section of already adopted cats">
+          List of cats that are no longer in KOCHKA Café because they were adopted.
+        </fbt>
+      </p>
+      <Table
+        columns={[
+          {
+            Header: <fbt desc="order of the cat (table header)">Order</fbt>,
+            accessor: 'order',
+          },
+          {
+            Header: <fbt desc="name of the cat (table header)">Name of the cat</fbt>,
+            accessor: 'name',
+          },
+          {
+            Header: (
+              <fbt desc="date when the cat castration was performed (table header)">
+                Date of castration
+              </fbt>
             ),
-          col4:
-            cat.dateOfDeworming != null ? (
-              <DateTime value={cat.dateOfDeworming} />
-            ) : (
-              <MissingData />
+            accessor: 'date_of_castration',
+          },
+          {
+            Header: (
+              <fbt desc="date of the last deworming (table header)">Date of last deworming</fbt>
             ),
-          col5:
-            cat.dateOfAdoption != null ? <DateTime value={cat.dateOfAdoption} /> : <MissingData />,
-          col6:
-            cat.dateOfVaccinationRabies != null ? (
-              <DateTime value={cat.dateOfVaccinationRabies} />
-            ) : (
-              <MissingData />
+            accessor: 'date_of_deworming',
+          },
+          {
+            Header: <fbt desc="date of the cat adoption (table header)">Date of adoption</fbt>,
+            accessor: 'date_of_adoption',
+          },
+          {
+            Header: <fbt desc="rabies vaccination (table header)">Rabies vaccination</fbt>,
+            accessor: 'date_of_rabies_vaccination',
+          },
+          {
+            Header: (
+              <fbt desc="vaccination triple/cuadruple felina (table header)">
+                Vaccination &quot;triple/cuadruple felina&quot;
+              </fbt>
             ),
-          col7: <TableCellTripleCuadrupleFelina data={cat} />,
-          col8:
-            cat.dateOfVaccinationLeucemiaFelina != null ? (
-              <DateTime value={cat.dateOfVaccinationLeucemiaFelina} />
-            ) : (
-              <MissingData />
+            accessor: 'date_of_cuadruple_feline_vacination',
+          },
+          {
+            Header: (
+              <fbt desc="vaccination leucemia felina (table header)">
+                Vaccination &quot;leucemia felina&quot;
+              </fbt>
             ),
-        };
-      })}
-    />
+            accessor: 'date_of_leucamia_vacination',
+          },
+        ]}
+        data={cats.adoptedCats.map((cat) => {
+          return {
+            order: `#${cat.order}`,
+            name: cat.name,
+            date_of_castration:
+              cat.dateOfCastration != null ? (
+                <DateTime value={cat.dateOfCastration} />
+              ) : (
+                <MissingData />
+              ),
+            date_of_deworming:
+              cat.dateOfDeworming != null ? (
+                <DateTime value={cat.dateOfDeworming} />
+              ) : (
+                <MissingData />
+              ),
+            date_of_adoption:
+              cat.dateOfAdoption != null ? (
+                <DateTime value={cat.dateOfAdoption} />
+              ) : (
+                <MissingData />
+              ),
+            date_of_rabies_vaccination:
+              cat.dateOfVaccinationRabies != null ? (
+                <DateTime value={cat.dateOfVaccinationRabies} />
+              ) : (
+                <MissingData />
+              ),
+            date_of_cuadruple_feline_vacination: <TableCellTripleCuadrupleFelina data={cat} />,
+            date_of_leucamia_vacination:
+              cat.dateOfVaccinationLeucemiaFelina != null ? (
+                <DateTime value={cat.dateOfVaccinationLeucemiaFelina} />
+              ) : (
+                <MissingData />
+              ),
+          };
+        })}
+      />
+    </LayoutBlock>
   );
 }
--- src/abacus-backoffice/src/cats/CatsTableCurrent.js
+++ src/abacus-backoffice/src/cats/CatsTableCurrent.js
@@ -1,9 +1,9 @@
 // @flow
 
 import { graphql, useFragment } from '@adeira/relay';
-import { MissingData, Table, Tooltip, DateTime } from '@adeira/sx-design';
+import { MissingData, Table, Tooltip, DateTime, Text, LayoutBlock } from '@adeira/sx-design';
 import fbt from 'fbt';
-import { type Node } from 'react';
+import React, { type Node } from 'react';
 
 import TableCellTripleCuadrupleFelina from './TableCellTripleCuadrupleFelina';
 import type { CatsTableCurrentFragment$key } from './__generated__/CatsTableCurrentFragment.graphql';
@@ -32,102 +32,116 @@ export default function CatsTableCurrent(props: Props): Node {
   );
 
   return (
-    <Table
-      columns={[
-        {
-          Header: <fbt desc="order of the cat (table header)">Order</fbt>,
-          accessor: 'col1',
-        },
-        {
-          Header: <fbt desc="name of the cat (table header)">Name of the cat</fbt>,
-          accessor: 'col2',
-        },
-        {
-          Header: (
-            <fbt desc="date when the cat castration was performed (table header)">
-              Date of castration
-            </fbt>
-          ),
-          accessor: 'col3',
-        },
-        {
-          Header: (
-            <fbt desc="date of the last deworming (table header)">Date of last deworming</fbt>
-          ),
-          accessor: 'col4',
-        },
-        {
-          Header: <fbt desc="rabies vaccination (table header)">Rabies vaccination</fbt>,
-          accessor: 'col5',
-        },
-        {
-          Header: (
-            <fbt desc="vaccination triple/cuadruple felina (table header)">
-              Vaccination &quot;triple/cuadruple felina&quot;
-            </fbt>
-          ),
-          accessor: 'col6',
-        },
-        {
-          Header: (
-            <fbt desc="vaccination leucemia felina (table header)">
-              Vaccination &quot;leucemia felina&quot;
-            </fbt>
-          ),
-          accessor: 'col7',
-        },
-      ]}
-      data={cats.currentCats.map((cat) => {
-        return {
-          col1: `#${cat.order}`,
-          col2: (
-            <>
-              {cat.name}
-              {cat.canBeAdopted === false ? (
-                <>
-                  {' '}
-                  <Tooltip
-                    title={
-                      <fbt desc="explanation of a cat that cannot be adopted">
-                        This cat cannot be adopted because it is ours.
-                      </fbt>
-                    }
-                  >
-                    ❤️
-                  </Tooltip>
-                </>
-              ) : null}
-            </>
-          ),
-          col3:
-            // TODO: display with warning/error when around 4 months old
-            cat.dateOfCastration != null ? (
-              <DateTime value={cat.dateOfCastration} />
-            ) : (
-              <MissingData />
+    <LayoutBlock spacing="none">
+      <Text as="h2">
+        <fbt desc="title of a section of currently available cats">
+          Currently available cats (<fbt:param name="numberOfCurrentlyAvailableCats">
+            {cats.currentCats.length}
+          </fbt:param>)
+        </fbt>
+      </Text>
+      <p>
+        <fbt desc="description of a section of currently available cats">
+          List of cats currently living in KOCHKA Café.
+        </fbt>
+      </p>
+      <Table
+        columns={[
+          {
+            Header: <fbt desc="order of the cat (table header)">Order</fbt>,
+            accessor: 'order',
+          },
+          {
+            Header: <fbt desc="name of the cat (table header)">Name of the cat</fbt>,
+            accessor: 'name',
+          },
+          {
+            Header: (
+              <fbt desc="date when the cat castration was performed (table header)">
+                Date of castration
+              </fbt>
             ),
-          col4:
-            // TODO: display with warning/error
-            cat.dateOfDeworming != null ? (
-              <DateTime value={cat.dateOfDeworming} />
-            ) : (
-              <MissingData />
+            accessor: 'date_of_castration',
+          },
+          {
+            Header: (
+              <fbt desc="date of the last deworming (table header)">Date of last deworming</fbt>
             ),
-          col5:
-            cat.dateOfVaccinationRabies != null ? (
-              <DateTime value={cat.dateOfVaccinationRabies} />
-            ) : (
-              <MissingData />
+            accessor: 'date_of_deworming',
+          },
+          {
+            Header: <fbt desc="rabies vaccination (table header)">Rabies vaccination</fbt>,
+            accessor: 'date_of_rabies_vaccination',
+          },
+          {
+            Header: (
+              <fbt desc="vaccination triple/cuadruple felina (table header)">
+                Vaccination &quot;triple/cuadruple felina&quot;
+              </fbt>
             ),
-          col6: <TableCellTripleCuadrupleFelina data={cat} />,
-          col7:
-            cat.dateOfVaccinationLeucemiaFelina != null ? (
-              <DateTime value={cat.dateOfVaccinationLeucemiaFelina} />
-            ) : (
-              <MissingData />
+            accessor: 'date_of_cuadruple_feline_vacination',
+          },
+          {
+            Header: (
+              <fbt desc="vaccination leucemia felina (table header)">
+                Vaccination &quot;leucemia felina&quot;
+              </fbt>
             ),
-        };
-      })}
-    />
+            accessor: 'date_of_leucamia_vacination',
+          },
+        ]}
+        data={cats.currentCats.map((cat) => {
+          return {
+            order: `#${cat.order}`,
+            name: (
+              <>
+                {cat.name}
+                {cat.canBeAdopted === false ? (
+                  <>
+                    {' '}
+                    <Tooltip
+                      title={
+                        <fbt desc="explanation of a cat that cannot be adopted">
+                          This cat cannot be adopted because it is ours.
+                        </fbt>
+                      }
+                    >
+                      ❤️
+                    </Tooltip>
+                  </>
+                ) : null}
+              </>
+            ),
+            date_of_castration:
+              // TODO: display with warning/error when around 4 months old
+              cat.dateOfCastration != null ? (
+                <DateTime value={cat.dateOfCastration} />
+              ) : (
+                <MissingData />
+              ),
+            date_of_deworming:
+              // TODO: display with warning/error
+              cat.dateOfDeworming != null ? (
+                <DateTime value={cat.dateOfDeworming} />
+              ) : (
+                <MissingData />
+              ),
+            date_of_rabies_vaccination:
+              cat.dateOfVaccinationRabies != null ? (
+                <DateTime value={cat.dateOfVaccinationRabies} />
+              ) : (
+                <MissingData />
+              ),
+            date_of_cuadruple_feline_vacination: <TableCellTripleCuadrupleFelina data={cat} />,
+            date_of_leucamia_vacination:
+              cat.dateOfVaccinationLeucemiaFelina != null ? (
+                <DateTime value={cat.dateOfVaccinationLeucemiaFelina} />
+              ) : (
+                <MissingData />
+              ),
+          };
+        })}
+      />
+    </LayoutBlock>
   );
 }
