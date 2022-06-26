ba7ccaf51fa1040733d584de6da3c6735233d97d
Martin Zlámal <mrtnzlml@gmail.com>
2022-06-26 14:12:45 -0500

    Abacus KO: use `next-compose-plugins` to configure Next.js

--- src/abacus-kochka/next.config.js
+++ src/abacus-kochka/next.config.js
@@ -1,22 +1,52 @@
 // @flow
 
-const path = require('path');
-const withTranspileModules = require('next-transpile-modules');
-const withCustomBabelConfigFile = require('next-plugin-custom-babel-config');
+/* eslint-disable import/order */
 
-const nextConfig = withCustomBabelConfigFile(
-  withTranspileModules([
-    '@adeira/css-colors',
-    '@adeira/fetch',
-    '@adeira/hooks',
-    '@adeira/icons',
-    '@adeira/js',
-    '@adeira/murmur-hash',
-    '@adeira/relay',
-    '@adeira/sx',
-    '@adeira/sx-design',
-  ])({
-    babelConfigFile: path.join(__dirname, '.babelrc.js'),
+const path = require('path');
+const withPlugins = require('next-compose-plugins');
+
+const withCustomBabelConfigFile = require('next-plugin-custom-babel-config')({
+  babelConfigFile: path.join(__dirname, '.babelrc.js'),
+});
+
+const withTranspileModules = require('next-transpile-modules')([
+  '@adeira/css-colors',
+  '@adeira/fetch',
+  '@adeira/hooks',
+  '@adeira/icons',
+  '@adeira/js',
+  '@adeira/murmur-hash',
+  '@adeira/relay',
+  '@adeira/sx',
+  '@adeira/sx-design',
+]);
+
+module.exports = (withPlugins(
+  [
+    [withCustomBabelConfigFile],
+    [withTranspileModules],
+    // other plugins here
+  ],
+  {
+    poweredByHeader: false,
+    reactStrictMode: true,
+    devIndicators: {
+      buildActivity: true,
+    },
+    i18n: {
+      locales: [
+        'en-us', // English in USA
+        'es-mx', // Spanish in Mexico
+      ],
+      defaultLocale: 'es-mx',
+    },
+    eslint: {
+      ignoreDuringBuilds: true,
+    },
+    experimental: {
+      esmExternals: 'loose',
+      runtime: 'nodejs',
+    },
     webpack: (nextConfig) => {
       nextConfig.module.rules.push({
         type: 'javascript/auto',
@@ -25,28 +55,5 @@ const nextConfig = withCustomBabelConfigFile(
 
       return nextConfig;
     },
-  }),
-);
-
-module.exports = ({
-  ...nextConfig,
-  poweredByHeader: false,
-  reactStrictMode: true,
-  devIndicators: {
-    buildActivity: true,
   },
-  i18n: {
-    locales: [
-      'en-us', // English in USA
-      'es-mx', // Spanish in Mexico
-    ],
-    defaultLocale: 'es-mx',
-  },
-  eslint: {
-    ignoreDuringBuilds: true,
-  },
-  experimental: {
-    esmExternals: 'loose',
-    runtime: 'nodejs',
-  },
-} /*: any */);
+) /*: $FlowFixMe */);
