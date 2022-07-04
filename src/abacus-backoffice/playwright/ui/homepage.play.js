35959c273ad562cc64df7e745dc27dedff87b0e6
Martin Zlámal <mrtnzlml@gmail.com>
2022-07-04 11:33:22 -0500

    Abacus BO: fix Playwright test

--- src/abacus-backoffice/playwright/ui/homepage.play.js
+++ src/abacus-backoffice/playwright/ui/homepage.play.js
@@ -3,5 +3,5 @@ const { test, expect } = require('@playwright/test');
 
 test('that homepage works', async ({ page, baseURL }) => {
   await page.goto(baseURL);
-  await expect(page).toHaveTitle(/^Abacus!$/);
+  await expect(page).toHaveTitle(/^Abacus$/);
 });
