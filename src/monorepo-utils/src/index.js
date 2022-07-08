77073be951c6647bb87fec1d0e79279d9b8fff44
Martin Zlámal <mrtnzlml@gmail.com>
2022-07-07 20:58:33 -0500

    Remove `TestRunner` and related packages

--- src/monorepo-utils/src/index.js
+++ src/monorepo-utils/src/index.js
@@ -10,6 +10,3 @@ export { default as Git } from './Git';
 export { default as Workspaces } from './Workspaces';
 
 export { globSync, globAsync } from './glob';
-
-export { default as getTouchedWorkspaces } from './getTouchedWorkspaces';
-export { default as getChangedFiles } from './getChangedFiles';
