df1c2515c683b0ab6cc45991aa09879d7d50cfa6
Martin Zlámal <mrtnzlml@gmail.com>
2022-07-07 23:22:47 -0500

    Run Eslint directly
    Simplifying all the machinery even further.

--- src/x/src/executors/lints.rs
+++ src/x/src/executors/lints.rs
@@ -1,9 +1,8 @@
 use crate::executors::{create_command, execute_command};
 
+const ESLINT_BIN: &str = "./node_modules/.bin/eslint";
 const ESLINT_CONFIG_PRETTIER_BIN: &str = "./node_modules/.bin/eslint-config-prettier";
 const PRETTIER_BIN: &str = "./node_modules/.bin/prettier";
-const JEST_BIN: &str = "./node_modules/.bin/jest";
-const JEST_CONFIG: &str = "./.jest-eslint.config.js";
 
 pub fn run_eslint_config_prettier_check() -> anyhow::Result<()> {
     execute_command(
@@ -25,13 +24,18 @@ pub fn run_prettier_check() -> anyhow::Result<()> {
 }
 
 pub fn run_eslint_check(trailing_args: &Vec<&str>) -> anyhow::Result<()> {
+    std::env::set_var("DEBUG", "eslint:cli-engine");
     execute_command(
-        create_command(JEST_BIN)
-            .expect("Jest binary doesn't exist")
-            .arg("--config")
-            .arg(JEST_CONFIG)
-            .arg("--changedSince")
-            .arg("origin/master^")
+        create_command(ESLINT_BIN)
+            .expect("Eslint binary doesn't exist")
+            .arg("--cache")
+            .arg("--cache-strategy")
+            .arg("content")
+            .arg("--cache-location")
+            .arg("node_modules/.cache/")
+            .arg("*.js") // root JS files
+            .arg("scripts/**/*.js")
+            .arg("src/**/*.js")
             .args(trailing_args),
     )
 }
