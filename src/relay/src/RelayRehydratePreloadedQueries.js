ab523ae63ded8f6168025c04296e76f8ecbe43ed
Martin Zlámal <mrtnzlml@gmail.com>
2022-07-28 19:03:16 -0500

    Relay: improve `RelayRehydratePreloadedQueries` docs

--- src/relay/src/RelayRehydratePreloadedQueries.js
+++ src/relay/src/RelayRehydratePreloadedQueries.js
@@ -11,22 +11,33 @@ type Props = {
 /**
  * This is an experimental (hence not document) component to allow a support for preloaded queries
  * in Next.js applications. It should be used somewhere in `_app.js` somewhere below
- * `RelayEnvironmentProvider` in the React tree. Additionally, it should be combined with Next.js
- * like so:
+ * `RelayEnvironmentProvider` in the React tree:
+ *
+ * ```
+ * <RelayRehydratePreloadedQueries Component={Component} pageProps={pageProps} />
+ * ```
+ *
+ * Additionally, it should be combined with Next.js like so:
  *
  * ```
  * const MenuQuery = graphql`
- *   query MenuQuery() {
+ *   query MenuQuery {
  *     ...MenuFragment
  *   }
  * `;
  *
- * export default function MenuPage(props): React.Node {
- *   const data = usePreloadedQuery(MenuQuery, props.relayPreloadedQueryRefs.menuQuery);
- *   return <Menu fragmentReference={data} />;
+ * type Props = {
+ *   +relayPreloadedQueryRefs: {
+ *     +menuQuery: PreloadedQuery<MenuQueryType>, // from __generated__
+ *   }
  * }
  *
- * export async function getServerSideProps(context) {
+ * export default function MenuPage(props: Props): React.Node {
+ *   const data = usePreloadedQuery(MenuQuery, props.relayPreloadedQueryRefs.menuQuery);
+ *   return <Menu relayFragment={data} />;
+ * }
+ *
+ * export async function getServerSideProps(context): Promise<…> {
  *   return {
  *     props: {
  *       relayPreloadedQueries: {
@@ -36,6 +47,8 @@ type Props = {
  *   };
  * }
  * ```
+ *
+ * Note: `relayPreloadQuery` is a custom function fetching the GraphQL data server-side.
  */
 export default function RelayRehydratePreloadedQueries({ Component, pageProps }: Props): Node {
   const environment = useRelayEnvironment();
