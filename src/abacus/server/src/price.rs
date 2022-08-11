881a0a44bf9f23ca74ae17cc7c26763524fe078b
Martin Zlámal <mrtnzlml@gmail.com>
2022-08-11 12:32:24 -0500

    Abacus: run Cargo `fmt` (Rust 1.63.0)

--- src/abacus/server/src/price.rs
+++ src/abacus/server/src/price.rs
@@ -2,16 +2,16 @@ use serde::{Deserialize, Serialize};
 
 #[allow(clippy::upper_case_acronyms)]
 #[derive(juniper::GraphQLEnum, Clone, Serialize, Deserialize, Debug, Copy, PartialEq)]
-pub(in crate) enum SupportedCurrency {
+pub(crate) enum SupportedCurrency {
     MXN,
 }
 
 #[derive(juniper::GraphQLObject, Clone, Deserialize, Debug, Serialize)]
-pub(in crate) struct Price {
+pub(crate) struct Price {
     /// The unit amount in centavo to be charged, represented as a whole integer.
     /// Centavo equals ¹⁄₁₀₀ of the basic monetary unit.
-    pub(in crate) unit_amount: i32,
+    pub(crate) unit_amount: i32,
 
     /// Three-letter [ISO currency code](https://www.iso.org/iso-4217-currency-codes.html).
-    pub(in crate) unit_amount_currency: SupportedCurrency,
+    pub(crate) unit_amount_currency: SupportedCurrency,
 }
