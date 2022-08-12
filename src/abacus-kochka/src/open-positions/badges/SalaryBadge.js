66928286aabe77ab009dfcac6ef230a02cd67182
Martin Zlámal <mrtnzlml@gmail.com>
2022-08-11 19:31:58 -0500

    Abacus KO: update open positions

--- src/abacus-kochka/src/open-positions/badges/SalaryBadge.js
+++ src/abacus-kochka/src/open-positions/badges/SalaryBadge.js
@@ -2,16 +2,34 @@
 
 import fbt from 'fbt';
 import React, { type Node } from 'react';
-import { Badge, Money, SupportedCurrencies } from '@adeira/sx-design';
+import { Badge, Money, type SupportedCurrencies } from '@adeira/sx-design';
 
-export default function SalaryBadge(): Node {
+type Props = {
+  +priceUnitAmount: number,
+  +priceUnitAmountCurrency: SupportedCurrencies,
+  +isPriceNegotiable?: boolean,
+};
+
+export default function SalaryBadge(props: Props): Node {
   return (
     <Badge tint="default">
       <fbt desc="salary badge text">
         <fbt:param name="money">
-          <Money priceUnitAmount={6000} priceUnitAmountCurrency={SupportedCurrencies.MXN} />
+          <Money
+            priceUnitAmount={props.priceUnitAmount}
+            priceUnitAmountCurrency={props.priceUnitAmountCurrency}
+          />
         </fbt:param>{' '}
-        / month
+        / month{' '}
+        <fbt:param name="negotiable">
+          {props.isPriceNegotiable ? (
+            <strong>
+              <fbt desc="negotiable">(negotiable)</fbt>
+            </strong>
+          ) : (
+            ''
+          )}
+        </fbt:param>
       </fbt>
     </Badge>
   );
