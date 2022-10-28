e68e70d8b92aea26b7ab57ddb79be1cc423f147c
Martin Zlámal <mrtnzlml@gmail.com>
2022-10-28 18:05:35 -0500

    SX Design: showcase more `Meter` component states in the story

--- src/sx-design/src/Meter/Meter.stories.js
+++ src/sx-design/src/Meter/Meter.stories.js
@@ -6,6 +6,7 @@
 import React, { type ElementConfig } from 'react';
 
 import Meter from './Meter';
+import LayoutBlock from '../Layout/LayoutBlock';
 import type { StoryTemplate } from '../types';
 
 // 👇 This default export determines where your story goes in the story list
@@ -15,16 +16,21 @@ export default {
 };
 
 // 👇 We create a "template" of how args map to rendering
-const Template = (args: ElementConfig<typeof Meter>) => <Meter {...args} />;
+const Template = (args: ElementConfig<typeof Meter>) => (
+  <LayoutBlock>
+    <Meter value={10} {...args} /> {/* red */}
+    <Meter value={50} {...args} /> {/* orange */}
+    <Meter value={90} {...args} /> {/* green */}
+  </LayoutBlock>
+);
 
 // 👇 Each story then reuses that template
 export const Default: StoryTemplate<typeof Meter> = Template.bind({});
 Default.storyName = 'Default';
 Default.args = {
-  value: 33,
   min: 0,
   max: 100,
   low: 25,
   high: 75,
-  optimum: 50,
+  optimum: 100,
 };
