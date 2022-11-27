10dd5fa8329d4346413e0b5b9df03b5dc7a534e9
Martin Zlámal <mrtnzlml@gmail.com>
2022-11-27 11:27:21 -0600

    Abacus BO: configure security headers
    See: https://nextjs.org/docs/advanced-features/security-headers

--- src/abacus-backoffice/next.config.js
+++ src/abacus-backoffice/next.config.js
@@ -12,7 +12,6 @@ const withCustomBabelConfigFile = require('next-plugin-custom-babel-config')({
 
 const withTranspileModules = require('next-transpile-modules')([
   '@adeira/css-colors',
-  '@adeira/fetch',
   '@adeira/forms',
   '@adeira/hooks',
   '@adeira/icons',
@@ -70,6 +69,57 @@ module.exports = (withPlugins(
       // https://github.com/vercel/next.js/issues/23725
       // https://github.com/vercel/next.js/pull/27069
       esmExternals: 'loose',
+    },
+    headers() {
+      return [
+        {
+          // Apply these headers to all routes in the application:
+          source: '/:path*',
+          headers: [
+            {
+              // This header controls DNS prefetching, allowing browsers to proactively perform
+              // domain name resolution on external links, images, CSS, JavaScript, and more.
+              // This prefetching is performed in the background, so the DNS is more likely to be
+              // resolved by the time the referenced items are needed. This reduces latency when
+              // the user clicks a link.
+              key: 'X-DNS-Prefetch-Control',
+              value: 'on',
+            },
+            {
+              // This header stops pages from loading when they detect reflected cross-site
+              // scripting (XSS) attacks. Although this protection is not necessary when sites
+              // implement a strong `Content-Security-Policy` disabling the use of inline
+              // JavaScript ('unsafe-inline'), it can still provide protection for older web
+              // browsers that don't support CSP.
+              key: 'X-XSS-Protection',
+              value: '1; mode=block',
+            },
+            {
+              // This header indicates whether the site should be allowed to be displayed within
+              // an iframe. This can prevent against clickjacking attacks. This header has been
+              // superseded by CSP's frame-ancestors option, which has better support in modern
+              // browsers.
+              key: 'X-Frame-Options',
+              value: 'SAMEORIGIN',
+            },
+            {
+              // This header prevents the browser from attempting to guess the type of content if
+              // the Content-Type header is not explicitly set. This can prevent XSS exploits for
+              // websites that allow users to upload and share files. For example, a user trying to
+              // download an image, but having it treated as a different Content-Type like an
+              // executable, which could be malicious. This header also applies to downloading
+              // browser extensions. The only valid value for this header is `nosniff`.
+              key: 'X-Content-Type-Options',
+              value: 'nosniff',
+            },
+            {
+              key: 'Content-Security-Policy-Report-Only',
+              value:
+                "default-src 'self'; report-uri https://o74963.ingest.sentry.io/api/4504227233071104/security/?sentry_key=fce56c4ba748422b93247bbf949af9f1",
+            },
+          ],
+        },
+      ];
     },
   },
 ) /*: $FlowFixMe */);
