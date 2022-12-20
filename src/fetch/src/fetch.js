247879a0f21a0b38897d03a78fd7d417f9c9e486
Martin Zlámal <mrtnzlml@gmail.com>
2022-12-20 10:24:36 -0600

    Archive `fetch` repository

--- src/fetch/src/fetch.js
+++
@@ -1,7 +0,0 @@
-// @flow
-
-import fetch from 'cross-fetch';
-
-// Dependency `cross-fetch` takes care of providing correct ponyfill for browsers,
-// React Native and Node.js. See: https://github.com/lquixada/cross-fetch/blob/0b198e8f3a8dc7ebad8f973d4a5bce6d00719b8e/package.json#L6-L8
-export default fetch;
--- src/fetch/src/test-utils.js
+++
@@ -1,9 +0,0 @@
-// @flow
-
-/* eslint-disable import/no-extraneous-dependencies */
-
-import { setupServer, type Server } from 'msw/node';
-
-export { rest } from 'msw';
-
-export const server: Server = setupServer();
