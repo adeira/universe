f3a4ad63161934a771c2e4bc713ad0bd8a6fc7b4
Martin Zlámal <mrtnzlml@gmail.com>
2023-03-03 21:42:06 +0100

    Upgrade `rimraf` to version 4

--- src/monorepo-npm-publisher/src/utils/resetBuildCache.js
+++ src/monorepo-npm-publisher/src/utils/resetBuildCache.js
@@ -1,8 +1,7 @@
 // @flow
 
 import rimraf from 'rimraf';
-import util from 'util';
 
 export async function resetBuildCache(buildCachePath: string): Promise<void> {
-  await util.promisify(rimraf)(buildCachePath);
+  await rimraf(buildCachePath);
 }
