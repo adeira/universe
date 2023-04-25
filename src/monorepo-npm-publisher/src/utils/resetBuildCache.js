95a194e2a00c244ab9bdf0abd092065abcc6a615
Martin Zlámal <mrtnzlml@gmail.com>
2023-04-25 10:04:20 -0600

    NPM Publisher: fix `rimraf` v5 usage (breaking change)

--- src/monorepo-npm-publisher/src/utils/resetBuildCache.js
+++ src/monorepo-npm-publisher/src/utils/resetBuildCache.js
@@ -1,6 +1,6 @@
 // @flow
 
-import rimraf from 'rimraf';
+import { rimraf } from 'rimraf';
 
 export async function resetBuildCache(buildCachePath: string): Promise<void> {
   await rimraf(buildCachePath);
