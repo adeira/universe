a9b50a6d4a7a3e29d84ef4eb1593d38e1d8a414c
DOUGES <itsdouges@users.noreply.github.com>
2023-05-18 17:09:09 +1000

    chore: add cli args

--- src/monorepo-shipit/bin/importit.js
+++ src/monorepo-shipit/bin/importit.js
@@ -1,8 +1,9 @@
 #!/usr/bin/env node
 
-// @flow strict-local
+// @flow
 
 import { invariant } from '@adeira/js';
+import commandLineArgs from 'command-line-args';
 
 import iterateConfigs from '../src/iterateConfigs';
 import createClonePhase from '../src/phases/createClonePhase';
@@ -14,29 +15,65 @@ import type { Phase } from '../types.flow';
 // TODO: check we can actually import this package (whether we have config for it)
 // yarn monorepo-babel-node src/monorepo-shipit/bin/importit.js git@github.com:adeira/fetch.git 1
 
-const argv = process.argv.splice(2); // TODO: better CLI
-invariant(argv.length === 2, 'Importit expects two arguments: git URL and PR number.');
-
-const exportedRepoURL = argv[0]; // git@github.com:adeira/fetch.git
-const pullRequestNumber = argv[1];
-
 const gitRegex = /^git@github.com:(?<packageName>.+)\.git$/;
+
+const options = commandLineArgs(
+  [
+    {
+      name: 'config-filter',
+      type: String,
+      defaultValue: '/*.js',
+    },
+    {
+      name: 'config-dir',
+      type: String,
+      defaultValue: './.shipit',
+    },
+    {
+      name: 'committer-name',
+      type: String,
+    },
+    {
+      name: 'committer-email',
+      type: String,
+    },
+    {
+      name: 'pull-request-id',
+      type: Number,
+    },
+    {
+      name: 'repo-url',
+      type: Number,
+    },
+  ],
+  { camelCase: true },
+);
+
+invariant(options.committerName, 'committer-name is required');
+invariant(options.committerEmail, 'committer-email is required');
+invariant(options.repoUrl, 'repo-url is required');
+invariant(options.pullRequestId, 'pull-request-id is required');
+
+process.env.SHIPIT_COMMITTER_EMAIL = options.committerName;
+process.env.SHIPIT_COMMITTER_NAME = options.committerEmail;
+
 invariant(
-  gitRegex.test(exportedRepoURL),
+  gitRegex.test(options.repoUrl),
   'We currently support imports only from GitHub.com - please open an issue to add additional services.',
 );
 
-const match = exportedRepoURL.match(gitRegex);
+const match = options.repoUrl.match(gitRegex);
 const packageName = match?.groups?.packageName;
-invariant(packageName != null, 'Cannot figure out package name from: %s', exportedRepoURL);
 
-iterateConfigs('/*.js', (config) => {
-  if (config.exportedRepoURL === exportedRepoURL) {
+invariant(packageName != null, 'Cannot figure out package name from: %s', options.repoUrl);
+
+iterateConfigs(options, (config) => {
+  if (config.exportedRepoURL === options.repoUrl) {
     new Set<Phase>([
       createClonePhase(config.exportedRepoURL, config.destinationPath),
       createCheckCorruptedRepoPhase(config.destinationPath),
       createCleanPhase(config.destinationPath),
-      createImportSyncPhase(config, packageName, pullRequestNumber),
+      createImportSyncPhase(config, packageName, options.pullRequestId),
     ]).forEach((phase) => phase());
   }
 });
