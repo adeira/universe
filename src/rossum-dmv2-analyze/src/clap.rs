b14b961294bd82e97d2d740ea523efdf3f3bec31
Martin Zlámal <mrtnzlml@gmail.com>
2023-07-06 07:02:36 -0600

    Rossum DMv2 analyze: improve CLI help

--- src/rossum-dmv2-analyze/src/clap.rs
+++ src/rossum-dmv2-analyze/src/clap.rs
@@ -4,9 +4,10 @@ pub fn generate_clap_app() -> Command {
     clap::command!()
         .arg(
             Arg::new("concurrency")
+                .short('c')
                 .long("concurrency")
                 .num_args(1)
-                .help("How many annotations should be analyzed concurrently.")
+                .long_help("How many annotations should be analyzed concurrently.")
                 .required(false)
                 .default_value("100"),
         )
@@ -14,27 +15,35 @@ pub fn generate_clap_app() -> Command {
             Arg::new("config-file")
                 .long("config-file")
                 .num_args(1)
-                .help("JSON config file with the new DMv2 configuration.")
+                .long_help(
+                    "JSON config file with the new DMv2 configuration. Copy-paste here the whole \
+                    content of DMv2 configuration including the top-level `configurations` key.",
+                )
                 .required(true),
         )
         .arg(
             Arg::new("dm-hook-id")
                 .long("dm-hook-id")
                 .num_args(1)
-                .help("DMv2 hook ID")
+                .long_help("DMv2 hook ID to analyze against.")
                 .required(true),
         )
         .arg(
             Arg::new("queue-id")
                 .long("queue-id")
                 .num_args(1)
-                .help("Queue ID")
+                .long_help(
+                    "Queue ID that will be used for the analysis of all relevant annotations.",
+                )
                 .required(true),
         )
         .arg(
             Arg::new("api-token")
                 .long("api-token")
                 .num_args(1)
+                .long_help(
+                    "API token belonging to the organization that will be used for calling Rossum API endpoints.",
+                )
                 .required(true),
         )
 }
