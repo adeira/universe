271074900e1d3af033a868338d5a55cc3c0d9129
Martin Zlámal <mrtnzlml@gmail.com>
2023-07-17 14:36:15 +0200

    Homebrew: add "test" formula
    Just to test some future changes instead of breaking the "production" formula.

---
+++ src/homebrew-universe/Formula/test.rb
@@ -0,0 +1,31 @@
+# Documentation: https://docs.brew.sh/Formula-Cookbook
+#                https://rubydoc.brew.sh/Formula
+class Test < Formula
+  desc ""
+  homepage "https://github.com/adeira/universe"
+  license ""
+
+  on_macos do
+    on_arm do
+      # openssl sha256 <file>
+      url "https://github.com/adeira/universe/releases/download/rossum-dmv2-analyze%2F0.1.1/aarch64-apple-darwin"
+      sha256 "3318dfb10f4cc2c5d0596495c63812d6139cc4c0e94eced353ff86863c83175c"
+    end
+
+    on_intel do
+      # openssl sha256 <file>
+      url "https://github.com/adeira/universe/releases/download/rossum-dmv2-analyze%2F0.1.1/x86_64-apple-darwin"
+      sha256 "f85f3f0e67248bee76c7f095636361b17b72a704e2dc6d6ff9a8f3d9a044f517"
+    end
+  end
+
+  def install
+    if OS.mac? && Hardware::CPU.arm?
+      bin.install "aarch64-apple-darwin" => "rossum-dmv2-analyze"
+    end
+
+    if OS.mac? && Hardware::CPU.intel?
+      bin.install "x86_64-apple-darwin" => "rossum-dmv2-analyze"
+    end
+  end
+end
