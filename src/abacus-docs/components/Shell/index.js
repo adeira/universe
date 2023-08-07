f3ccfc40f2cef049c3aa3882a6e5a648b2eef601
Martin Zlámal <mrtnzlml@gmail.com>
2023-08-07 08:19:30 +0200

    Remove unused `abacus-docs` repository

--- src/abacus-docs/components/Shell/index.js
+++
@@ -1,7 +0,0 @@
-// @flow
-
-export * from './Footer';
-export * from './SideNav';
-export * from './TableOfContents';
-export * from './ThemeToggle';
-export * from './TopNav';
--- src/abacus-docs/markdoc/functions.js
+++
@@ -1,9 +0,0 @@
-// @flow
-
-export const upper = {
-  transform(parameters: $FlowFixMe): $FlowFixMe {
-    const string = parameters[0];
-
-    return typeof string === 'string' ? string.toUpperCase() : string;
-  },
-};
