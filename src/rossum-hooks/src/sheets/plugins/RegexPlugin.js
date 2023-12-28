46a3092caae047899a0a7655276ce8eef39328f2
Martin Zlámal <mrtnzlml@gmail.com>
2023-12-28 21:34:02 +0100

    Rossum Sheets: implement Regex plugin

---
+++ src/rossum-hooks/src/sheets/plugins/RegexPlugin.js
@@ -0,0 +1,108 @@
+// @flow
+
+import { FunctionPlugin, FunctionArgumentType, CellError } from 'hyperformula';
+
+export const RegexPluginTranslations = {
+  enUS: {
+    REGEXEXTRACT: 'REGEXEXTRACT',
+    REGEXMATCH: 'REGEXMATCH',
+    REGEXREPLACE: 'REGEXREPLACE',
+  },
+};
+
+export class RegexPlugin extends FunctionPlugin {
+  regexExtract(ast: $FlowFixMe, state: $FlowFixMe): $FlowFixMe {
+    return this.runFunction(
+      ast.args,
+      state,
+      this.metadata('REGEXEXTRACT'),
+      (text, regularExpression) => {
+        if (typeof text !== 'string') {
+          return new CellError('VALUE', 'Function REGEXEXTRACT operates only on string.');
+        }
+        if (typeof regularExpression !== 'string') {
+          return new CellError(
+            'VALUE',
+            'Function REGEXEXTRACT accepts string as a second argument.',
+          );
+        }
+
+        const match = text.match(regularExpression);
+        if (match == null) {
+          return '';
+        }
+        return match[1] ?? match[0];
+      },
+    );
+  }
+
+  regexMatch(ast: $FlowFixMe, state: $FlowFixMe): $FlowFixMe {
+    return this.runFunction(
+      ast.args,
+      state,
+      this.metadata('REGEXMATCH'),
+      (text, regularExpression) => {
+        if (typeof text !== 'string') {
+          return new CellError('VALUE', 'Function REGEXMATCH operates only on string.');
+        }
+        if (typeof regularExpression !== 'string') {
+          return new CellError('VALUE', 'Function REGEXMATCH accepts string as a second argument.');
+        }
+
+        return text.match(regularExpression) != null;
+      },
+    );
+  }
+
+  regexReplace(ast: $FlowFixMe, state: $FlowFixMe): $FlowFixMe {
+    return this.runFunction(
+      ast.args,
+      state,
+      this.metadata('REGEXREPLACE'),
+      (text, regularExpression, replacement) => {
+        if (typeof text !== 'string') {
+          return new CellError('VALUE', 'Function REGEXREPLACE operates only on string.');
+        }
+        if (typeof regularExpression !== 'string') {
+          return new CellError(
+            'VALUE',
+            'Function REGEXREPLACE accepts string as a second argument.',
+          );
+        }
+        if (typeof replacement !== 'string') {
+          return new CellError('VALUE', 'Function REGEXREPLACE accepts only strings.');
+        }
+
+        return text.replace(new RegExp(regularExpression), replacement);
+      },
+    );
+  }
+}
+
+RegexPlugin.implementedFunctions = {
+  REGEXEXTRACT: {
+    // https://support.google.com/docs/answer/3098244
+    method: 'regexExtract',
+    parameters: [
+      { argumentType: FunctionArgumentType.STRING },
+      { argumentType: FunctionArgumentType.STRING },
+    ],
+  },
+  REGEXMATCH: {
+    // https://support.google.com/docs/answer/3098292
+    method: 'regexMatch',
+    parameters: [
+      { argumentType: FunctionArgumentType.STRING },
+      { argumentType: FunctionArgumentType.STRING },
+    ],
+  },
+  REGEXREPLACE: {
+    // https://support.google.com/docs/answer/3098245
+    method: 'regexReplace',
+    parameters: [
+      { argumentType: FunctionArgumentType.STRING },
+      { argumentType: FunctionArgumentType.STRING },
+      { argumentType: FunctionArgumentType.STRING },
+    ],
+  },
+};
---
+++ src/rossum-hooks/src/sheets/plugins/__tests__/RegexPlugin.test.js
@@ -0,0 +1,98 @@
+// @flow
+
+import payload from '../../__tests__/fixtures/payload.json';
+import settings from '../../__tests__/fixtures/settings.json';
+import createHyperFormulaInstance from '../../createHyperFormulaInstance';
+
+let hfInstance;
+let sheetId;
+beforeAll(() => {
+  hfInstance = createHyperFormulaInstance({ ...payload, settings });
+  sheetId = hfInstance.getSheetId(hfInstance.addSheet('__tests__'));
+});
+
+afterAll(() => {
+  hfInstance.destroy();
+});
+
+describe('REGEXEXTRACT', () => {
+  it('works as expected', () => {
+    expect(
+      hfInstance.calculateFormula(
+        '=REGEXEXTRACT("My favorite number is 241, but my friend\'s is 17", "\\d+")',
+        sheetId,
+      ),
+    ).toBe('241');
+
+    expect(hfInstance.calculateFormula('=REGEXEXTRACT("Google Doc 101", "[0-9]+")', sheetId)).toBe(
+      '101',
+    );
+
+    expect(
+      hfInstance.calculateFormula(
+        '=REGEXEXTRACT("The price today is $826.25", "[0-9]*\\.[0-9]+[0-9]+")',
+        sheetId,
+      ),
+    ).toBe('826.25');
+
+    expect(
+      hfInstance.calculateFormula(
+        '=REGEXEXTRACT("(Content) between brackets", "\\(([A-Za-z]+)\\)")',
+        sheetId,
+      ),
+    ).toBe('Content');
+
+    expect(hfInstance.calculateFormula('=REGEXEXTRACT("nothing", "something")', sheetId)).toBe('');
+  });
+});
+
+describe('REGEXMATCH', () => {
+  it('works as expected', () => {
+    expect(hfInstance.calculateFormula('=REGEXMATCH("Spreadsheets", "S.r")', sheetId)).toBe(true);
+    expect(hfInstance.calculateFormula('=REGEXMATCH("Spreadsheets", "S.rX")', sheetId)).toBe(false);
+
+    expect(hfInstance.calculateFormula('=REGEXMATCH("Google Doc 101", "[0-9]+")', sheetId)).toBe(
+      true,
+    );
+
+    expect(
+      hfInstance.calculateFormula(
+        '=REGEXMATCH("The price today is $826.25", "[0-9]*\\.[0-9]+[0-9]+")',
+        sheetId,
+      ),
+    ).toBe(true);
+
+    expect(
+      hfInstance.calculateFormula(
+        '=REGEXMATCH("(Content) between brackets", "\\(([A-Za-z]+)\\)")',
+        sheetId,
+      ),
+    ).toBe(true);
+  });
+});
+
+describe('REGEXREPLACE', () => {
+  it('works as expected', () => {
+    expect(
+      hfInstance.calculateFormula('=REGEXREPLACE("Spreadsheets", "S.*d", "Bed")', sheetId),
+    ).toBe('Bedsheets');
+
+    expect(
+      hfInstance.calculateFormula('=REGEXREPLACE("Google Doc 101", "[0-9]+", "777")', sheetId),
+    ).toBe('Google Doc 777');
+
+    expect(
+      hfInstance.calculateFormula(
+        '=REGEXREPLACE("The price today is $826.25", "[0-9]*\\.[0-9]+[0-9]+", "315.75")',
+        sheetId,
+      ),
+    ).toBe('The price today is $315.75');
+
+    expect(
+      hfInstance.calculateFormula(
+        '=REGEXREPLACE("(Content) between brackets", "\\(([A-Za-z]+)\\)", "Word")',
+        sheetId,
+      ),
+    ).toBe('Word between brackets');
+  });
+});
