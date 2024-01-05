bb1d16759fa8fa07ce10ba23b8e3764f32fb7144
Martin Zlámal <mrtnzlml@gmail.com>
2024-01-05 09:57:43 +0100

    Rossum Sheets: improve error handling for DigitalOcean

--- src/rossum-hooks/src/sheets/handlers/digitalOceanFunction.js
+++ src/rossum-hooks/src/sheets/handlers/digitalOceanFunction.js
@@ -1,17 +1,26 @@
 // @flow
 
+import createMessage from '../../utils/createMessage';
 import processRossumPayload from '../processRossumPayload';
 import type { ExtensionUserConfig } from '../validateUserConfig';
 import type { WebhookPayload } from '../../flowTypes';
 
 export function main(payload: WebhookPayload<ExtensionUserConfig>): { body: string } {
-  const { messages, operations, automation_blockers } = processRossumPayload(payload);
-
-  return {
-    body: JSON.stringify({
-      messages,
-      operations,
-      automation_blockers,
-    }),
-  };
+  try {
+    const { messages, operations, automation_blockers } = processRossumPayload(payload);
+    return {
+      body: JSON.stringify({
+        messages,
+        operations,
+        automation_blockers,
+      }),
+    };
+  } catch (error) {
+    return {
+      body: JSON.stringify({
+        messages: [createMessage('error', error.message)],
+        operations: [],
+      }),
+    };
+  }
 }
