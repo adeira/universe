c00b3b2fee387336b3547381f4d14d3b16724ea0
Martin Zlámal <mrtnzlml@gmail.com>
2024-01-08 15:14:00 +0100

    Rossum sheets: allow for intermediate calculations

--- src/rossum-sheets/src/createHyperFormulaInstance.js
+++ src/rossum-sheets/src/createHyperFormulaInstance.js
@@ -79,10 +79,13 @@ export default function createHyperFormulaInstance(
         Object.values(userSheets[sheetName].columns)
           .map((datapointID) => {
             if (isMetaField(datapointID)) {
-              // TODO: use `registerFunctionPlugin` instead?
+              // Meta fields such as `annotation.*` OR `document.*`:
               return lodashGet(payload, datapointID);
+            } else if (hfInstance.validateFormula(datapointID)) {
+              // Intermediate formula (no special treatment):
+              return datapointID;
             }
-
+            // Fallback to regular Rossum datapoint:
             const dpValue =
               findBySchemaId(payload.annotation.content, datapointID)[i].content.normalized_value ??
               findBySchemaId(payload.annotation.content, datapointID)[i].content.value;
