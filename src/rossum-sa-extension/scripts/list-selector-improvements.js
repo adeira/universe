177881e12c64ccc804a6ba2209e72efffbd127fa
Martin Zlámal <mrtnzlml@gmail.com>
2024-01-27 10:53:17 +0100

    Rossum SA ext: fix sorting of irrelevant lists
    Closes: https://github.com/adeira/universe/issues/7242

--- src/rossum-sa-extension/scripts/list-selector-improvements.js
+++ src/rossum-sa-extension/scripts/list-selector-improvements.js
@@ -2,25 +2,32 @@
 
 const listSelectorImprovements = document.createElement('style');
 
-// TODO: apply only on /settings/extensions/logs (?)
 listSelectorImprovements.textContent = `
-  div:has(> ul.MuiMenu-list) {
-    min-width: 400px !important;
+  div:has(> ul.rossum-sa-extension-list) {
+    min-width: 440px !important;
+    max-height: initial !important;
   }`;
 
 document.head?.appendChild(listSelectorImprovements);
 
 const listSelectorObserver = new MutationObserver((mutations) => {
-  for (const mutation of mutations) {
-    if (mutation.type === 'childList' && mutation.addedNodes.length > 0) {
-      for (const node of mutation.addedNodes) {
-        if (node instanceof HTMLElement) {
-          const element = node.querySelector('ul.MuiMenu-list');
-          if (element) {
-            // TODO: remove the extension ID and append it to the end of the item name
-            Array.from(element.getElementsByTagName('li'))
-              .sort((a, b) => a.textContent.localeCompare(b.textContent))
-              .forEach((li) => element.appendChild(li));
+  if (window.location.pathname === '/settings/extensions/logs') {
+    // It is important to execute these modifications only on the specified path not to influence
+    // other parts of the application.
+
+    for (const mutation of mutations) {
+      if (mutation.type === 'childList' && mutation.addedNodes.length > 0) {
+        for (const node of mutation.addedNodes) {
+          if (node instanceof HTMLElement) {
+            const element = node.querySelector('ul.MuiMenu-list');
+            if (element) {
+              element.classList.add('rossum-sa-extension-list'); // to apply custom styles specified above
+
+              // TODO: remove the extension ID and append it to the end of the item name
+              Array.from(element.getElementsByTagName('li'))
+                .sort((a, b) => a.textContent.localeCompare(b.textContent))
+                .forEach((li) => element.appendChild(li));
+            }
           }
         }
       }
