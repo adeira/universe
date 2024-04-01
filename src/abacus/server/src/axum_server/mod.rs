ca5a54aafee7386599ad1c8e244b2b95c9d76d57
Martin Zlámal <mrtnzlml@gmail.com>
2024-04-01 16:41:06 -0600

    Abacus: add `/redirect/:uuid` route to Axum server

--- src/abacus/server/src/axum_server/mod.rs
+++ src/abacus/server/src/axum_server/mod.rs
@@ -1,23 +1,25 @@
+mod handlers;
+
 use crate::arango::ConnectionPool;
 use crate::auth::users::{AnonymousUser, User};
+use crate::axum_server::handlers::{graphql_axum_handler, redirect_axum_handler};
 use crate::global_configuration::GlobalConfiguration;
 use crate::graphql_context::Context;
-use crate::graphql_schema::{create_graphql_schema, Schema};
+use crate::graphql_schema::create_graphql_schema;
 use axum::{
     routing::{get, post},
     Extension, Router,
 };
-use juniper_axum::{extract::JuniperRequest, response::JuniperResponse};
 use std::sync::Arc;
 
 pub fn create_axum_server(
-    pool: ConnectionPool,
+    connection_pool: ConnectionPool,
     global_configuration: GlobalConfiguration,
 ) -> Router<()> {
     let graphql_schema = create_graphql_schema();
 
     let context = Context {
-        pool,
+        pool: connection_pool.clone(),
         uploadables: None,
         user: User::AnonymousUser(AnonymousUser::new()), // TODO
         global_configuration,
@@ -28,18 +30,13 @@ pub fn create_axum_server(
         // POST /graphql (multipart/form-data) → TODO
         .route(
             "/graphql",
-            post(graphql_handler),
+            post(graphql_axum_handler),
         )
+        // GET /redirect/:uuid
+        .route("/redirect/:uuid", get(redirect_axum_handler))
         // GET /status/ping
         .route("/status/ping", get(|| async { "pong" }))
         .layer(Extension(Arc::new(graphql_schema)))
         .layer(Extension(context))
-}
-
-async fn graphql_handler(
-    Extension(schema): Extension<Arc<Schema>>,
-    Extension(context): Extension<Context>,
-    JuniperRequest(req): JuniperRequest, // should be the last argument as consumes `Request`
-) -> JuniperResponse {
-    JuniperResponse(req.execute(&*schema, &context).await)
+        .layer(Extension(connection_pool))
 }
