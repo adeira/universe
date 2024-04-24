4f52803e45ab0c0412ccabdcbf186c582188701d
Martin Zlámal <mrtnzlml@gmail.com>
2024-04-24 15:29:26 -0600

    Remove unused `adeira/flow-config-parser` repo

--- src/flow-config-parser/src/__tests__/parse.test.js
+++
@@ -1,8 +0,0 @@
-// @flow
-
-import path from 'path';
-import { generateTestsFromFixtures } from '@adeira/fixtures-tester';
-
-import parse from '../parse';
-
-generateTestsFromFixtures(path.join(__dirname, 'fixtures'), parse);
