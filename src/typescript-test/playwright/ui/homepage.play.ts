302c4fa88e01ef49d965632d5044def91f838a3d
Martin Zlámal <mrtnzlml@gmail.com>
2024-09-19 09:25:55 +0200

    Remove unused repositories

--- src/typescript-test/playwright/ui/homepage.play.ts
+++
@@ -1,6 +0,0 @@
-import { test, expect } from '@playwright/test';
-
-test('that homepage works', async ({ page }) => {
-  await page.goto('/');
-  await expect(page).toHaveTitle(/^Create Next App$/);
-});
