554be6c148003b732917e1af6d7ce8bf3b4abc1f
Martin Zlámal <mrtnzlml@gmail.com>
2024-09-19 09:25:55 +0200

    Remove unused repositories

--- src/typescript-test/src/app/api/hello/route.ts
+++
@@ -1,7 +0,0 @@
-/* eslint-disable ft-flow/no-types-missing-file-annotation */
-
-export async function GET(request: Request) {
-  await Promise.resolve();
-  console.log(request); // eslint-disable-line no-console
-  return new Response('Hello, Next.js!');
-}
--- src/x/src/executors/lints.rs
+++ src/x/src/executors/lints.rs
@@ -41,9 +41,6 @@ pub fn run_eslint_check(trailing_args: &Vec<&str>) -> anyhow::Result<()> {
             .arg("*.js") // root JS files
             .arg("scripts/**/*.js")
             .arg("src/**/*.js")
-            // .arg("src/**/*.mjs")
-            .arg("src/**/*.ts")
-            .arg("src/**/*.tsx")
             .args(trailing_args),
     )
 }
