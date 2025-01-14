4b7d186621ecc17fcb3d73257b71343f5458f033
Martin Zlámal <mrtnzlml@gmail.com>
2025-01-14 09:25:45 +0100

    Abacus: remove

--- src/abacus/server/src/commerce/dal/mod.rs
+++
@@ -1,4 +0,0 @@
-pub(in crate::commerce) mod orders;
-pub(in crate::commerce) mod product_addons;
-pub(in crate::commerce) mod product_categories;
-pub(in crate::commerce) mod products;
--- src/abacus/server/src/commerce/mod.rs
+++
@@ -1,9 +0,0 @@
-// Database abstraction layer should be accessible only through model layer (it checks permissions
-// and other business rules).
-pub(in crate::commerce) mod dal;
-
-// Model layer should be accessible only through public API.
-pub(in crate::commerce) mod model;
-
-// API is accessible from anywhere needed.
-pub(crate) mod api;
