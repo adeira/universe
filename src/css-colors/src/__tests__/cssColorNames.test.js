d915d358cbf960be2bda4701aa27766f8af968c7
Martin Zlámal <mrtnzlml@gmail.com>
2025-01-14 10:44:31 +0100

    CSS colors: remove

--- src/css-colors/src/__tests__/cssColorNames.test.js
+++
@@ -1,7 +0,0 @@
-// @flow strict
-
-import cssColorNames from '../cssColorNames';
-
-it('returns a map of all colors', () => {
-  expect(cssColorNames.get('rebeccapurple')).toBe('#663399');
-});
--- src/css-colors/src/hex3ToHex6.js
+++
@@ -1,9 +0,0 @@
-// @flow strict
-
-export default function hex3ToHex6(value: string): string {
-  const r = value[1];
-  const g = value[2];
-  const b = value[3];
-
-  return `#${r}${r}${g}${g}${b}${b}`;
-}
