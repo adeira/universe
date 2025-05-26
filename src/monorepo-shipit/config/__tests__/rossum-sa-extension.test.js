c613b62a048a93749b2e957769f72c27dfcb8c7b
Martin Zlámal <mrtnzlml@gmail.com>
2025-05-26 09:05:02 +0200

    Shipit: configure `rossum-sa-extension` exports

---
+++ src/monorepo-shipit/config/__tests__/rossum-sa-extension.test.js
@@ -0,0 +1,13 @@
+// @flow strict-local
+
+import path from 'path';
+
+import testExportedPaths from './testExportedPaths';
+
+testExportedPaths(path.join(__dirname, '..', 'rossum-sa-extension.js'), [
+  ['src/rossum-sa-extension/package.json', 'package.json'],
+
+  // invalid cases:
+  ['src/packages/monorepo/outsideScope.js', undefined], // correctly deleted
+  ['package.json', undefined], // correctly deleted
+]);
